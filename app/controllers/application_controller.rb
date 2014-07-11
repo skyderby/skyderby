cdf0728fc6a4d6bd5f0bec9892f27c2eb3e52407
Aleksandr K. MBP <ab@ipkunin.ru>
2014-07-11 20:58:09 +0400

    change routes, add default option to url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,9 @@ class ApplicationController < ActionController::Base
   def set_locale
       I18n.locale = params[:locale] || I18n.default_locale
   end
+
+  def default_url_options(options={})
+    { locale: I18n.locale }
+  end
+
 end
