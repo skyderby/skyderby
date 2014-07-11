64cf6073ad14726289894737c42526e1c55d3569
Aleksandr K. MBP <ab@ipkunin.ru>
2014-07-11 20:29:17 +0400

    First steps I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
+   
+  def set_locale
+      I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 TrackingDerby::Application.routes.draw do
 
-  match '/tracks', :to  => 'tracks#index', :as => :track, :via => :get
-  match '/track/:id', :to => 'tracks#show', :as => 'show_track', :via => :get
-  match '/tracks/new', :to => 'tracks#new', :as => :new_track, :via => [:get, :post]
+  scope "(/:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+    match '/tracks', :to  => 'tracks#index', :as => :track, :via => :get
+    match '/track/:id', :to => 'tracks#show', :as => 'show_track', :via => :get
+    match '/tracks/new', :to => 'tracks#new', :as => :new_track, :via => [:get, :post]
 
-  match '/select_track', :to => 'track_select#track_select', :as => :track_select, :via => [:post, :get]
+    match '/select_track', :to => 'track_select#track_select', :as => :track_select, :via => [:post, :get]
 
-  match '/about', :to => 'static_pages#about', :as => :about, :via => :get
-  match '/upload_error', :to => 'static_pages#upload_error', :as => :upload_error, :via => :get
+    match '/about', :to => 'static_pages#about', :as => :about, :via => :get
+    match '/upload_error', :to => 'static_pages#upload_error', :as => :upload_error, :via => :get
 
-  root 'static_pages#index'
+    get '/:locale' => 'static_pages#index'
+    root 'static_pages#index'
+  end
+
+  root to: redirect("/#{I18n.default_locale}", status: 302), as: :redirected_root
 
 end
