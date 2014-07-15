f572236ac869fb78599bf61148f4a30636367f93
Aleksandr K. MBP <ab@ipkunin.ru>
2014-07-16 00:48:47 +0400

    language switch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def lang_presentation(l_code)
+    l_array = {:en => 'English', :ru => "Русский"}
+    return l_array[l_code]
+  end
+
+  def lang_menu
+    content_tag(:ul, class: ('dropdown-menu'), role: ('menu')) do
+      I18n.available_locales.each do |loc|
+        locale_param = request.path == root_path ? root_path(locale: loc) : params.merge(locale: loc)
+        concat content_tag(:li, (link_to lang_presentation(loc), locale_param), class: (I18n.locale == loc ? "active" : ""))
+      end
+    end
+  end
 end
