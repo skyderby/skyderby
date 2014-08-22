1bc669b566af32b0660837d2c6539e10a7041618
Aleksandr K. MBP <ab@ipkunin.ru>
2014-08-23 00:14:13 +0400

    style flashes with bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,20 @@ module ApplicationHelper
       end
     end
   end
+
+  def bootstrap_class_for(flash_type)
+    case flash_type
+      when "success"
+        "alert-success"   # Green
+      when "error"
+        "alert-danger"    # Red
+      when "alert"
+        "alert-warning"   # Yellow
+      when "notice"
+        "alert-info"      # Blue
+      else
+        flash_type.to_s
+    end
+  end
+
 end
