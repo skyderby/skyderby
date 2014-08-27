6c3bd3c530de4d730bd907f43c514b4235c8b845
Aleksandr K. MBP <ab@ipkunin.ru>
2014-08-27 17:16:01 +0400

    working on events

--- db/migrate/20140823102843_add_relation_to_competitor.rb
+++ db/migrate/20140823102843_add_relation_to_competitor.rb
@@ -1,5 +1,5 @@
 class AddRelationToCompetitor < ActiveRecord::Migration
   def change
-    t.references :participatable, :polymorphic => true
+    add_references :participatable, :polymorphic => true
   end
 end
