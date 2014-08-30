a7afe63678f75c2e610d86f373e3b146f8b3edfc
Aleksandr K. MBP <ab@ipkunin.ru>
2014-08-30 11:00:07 +0400

    refactoring, convert view to HAML, new fields

--- db/migrate/20140824170013_create_user_profiles.rb
+++ db/migrate/20140824170013_create_user_profiles.rb
@@ -1,24 +1,24 @@
 class CreateUserProfiles < ActiveRecord::Migration
   def change
-    # create_table :user_profiles do |t|
-    #
-    #   t.string :last_name
-    #   t.string :first_name
-    #   t.string :name
-    #
-    #   t.integer :jumps_total
-    #   t.integer :jumps_wingsuit
-    #   t.integer :jumps_last_year
-    #
-    #   t.attachment :userpic
-    #
-    # end
+    create_table :user_profiles do |t|
 
-    # remove_column :users, :last_name
-    # remove_column :users, :first_name
-    # remove_column :users, :name
-    # remove_column :users, :total_jumps
-    # remove_column :users, :wingsuit_jumps
+      t.string :last_name
+      t.string :first_name
+      t.string :name
+
+      t.integer :jumps_total
+      t.integer :jumps_wingsuit
+      t.integer :jumps_last_year
+
+      t.attachment :userpic
+
+    end
+
+    remove_column :users, :last_name
+    remove_column :users, :first_name
+    remove_column :users, :name
+    remove_column :users, :total_jumps
+    remove_column :users, :wingsuit_jumps
 
   end
 end
