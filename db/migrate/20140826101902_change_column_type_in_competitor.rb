3280fe370a9c0908184fab08d6a1cd405912cd1c
Aleksandr K. MBP <ab@ipkunin.ru>
2014-08-31 14:32:12 +0400

    hotfix

--- db/migrate/20140826101902_change_column_type_in_competitor.rb
+++ db/migrate/20140826101902_change_column_type_in_competitor.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeInCompetitor < ActiveRecord::Migration
   def change
-    #remove_reference :competitors, :participatable
+    remove_reference :competitors, :participatable
     add_reference :competitors, :participation_forms
   end
 end
