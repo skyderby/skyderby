5762328ed9b1a7eb7509fcf17a70024da6bc4c18
AleksandrK <ab@ipkunin.ru>
2014-09-01 12:35:28 +0400

    hotfix

--- db/schema.rb
+++ db/schema.rb
@@ -194,11 +194,9 @@ ActiveRecord::Schema.define(version: 20140831192708) do
     t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "wingsuits", force: true do |t|
     t.integer "manufacturer_id"
