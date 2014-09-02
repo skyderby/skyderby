8eee23fa954ac97067d20180d3a5a39df3a765a4
Aleksandr K. MBP <ab@ipkunin.ru>
2014-09-02 09:44:56 +0400

    add dz_info to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -22,7 +22,7 @@ class EventsController < ApplicationController
   end
 
   def update
-    @event.update params[:event].permit(:name, :place, :comp_range_from, :comp_range_to, :descriprion, :form_info)
+    @event.update params[:event].permit(:name, :place, :comp_range_from, :comp_range_to, :descriprion, :form_info, :dz_info)
     redirect_to @event, notice: 'Данные успешно обновлены.'
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140901171358) do
+ActiveRecord::Schema.define(version: 20140902053906) do
 
   create_table "assignments", force: true do |t|
     t.integer "user_id"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20140901171358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "form_info"
+    t.text     "dz_info"
   end
 
   create_table "invitations", force: true do |t|
