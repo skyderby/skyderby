844deefe97d5a0ad01e4e62487c5a80713bf0582
Aleksandr K. MBP <ab@ipkunin.ru>
2014-09-30 23:24:03 +0400

    anchor для вкладок. Рефакторинг

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,16 @@ $(document).ready(function($) {
         }
     });
 
+    var hash = window.location.hash;
+    hash && $('ul.nav a[href="' + hash + '"]').tab('show');
+
+    $('.nav-tabs a').click(function (e) {
+        $(this).tab('show');
+        var scrollmem = $('body').scrollTop();
+        window.location.hash = this.hash;
+        $('html,body').scrollTop(scrollmem);
+    });
+
     var ws_field = $('.wingsuit-autocomplete');
     ws_field.autocomplete({
         serviceUrl: '/wingsuits/autocomplete',
