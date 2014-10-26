4a6e271dfb0e1b229526e134e1c9451e38da631b
Aleksandr K. MBP <ab@ipkunin.ru>
2014-10-26 18:27:21 +0300

    рефакторинг

--- lib/median_filter.rb
+++ lib/median_filter.rb
@@ -1,45 +1,44 @@
-class MedianFilter
-  attr_accessor :step
+module MedianFilter
 
-  def initialize(step, keys)
+  class MFilter
 
-    raise ArgumentError.new('Step must be odd and higher than 3') if step < 3 && step % 2 != 1
-    raise ArgumentError.new('Keys must be array of symbols') unless keys.kind_of?(Array) && keys.count > 0
+    def initialize(window_size, keys)
+      raise ArgumentError.new('Step must be odd and higher than 2') if window_size_invalid(window_size)
+      raise ArgumentError.new('Keys must be array of symbols') if keys_invalid(keys)
 
-    @step = step
-    @keys = keys
-
-  end
-
-  def filter(param_values)
-    process_values param_values.clone
-  end
-
-  def filter!(param_values)
-    process_values param_values
-  end
-
-  private
-
-  def process_values(values)
-
-    half_step = (@step - 1) / 2
-
-    values.each_index do |i|
-
-      item = values[i]
-
-      median_points = [ values[ [0, i - half_step].max ],
-                        item,
-                        values[ [values.count - 1, i + half_step].min ] ]
-
-      @keys.each do |key|
-        item[key] = median_points.map { |x| x[key] || 0 }.sort[half_step]
-      end
-
+      @window_size = window_size
+      @keys = keys
     end
-    
-    values
+
+    def process(values)
+      neighbors = (@window_size / 2).floor
+      temp = ([values.first] * neighbors) + values + ([values.last] * neighbors)
+
+      temp.each_cons(@window_size).map do |window|
+        @keys.each do |key|
+          window[neighbors][key] = window.map { |x| x[key] || 0 }.sort[neighbors]
+        end
+        window[neighbors]
+      end
+    end
+
+    private
+
+    def window_size_invalid(window_size)
+      window_size < 3 && window_size.even?
+    end
+
+    def keys_invalid(keys)
+      return true unless keys.kind_of?(Array) || !keys.empty? || keys.detect{ |x| !x.kind_of? Symbol }.nil?
+      false
+    end
+
+  end
+
+  def process(values, window_size, keys)
+
+    median_filter = MFilter.new(window_size, keys)
+    median_filter.process values
 
   end
 
