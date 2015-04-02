b5dd9983b83733790d0786fed55641952ef0ee8a
Aleksandr K. MBP <ab@ipkunin.ru>
2015-04-03 01:40:35 +0300

    hotfix for profile page

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -7,7 +7,7 @@ module UserProfileHelper
                   .includes(:wingsuit)
                   .order('id DESC')
                   
-    tracks = tracks.public_track unless @profile == current_user.user_profile
+    tracks = tracks.public_track unless current_user && @profile == current_user.user_profile
     render template: 'api/user_profiles/_tracks.json.jbuilder',
            format: :json,
            locals: { tracks: tracks }
