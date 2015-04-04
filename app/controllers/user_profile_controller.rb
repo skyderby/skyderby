95c1355a4f3f29e7ebdef2b297e9f8414534d6b1
Aleksandr K. MBP <ab@ipkunin.ru>
2015-04-04 23:13:53 +0300

    hotfix for critical error

--- app/controllers/user_profile_controller.rb
+++ app/controllers/user_profile_controller.rb
@@ -1,7 +1,5 @@
 # encoding: utf-8
 class UserProfileController < ApplicationController
-  before_action :set_user, only: [:update]
-
   def show
     @profile = UserProfile
                 .includes(:badges)
@@ -15,19 +13,14 @@ class UserProfileController < ApplicationController
   end
 
   def update
-    @profile = @user.user_profile
+    @profile = UserProfile.find(params[:id])
     @profile.update profile_params
     redirect_to user_profile_path(@profile)
   end
 
   private
 
-  def set_user
-    @user = User.find(params[:user_id])
-  end
-
   def profile_params
     params.require(:user_profile).permit(:name, :userpic)
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,7 @@
 # encoding: utf-8
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
 
   def index
-    # User.joins("LEFT OUTER JOIN (#{subquery}) subquery ON users.id = subquery.user_id").select('IFNULL(subquery.result, 0) time', 'users.id').each{ |x| puts x[:id].to_s + ' - ' + x[:time].to_s }
-    time_subquery = TrackResult.where(discipline: 0).joins(:track).select('MAX(result) result', 'tracks.user_id').group('tracks.user_id').to_sql
-    distance_subquery = TrackResult.where(discipline: 1).joins(:track).select('MAX(result) result', 'tracks.user_id').group('tracks.user_id').to_sql
-    speed_subquery = TrackResult.where(discipline: 2).joins(:track).select('MAX(result) result', 'tracks.user_id').group('tracks.user_id').to_sql
-    @users = User.includes()
   end
   
   def show
@@ -17,18 +11,14 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user.update user_params
-    redirect_to user_path(@user)
   end
 
   private
 
     def set_user
-      @user = User.find(params[:id])
     end
 
     def user_params
-      params.require(:user).permit(:first_name, :last_name)
     end
 
 end
