14ca8539c36ebb601b8d1eeaef42611280b87bc6
Aleksandr K. MBP <ab@ipkunin.ru>
2015-04-04 13:18:33 +0300

    Sort tracks by date created, adapt view for ipad

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -5,7 +5,8 @@ module UserProfileHelper
                   .includes(:distance)
                   .includes(:speed)
                   .includes(:wingsuit)
-                  .order('id DESC')
+                  .includes(wingsuit: :manufacturer)
+                  .order('created_at DESC')
                   
     tracks = tracks.public_track unless current_user && @profile == current_user.user_profile
     render template: 'api/user_profiles/_tracks.json.jbuilder',
