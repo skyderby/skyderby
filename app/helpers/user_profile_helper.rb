2fac5f70843d791039b844c64c687e61ce079b14
Aleksandr K. MBP <ab@ipkunin.ru>
2015-04-05 15:22:30 +0300

    user profile refactoring

--- app/helpers/user_profile_helper.rb
+++ app/helpers/user_profile_helper.rb
@@ -13,4 +13,72 @@ module UserProfileHelper
     #        format: :json,
     #        locals: { tracks: tracks }
   end
+
+  def is_owner
+    current_user && current_user.user_profile == @profile
+  end
+
+  def profile_name
+    unless @profile.name.blank?
+      @profile.name
+    else
+      'Name not set'
+    end
+  end
+
+  def max_distance
+    track = profile_tracks.max_by do |x| 
+      if x.distance
+        x.distance.result 
+      else
+        0
+      end
+    end
+
+    if track
+      track.distance.result.round
+    else
+      '----'
+    end
+  end
+
+  def max_speed
+    track = profile_tracks.max_by do |x| 
+      if x.speed
+        x.speed.result 
+      else
+        0
+      end
+    end
+
+    if track
+      track.speed.result.round
+    else
+      '---'
+    end
+  end
+
+  def max_time
+    track = profile_tracks.max_by do |x| 
+      if x.time
+        x.time.result 
+      else
+        0
+      end
+    end
+
+    if track
+      track.time.result.round(1)
+    else
+      '--.-'
+    end
+  end
+
+  def profile_tracks
+    if is_owner
+      profile_tracks = @profile.tracks
+    else
+      profile_tracks = @profile.public_tracks
+    end
+  end
 end
