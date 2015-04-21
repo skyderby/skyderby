c42e4c954ab316ea880d9eb3b7eb61f323ffe461
Aleksandr K. MBP <ab@ipkunin.ru>
2015-04-21 11:58:52 +0300

    allow worldwide base competitions

--- app/models/virtual_competition.rb
+++ app/models/virtual_competition.rb
@@ -24,23 +24,14 @@ class VirtualCompetition < ActiveRecord::Base
   enum jumps_kind: [:skydive, :base]
   enum suits_kind: [:wingsuit, :tracksuit]
   enum discipline: 
-    [:time, :distance, :speed, :distance_in_time, :straightline_distance_in_time]
+    [:time, :distance, :speed, :distance_in_time, :time_until_line]
 
   def self.by_track(track)
     competitions = VirtualCompetition.order(:name)
     competitions = competitions.tracksuit if track.wingsuit.tracksuit?
     competitions = competitions.wingsuit if track.wingsuit.wingsuit?
-
-    # Place = nil means worldwide (without filter)
-    # BASE jumps could be only used in virtual competitions with place
-    if track.skydive?
-      competitions = 
-        competitions.skydive
-                    .where('place_id = ? OR place_id IS NULL', track.place_id)
-    elsif track.base?
-      competitions = competitions.base.where(place_id: track.place_id)
-    end
-
+    # Blank place means worldwide (without filter)
+    competitions = competitions.where(place_id: track.place_id) if track.place_id
     competitions = competitions.where('period_from <= ?', track.created_at)
     competitions = competitions.where('period_to >= ?', track.created_at)
 
