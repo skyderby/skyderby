21acb1fe5838e4db9d082e1e1944c5485b1899a6
Aleksandr Kunin <ab@ipkunin.ru>
2015-05-03 12:09:53 +0300

    api/user_profile_controller refactoring

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -5,6 +5,7 @@ class UserProfile < ActiveRecord::Base
            -> { where(visibility: 0).order('created_at DESC') },
            class_name: 'Track' 
   has_many :badges
+  has_many :event_organizers
 
   has_attached_file :userpic,
                     styles: { large: '500x500>',
@@ -14,4 +15,15 @@ class UserProfile < ActiveRecord::Base
 
   validates_attachment_content_type :userpic, content_type:
     ['image/jpeg', 'image/jpg', 'image/png']
+
+  # Возвращает массив ID соревнований в которых является организатором
+  def organizer_of_events
+    event_organizers.pluck(:event_id)
+  end
+
+  class << self
+    def search(query)
+      where('LOWER(name) LIKE LOWER(?)', "%#{query}%")
+    end
+  end
 end
