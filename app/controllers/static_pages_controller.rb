54d6c719fcb6d6d7080dc6fae5f7e483e316d9fb
Aleksandr Kunin <ab@ipkunin.ru>
2015-06-01 19:38:48 +0300

    added logo for OpenGraph, added border for center block in index

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,6 +4,8 @@ class StaticPagesController < ApplicationController
     @track = Track.new
     @competitions = 
       VirtualCompetition.joins(:group)
+                        .includes(:virtual_comp_results)
+                        .includes(virtual_comp_results: :user_profile)
                         .where('virtual_comp_groups.display_on_start_page = ?', true)
                         .order('virtual_comp_groups.name')
                         .group_by { |x| x.group.name }
