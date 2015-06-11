e2c74f0915fbfcb8dfda264300a2b7ef2c3aae20
Aleksandr Kunin <ab@ipkunin.ru>
2015-06-11 09:18:58 +0300

    refactoring

--- lib/skyderby/parsers/columbus_parser.rb
+++ lib/skyderby/parsers/columbus_parser.rb
@@ -15,30 +15,59 @@ require 'tracks/track_point'
 module Skyderby
   module Parsers
     class ColumbusParser < CSVParser
+      # Column indexes
+      INDEX     = 0
+      TAG       = 1
+      DATE      = 2
+      TIME      = 3
+      LATITUDE  = 4
+      LONGITUDE = 5
+      HEIGHT    = 6
+      SPEED     = 7
+      HEADING   = 8
+      VOX       = 9
+
+      # Coordinates suffixes
+      NORTH = 'N'
+      SOUTH = 'S'
+      EAST = 'E'
+      WEST = 'W'
+
       protected
 
       def parse_row(row)
-        return nil if row[6].to_f == 0.0
+        row_height = row[HEIGHT].to_f
+        return nil if row_height == 0.0
 
         TrackPoint.new(latitude: parse_latitude(row),
                        longitude: parse_longitude(row),
-                       elevation: row[6].to_f,
-                       abs_altitude: row[6].to_f,
+                       elevation: row_height,
+                       abs_altitude: row_height,
                        gps_time: parse_datetime(row))
       end
 
       private
 
+      def parse_coordinate(coordinate)
+        coordinate_suffix = coordinate[coordinate.length - 1]
+        coordinate_value = coordinate[0..(coordinate.length - 2)]
+
+        -coordinate_value if [SOUTH, WEST].include? coordinate_suffix
+      end
+
       def parse_latitude(row)
-        (row[4][0..(row[4].length - 2)] * (row[4][row[4].length - 1] == 'N' ? 1 : -1)).to_f
+        parse_coordinate(row[LATITUDE]).to_f
       end
 
       def parse_longitude(row)
-        (row[5][0..(row[5].length - 2)] * (row[5][row[5].length - 1] == 'E' ? 1 : -1)).to_f
+        parse_coordinate(row[LONGITUDE]).to_f
       end
 
       def parse_datetime(row)
-        Time.strptime('20' + row[2].to_s + 'T' + row[3].to_s, '%Y%m%dT%H%M%S')
+        date_str = '20' + row[DATE].to_s
+        time_str = row[TIME].to_s
+
+        Time.strptime("#{date_str}T#{time_str}", '%Y%m%dT%H%M%S')
       end
     end
   end
