4cce1cca680eddb2026c3a05efeb0118f3b0a685
Aleksandr Kunin <ab@ipkunin.ru>
2015-06-17 20:29:50 +0300

    extracted suit presentation logic to helper

--- app/helpers/wingsuit_helper.rb
+++ app/helpers/wingsuit_helper.rb
@@ -1,2 +1,17 @@
 module WingsuitHelper
+  def suit_presentation(suit)
+    manufacturer = suit.manufacturer
+    code_span = content_tag(:span,
+                            class: 'text-warning',
+                            'data-toggle' => 'tooltip',
+                            title: manufacturer.name) do
+      manufacturer.code || ''
+    end
+
+    name_span = content_tag(:span) do
+      ' ' + suit.name
+    end
+
+    code_span.concat(name_span)
+  end
 end
