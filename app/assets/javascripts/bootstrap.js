d88aa2740edf9d11bf5ffa22263d9ce4ef6171e3
Aleksandr Kunin <ab@ipkunin.ru>
2015-06-18 23:32:45 +0300

    View 5 best results in table, design improvements

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -1,4 +1,6 @@
 $(function() {
   $("a[rel~=popover], .has-popover, [data-toggle=popover]").popover();
   $("a[rel~=tooltip], .has-tooltip, [data-toggle=tooltip]").tooltip();
+
+  $('table.table td a').on('click', function(e) { e.stopPropagation(); });
 });
