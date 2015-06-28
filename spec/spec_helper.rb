afe11102e26586b072326c77f98b3aa8661a3158
Aleksandr Kunin <ab@ipkunin.ru>
2015-06-28 17:44:47 +0300

    database_cleaner configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,8 +44,23 @@ RSpec.configure do |config|
   config.include ControllerHelpers, type: :controller
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
   end
 
   # config.around(:each) do |example|
