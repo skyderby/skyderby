73f85af43cc47bdb09d692f9e1c58a78a17c92d5
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-04 10:46:22 +0300

    Added qualification rounds and jumps, added tournament views

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,12 +1,58 @@
 class TournamentsController < ApplicationController
-  before_action :set_tournament
+  before_action :set_tournament, only: [:show, :edit, :update, :destroy]
+
+  load_and_authorize_resource
+
+  def index
+    @tournaments = Tournament.order('id DESC')
+  end
 
   def show
+  end
+
+  def new
+    @tournament = Tournament.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tournament = Tournament.new(tournament_params)
+
+    if @tournament.save
+      redirect_to @tournament, notice: 'Tournament was successfully created.'
+    else
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @tournament.update(tournament_params)
+      redirect_to @tournament, notice: 'Tournament was successfully updated.'
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @tournament.destroy
+    redirect_to tournaments_path
   end
 
   private
 
   def set_tournament
     @tournament = Tournament.find(params[:id])
+  end
+
+  def tournament_params
+    params.require(:tournament).permit(
+      :name,
+      :finish_start_lat,
+      :finish_start_lon,
+      :finish_end_lat,
+      :finish_end_lon
+    )
   end
 end
--- app/models/tournament_match_competitor.rb
+++
@@ -1,5 +0,0 @@
-class TournamentMatchCompetitor < ActiveRecord::Base
-  belongs_to :tournament_match
-  belongs_to :tournament_competitor
-  belongs_to :track
-end
