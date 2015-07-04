d552cb2a0bf0df3e5f4d897d03cb6001d8aad8d1
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-04 17:59:38 +0300

    annotate

--- app/models/tournament_competitor.rb
+++ app/models/tournament_competitor.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: tournament_competitors
+#
+#  id              :integer          not null, primary key
+#  tournament_id   :integer
+#  user_profile_id :integer
+#  wingsuit_id     :integer
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class TournamentCompetitor < ActiveRecord::Base
   belongs_to :tournament
   belongs_to :user_profile
