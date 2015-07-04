bf1e58746cdc4fbda32943914c5ff1aca26fdbc3
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-04 20:24:55 +0300

    Automatic results processing, added fields, forms and controller changed accordingly

--- app/models/tournament_match_competitor.rb
+++ app/models/tournament_match_competitor.rb
@@ -15,4 +15,32 @@ class TournamentMatchCompetitor < ActiveRecord::Base
   belongs_to :tournament_competitor
   belongs_to :tournament_match
   belongs_to :track
+
+  before_save :calculate_result
+
+  delegate :start_time, to: :tournament_match
+
+  def calculate_result
+    return unless track
+    return if (result || 0) > 0
+
+    track_points = Skyderby::Tracks::Points.new(track)
+    self.result = Skyderby::ResultsProcessors::TimeUntilIntersection.new(
+      track_points, start_time: start_time, finish_line: finish_line
+    ).calculate
+  end
+
+  def finish_line
+    tournament = tournament_match.round.tournament
+    [
+      Skyderby::Tracks::TrackPoint.new(
+        latitude: tournament.finish_start_lat,
+        longitude: tournament.finish_start_lon
+      ),
+      Skyderby::Tracks::TrackPoint.new(
+        latitude: tournament.finish_end_lat,
+        longitude: tournament.finish_end_lon
+      )
+    ]
+  end
 end
