4f182f45d34932e13859d93acd2c92f7668c9823
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-07 00:17:27 +0300

    Implemented place presentation method and test.

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,17 @@
 module PlacesHelper
+  def place_presentation(place)
+    country = place.country
+    code_span = content_tag(:span,
+                            class: 'text-warning',
+                            'data-toggle' => 'tooltip',
+                            title: country.name) do
+      country.code.upcase || ''
+    end
+
+    name_span = content_tag(:span) do
+      ', ' + place.name
+    end
+
+    code_span.concat(name_span)
+  end
 end
