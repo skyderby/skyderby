698c1d5624b9c97f9a5987b4c516c0642007805a
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-07 21:39:43 +0300

    changed format of place presentation

--- spec/helpers/places_helper.rb
+++ spec/helpers/places_helper.rb
@@ -4,7 +4,7 @@ describe PlacesHelper, type: :helper do
   it 'returns place presentation with country code' do
     place = create :place, :gridset
     expect(helper.place_presentation(place)).to eq(
-      "<span class=\"text-warning\" data-toggle=\"tooltip\" title=\"#{place.country.name}\">#{place.country.code.upcase}</span><span>, #{place.name}</span>"
+      "<span>#{place.name}</span> (<span class=\"text-warning\" data-toggle=\"tooltip\" title=\"#{place.country.name}\">#{place.country.code.upcase}</span>)"
     )
   end
 end
