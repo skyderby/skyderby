4854eddd6beb02a136faa816842a5d8709e30104
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-12 17:02:44 +0300

    created index and show pages for wingsuits

--- spec/factories/wingsuits.rb
+++ spec/factories/wingsuits.rb
@@ -2,11 +2,15 @@
 #
 # Table name: wingsuits
 #
-#  id              :integer          not null, primary key
-#  manufacturer_id :integer
-#  ws_class_id     :integer
-#  name            :string(255)
-#  kind            :integer          default(0)
+#  id                 :integer          not null, primary key
+#  manufacturer_id    :integer
+#  name               :string(255)
+#  kind               :integer          default(0)
+#  photo_file_name    :string(255)
+#  photo_content_type :string(255)
+#  photo_file_size    :integer
+#  photo_updated_at   :datetime
+#  description        :text(65535)
 #
 
 FactoryGirl.define do
