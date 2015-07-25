afa69fe74276d0251406d665a9e0eae1d13fb1ff
Aleksandr Kunin <ab@ipkunin.ru>
2015-07-25 22:50:21 +0300

    added trajectory distance indication. minor issues with sponsors.

--- app/assets/javascripts/app/views/events/sponsor.js
+++ app/assets/javascripts/app/views/events/sponsor.js
@@ -3,6 +3,7 @@ Skyderby.views.EventSponsor = Backbone.View.extend({
     template: JST['app/templates/sponsor'],
 
     tagName: 'div',
+    className: 'col-md-3 col-sm-3 text-center',
 
     render: function() {
         this.$el.append(this.template({sponsor: this.model}));
