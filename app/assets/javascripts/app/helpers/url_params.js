a43e2512ca8cd9f5a7d7dca886f782af89e66dd1
Aleksandr Kunin <ab@ipkunin.ru>
2015-10-12 09:50:22 +0300

    added js helper to parse params

---
+++ app/assets/javascripts/app/helpers/url_params.js
@@ -0,0 +1,13 @@
+Skyderby.helpers.getUrlParams = function() {
+    var params = {};
+
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        params[decodeURIComponent(pair[0])] = pair[1];
+    }
+
+    return params;
+};
