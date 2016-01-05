378030e3f9a1a481dd5773f5cbb977a9800a116c
Aleksandr Kunin <ab@ipkunin.ru>
2016-01-05 14:16:51 +0300

    Implemented asynchronous google maps api loading

---
+++ app/assets/javascripts/app/helpers/init_maps_api.js
@@ -0,0 +1,7 @@
+Skyderby.helpers.init_maps_api = function() {
+    if (window.Skyderby.maps_api_ready) {
+        window.Skyderby.trigger('maps_api_ready');
+    } else {
+        Skyderby.helpers.load_script('http://maps.googleapis.com/maps/api/js?callback=on_maps_api_ready');
+    }
+};
