16b117c968d460914e0d1195980b69627dea9595
Aleksandr Kunin <ab@ipkunin.ru>
2016-01-07 21:44:33 +0300

    change maps url to https

--- app/assets/javascripts/app/helpers/init_maps_api.js
+++ app/assets/javascripts/app/helpers/init_maps_api.js
@@ -2,6 +2,6 @@ Skyderby.helpers.init_maps_api = function() {
     if (window.Skyderby.maps_api_ready) {
         window.Skyderby.trigger('maps_api_ready');
     } else {
-        Skyderby.helpers.load_script('http://maps.googleapis.com/maps/api/js?callback=on_maps_api_ready');
+        Skyderby.helpers.load_script('https://maps.googleapis.com/maps/api/js?callback=on_maps_api_ready');
     }
 };
