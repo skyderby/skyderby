0d81303ad6d76ed7782506ab26e5a192be90954e
Aleksandr Kunin <ab@ipkunin.ru>
2016-03-24 22:22:57 +0300

    Added annotations to models, spec/models and spec/factories

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,18 +2,19 @@
 #
 # Table name: events
 #
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  range_from      :integer
-#  range_to        :integer
-#  created_at      :datetime
-#  updated_at      :datetime
-#  status          :integer          default(0)
-#  user_profile_id :integer
-#  place_id        :integer
-#  is_official     :boolean          default(FALSE)
-#  rules           :integer          default(0)
-#  starts_at       :date
+#  id                :integer          not null, primary key
+#  name              :string(255)
+#  range_from        :integer
+#  range_to          :integer
+#  created_at        :datetime
+#  updated_at        :datetime
+#  status            :integer          default(0)
+#  user_profile_id   :integer
+#  place_id          :integer
+#  is_official       :boolean          default(FALSE)
+#  rules             :integer          default(0)
+#  starts_at         :date
+#  wind_cancellation :boolean          default(FALSE)
 #
 
 class Event < ActiveRecord::Base
