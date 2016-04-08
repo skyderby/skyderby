b9e93b528317257e2c3cb133f1016b612feca6c5
Aleksandr Kunin <ab@ipkunin.ru>
2016-04-08 20:19:01 +0300

    Migration to postgres, fixing tests.

--- lib/skyderby/tracks/edit_data.rb
+++ lib/skyderby/tracks/edit_data.rb
@@ -37,9 +37,35 @@ module Skyderby
       protected
 
       def init_points
-        @points = Skyderby::Tracks::Points.new(@track).points.map do |point|
-          [point[:fl_time_abs], point[:elevation]]
+        points = 
+          Point.where(tracksegment_id: @track.tracksegments.pluck(:id))
+               .where('round(gps_time_in_seconds) = gps_time_in_seconds')
+               .order(:gps_time_in_seconds)
+               .pluck_to_hash(:gps_time_in_seconds,
+                              :elevation,
+                              :abs_altitude)
+
+        msl_offset =
+          if @track.ground_level && @track.ground_level > 0
+            @track.ground_level
+          elsif @track.place_msl
+            @track.place_msl
+          else
+            points.map { |x| x[:abs_altitude] }.min
+          end
+
+        has_abs_altitude = @track.ge_enabled
+        min_gps_time = points.first[:gps_time_in_seconds]
+
+        @points = points.map do |point|
+          [
+            (point[:gps_time_in_seconds] - min_gps_time).to_i, 
+            has_abs_altitude ? point[:abs_altitude] - msl_offset : point[:elevation]
+          ]
         end
+        # @points = Skyderby::Tracks::Points.new(@track).points.map do |point|
+          # [point[:fl_time_abs], point[:elevation]]
+        # end
       end
     end
   end
