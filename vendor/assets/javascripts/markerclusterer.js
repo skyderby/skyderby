020c97d1eebdb7154ff1671076277dd8c41e0a6a
Aleksandr Kunin <ab@ipkunin.ru>
2016-08-03 17:11:50 +0300

    fixed errors with vendor images. moved to public

--- vendor/assets/javascripts/markerclusterer.js
+++ vendor/assets/javascripts/markerclusterer.js
@@ -190,7 +190,7 @@ function MarkerClusterer(map, opt_markers, opt_options) {
  * @type {string}
  * @private
  */
-MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../assets/markerclusterer/m';
+MarkerClusterer.prototype.MARKER_CLUSTER_IMAGE_PATH_ = '../markerclusterer/m';
 
 
 /**
