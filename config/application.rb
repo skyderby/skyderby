c6efadd17c8a2853f029b4e519cf0be43dcbc7cd
Aleksandr Kunin <ab@ipkunin.ru>
2016-08-03 17:11:50 +0300

    fixed errors with vendor images. moved to public

--- config/application.rb
+++ config/application.rb
@@ -16,9 +16,7 @@ module Skyderby
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
 
-    config.assets.paths << "#{Rails}/vendor/assets/images"
-
-    # The default locale is :ru and all translations from config/locales/**/*.rb,yml are auto loaded.
+    # The default locale is :en and all translations from config/locales/**/*.rb,yml are auto loaded.
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.available_locales = [:en, :ru, :de, :es]
     config.i18n.default_locale = :en
