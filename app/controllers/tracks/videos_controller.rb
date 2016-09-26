97bd865dcc08dfa4ac12d185f6670193f0407efa
Aleksandr Kunin <ab@ipkunin.ru>
2016-09-26 14:59:51 +0300

    added chart, improved layout

--- app/controllers/tracks/videos_controller.rb
+++ app/controllers/tracks/videos_controller.rb
@@ -3,7 +3,17 @@ class Tracks::VideosController < ApplicationController
   before_filter :authorize_track
 
   def new
-		@video = TrackVideo.new
+		@video = TrackVideo.new(track_offset: @track.ff_start)
+    start_time_in_seconds = @track.points.first.gps_time_in_seconds.to_f
+    @points = 
+        @track.points.trimmed(seconds_before_start: 20).freq_1Hz.pluck_to_hash(
+          "gps_time_in_seconds - #{start_time_in_seconds} AS fl_time",
+          "#{@track.point_altitude_field} AS altitude",
+          :h_speed,
+          :v_speed,
+          'CASE WHEN v_speed = 0 THEN h_speed / 0.1
+                ELSE h_speed / ABS(v_speed)
+          END AS glide_ratio')
   end
 
   def show
