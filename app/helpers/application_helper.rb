a3be01c65d272eb9051524c7e681d1cf7c02ca76
Aleksandr Kunin <ab@ipkunin.ru>
2016-12-01 11:32:25 +0300

    whitelist request params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,7 +21,7 @@ module ApplicationHelper
   def lang_menu
     content_tag(:ul, class: ('dropdown-menu'), role: ('menu')) do
       I18n.available_locales.each do |loc|
-        locale_param = request.path == root_path ? root_path(locale: loc) : params.merge(locale: loc)
+        locale_param = request.path == root_path ? root_path(locale: loc) : params.to_h.merge(locale: loc)
         concat content_tag(:li, (link_to lang_presentation(loc), locale_param), class: (I18n.locale == loc ? 'active' : ''))
       end
     end
