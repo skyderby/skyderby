f92de2bfdc1883e952e721b6eaa16a5ca399b2a8
Aleksandr Kunin <ab@ipkunin.ru>
2016-12-06 13:14:09 +0300

    fix for eager loading lib folder

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  config.eager_load_paths << "#{Rails.root}/lib"
+  config.eager_load_paths += ["#{Rails.root}/lib"]
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
