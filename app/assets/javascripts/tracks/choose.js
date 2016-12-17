6393551bc52f7994a06006b6293e938cfa31a495
Aleksandr Kunin <skyksandr@gmail.com>
2016-12-17 14:36:16 +0300

    cleanup unused file

--- app/assets/javascripts/tracks/choose.js
+++
@@ -1,9 +0,0 @@
-$(document).on('ready page:load', function() {
-    $('.track-segment-row')
-        .off('click')
-        .on('click', function(e) {
-            e.preventDefault();
-            $('#track_track_index').val($(this).data('index'));
-            $('#select-segment-form').submit();
-        });
-});
