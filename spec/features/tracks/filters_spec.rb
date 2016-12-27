3165bba645104715c469b7230056f7ecb8d50423
Aleksandr Kunin <skyksandr@gmail.com>
2016-12-27 11:36:17 +0300

    fix filter spec

--- spec/features/tracks/filters_spec.rb
+++ spec/features/tracks/filters_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'Filtering tracks list', js: true do
   scenario 'Filter by suit' do
-    3.times do |x| 
+    3.times do |x|
       wingsuit = create :wingsuit, name: "suit-#{x}"
       create :empty_track, wingsuit: wingsuit
     end
@@ -10,10 +10,10 @@ feature 'Filtering tracks list', js: true do
 
     find('#select2-query_wingsuit_id-container').click
     sleep 0.5
-    first("li.select2-results__option", text: 'suit-1').click
+    first('li.select2-results__option', text: 'suit-1').click
     sleep 0.5
 
-    expect(page.all('#tracks-table > tbody > tr').count).to eq(1)
-    expect(page).to have_css('#tracks-table > tbody > tr > td', text: 'suit-1')
+    expect(page.all('#tracks-table > .tbody > .tr').count).to eq(1)
+    expect(page).to have_css('#tracks-table > .tbody > .tr > .td', text: 'suit-1')
   end
 end
