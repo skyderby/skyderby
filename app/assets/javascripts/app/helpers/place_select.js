3fc1bcea5c03351467b9f49168a589e6e91a47f7
Александр Кунин <aleksandrkunin@valentinakunina.croc.ru>
2016-12-29 13:08:30 +0300

    refactor places select

--- app/assets/javascripts/app/helpers/place_select.js
+++ app/assets/javascripts/app/helpers/place_select.js
@@ -1,46 +1,15 @@
 Skyderby.helpers.PlaceSelect = function(elem, opts) {
-
     var options = {
         theme: 'bootstrap',
         width: '100%',
         placeholder: I18n.t('events.show.place_placeholder'),
         allowClear: true,
         ajax: {
-            url: '/places',
+            url: '/places/select_options',
             dataType: 'json',
             type: "GET",
             quietMillis: 50,
-            data: function (term) {
-                return {
-                    query: term
-                };
-            },
-            processResults: function (data) {
-                var places_data = _.chain(data)
-                    .map(function(obj) {
-                        return {
-                            id: obj.id,
-                            text: obj.name,
-                            country: obj.country.name
-                        };
-                    })
-                    .groupBy(function(obj) { 
-                        return obj.country;
-                    })
-                    .map(function(obj, key) {
-                        return {
-                            text: key, 
-                            children: obj
-                        };
-                    })
-                    .sortBy(function(obj) {
-                        return obj.text;
-                    })
-                    .value();
-                return {
-                    results: places_data
-                };
-            },
+            data: function (term) { return { query: term }; },
             cache: true
         }
     };
