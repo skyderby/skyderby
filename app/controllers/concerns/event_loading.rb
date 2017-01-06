54d4503707ae1120068f773cfda31e37a82b0eeb
Александр Кунин <aleksandrkunin@Valentina-Kunina.local>
2017-01-06 18:59:56 +0300

    comp track upload progress and highlighting changed row

--- app/controllers/concerns/event_loading.rb
+++ app/controllers/concerns/event_loading.rb
@@ -12,23 +12,31 @@ module EventLoading
 
   def respond_with_errors(errors)
     respond_to do |format|
-      format.js { render template: 'errors/ajax_errors', locals: {errors: errors} }
-      format.json { render json: errors, status: :unprocessible_entry }
+      format.js do
+        render template: 'errors/ajax_errors',
+               locals: { errors: errors },
+               status: :unprocessable_entity
+      end
+      format.json { render json: errors, status: :unprocessable_entity }
     end
   end
 
   def create_scoreboard(event_id, display_raw_results)
-    load_event(params[:event_id])
-    @scoreboard = Events::ScoreboardFactory.new(@event, @display_raw_results).create 
+    load_event(event_id)
+    @scoreboard = Events::ScoreboardFactory.new(@event, display_raw_results).create
   end
 
   def load_event(event_id)
     @event = Event.includes(
       :rounds,
-      sections: [:event_tracks, 
-                 {competitors: [:wingsuit, 
-                               {profile: :country}, 
-                               {event_tracks: {round: :event_tracks}}]}]
+      sections: [
+        :event_tracks,
+        { competitors: [
+          :wingsuit,
+          { profile: :country },
+          { event_tracks: { round: :event_tracks } }
+        ] }
+      ]
     ).find(event_id)
   end
 
