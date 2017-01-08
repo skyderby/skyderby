6242e3f3405cbcce5898482fe80476c1d88ddc28
Aleksandr Kunin <skyksandr@gmail.com>
2017-01-08 13:41:40 +0300

    implemented grads dataset and parser

---
+++ app/services/gfs_grads_fetcher.rb
@@ -0,0 +1,74 @@
+require 'net/http'
+
+# Fetching parameters
+#
+# hgtsfc  ** surface geopotential height [gpm]
+#  - request params [time = 0..120][lat = 0..720][lon = 0..1439]
+#
+# hgtprs  ** (1000 975 950 925 900.. 7 5 3 2 1) geopotential height [gpm]
+# ugrdprs ** (1000 975 950 925 900.. 7 5 3 2 1) u-component of wind [m/s]
+# vgrdprs ** (1000 975 950 925 900.. 7 5 3 2 1) v-component of wind [m/s]
+#  - request params [time = 0..120][lev = 0..30][lat = 0..720][lon = 0..1439]
+#
+# Longitude: 0.00000000000°E to 359.75000000000°E (1440 points, avg. res. 0.25°)
+# Latitude: -90.00000000000°N to 90.00000000000°N (721 points, avg. res. 0.25°)
+# Altitude:  1000.00000000000 to 1.00000000000 (31 points, avg. res. 33.3)
+# Time: 00Z03JAN2017 to 00Z08JAN2017 (121 points, avg. res. 0.042 days)
+# Time resolution ~ 1h (1.008h)
+#
+class GfsGradsFetcher
+  LEVELS = '0:20'.freeze
+  LAT_LON_RESOLUTION = 0.25
+
+  VALUES = [:hgtsfc, :hgtprs, :ugrdprs, :vgrdprs].freeze
+
+  def initialize(opts = {})
+    @latitude = opts[:latitude]
+    @longitude = opts[:longitude]
+    @date_time = opts[:date_time] || Time.now.utc
+
+    @dataset = Dataset.for(date_time)
+  end
+
+  def execute
+    threads =
+      VALUES.map do |key|
+        url = "#{dataset.url}?#{key}#{value_params(key)}"
+        Thread.new do
+          Hash[key, Net::HTTP.get(URI.parse(url))]
+        end
+      end
+
+    threads.each(&:join).map(&:value).reduce({}, :merge)
+  end
+
+  private
+
+  def value_params(val_key)
+    val_params = {
+      hgtsfc: -> { "#{time_param}#{latitude_param}#{longitude_param}" },
+      default: -> { "#{time_param}#{altitude_param}#{latitude_param}#{longitude_param}" }
+    }
+
+    (val_params[val_key] || val_params[:default]).call
+  end
+
+  def latitude_param
+    start_latitude = -90
+    "[#{((latitude - start_latitude) / LAT_LON_RESOLUTION).round}]"
+  end
+
+  def longitude_param
+    "[#{(longitude / LAT_LON_RESOLUTION).round}]"
+  end
+
+  def altitude_param
+    "[#{LEVELS}]"
+  end
+
+  def time_param
+    "[#{((date_time.to_time - dataset.start_time.to_time) / 1.hour).to_i}]"
+  end
+
+  attr_reader :latitude, :longitude, :date_time, :dataset
+end
---
+++ app/services/gfs_grads_fetcher/dataset.rb
@@ -0,0 +1,67 @@
+class GfsGradsFetcher
+  class Dataset
+    DATASETS_INTERVAL = 6
+    DATASETS_HISTORY_DAYS = 5
+    DAYS_IN_DATASET = 5
+    SERVICE_URL = 'http://nomads.ncep.noaa.gov/dods'.freeze
+
+    class DateOutOfRange      < StandardError; end
+    class NoDatasetAvailable  < StandardError; end
+    class UnknownAvailability < StandardError; end
+
+    def initialize(opts = {})
+      @date_time = opts[:date_time]
+    end
+
+    def available?
+      response = Net::HTTP.get(URI.parse(url(ext: 'info')))
+
+      if response.include? "#{name} is not an available dataset"
+        false
+      elsif response.include? "GrADS Data Server - info for /#{name}"
+        true
+      else
+        raise UnknownAvailability
+      end
+    end
+
+    def url(ext: 'asc')
+      "#{SERVICE_URL}/#{name}.#{ext}"
+    end
+
+    def name
+      # date in url example - 20170228
+      date_param = date_time.strftime('%Y%m%d')
+      "gfs_0p25_1hr/gfs#{date_param}/gfs_0p25_1hr_#{time_offset}z"
+    end
+
+    def start_time
+      Time.zone.parse("#{date_time.strftime('%Y-%m-%d')}T#{time_offset}:00:00")
+    end
+
+    def self.for(date_time)
+      today = DateTime.current.to_date
+      date_range = (today - DATASETS_HISTORY_DAYS)..(today + DAYS_IN_DATASET)
+      raise DateOutOfRange unless date_range.cover? date_time.to_date
+
+      dataset_date_time = date_time > DateTime.current ? DateTime.current : date_time
+      dataset = new(date_time: dataset_date_time)
+      return dataset if dataset.available?
+
+      dataset = new(date_time: dataset_date_time - DATASETS_INTERVAL.hours)
+      return dataset if dataset.available?
+
+      raise NoDatasetAvailable
+    end
+
+    private
+
+    # time_offset values - 00, 06, 12, 18
+    def time_offset
+      current_hour = date_time.hour
+      format('%02d', current_hour - current_hour % DATASETS_INTERVAL)
+    end
+
+    attr_reader :date_time
+  end
+end
