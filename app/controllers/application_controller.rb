3aab480d287f846e9e2310538c0c90b26b2c6f35
Aleksandr Kunin <skyksandr@gmail.com>
2017-02-17 12:19:39 +0300

    fixed protections from CSRF prepend issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, prepend: true
   before_action :set_locale
   before_action :store_current_location, unless: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
