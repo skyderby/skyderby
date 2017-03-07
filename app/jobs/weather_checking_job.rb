9a818e6465fd71aec198acb43af15d9435efa7c5
Aleksandr Kunin <skyksandr@gmail.com>
2017-03-07 17:55:47 +0300

    fixed #70

--- app/jobs/weather_checking_job.rb
+++ app/jobs/weather_checking_job.rb
@@ -3,7 +3,8 @@ class WeatherCheckingJob < ApplicationJob
     track = Track.find_by(id: track_id)
     return if !track || track.base? || !track&.place
 
-    gps_time = track.points.trimmed.first.gps_time
+    gps_time = track.points.trimmed.first&.gps_time
+    return unless gps_time
 
     PlaceWeatherService.new(place: track.place, date_time: gps_time).execute
   end
