2f3cf2f4ecb4c43cfff67ab85bbed8b3cad00cca
Aleksandr Kunin <skyksandr@gmail.com>
2017-06-26 15:37:17 +0300

    fixed error with merge params

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,9 +20,8 @@ module ApplicationHelper
 
   def lang_menu
     content_tag(:ul, class: 'dropdown-menu dropdown-menu-right', role: 'menu') do
-      I18n.available_locales.each do |loc|
-        locale_param = request.path == root_path ? root_path(locale: loc) : params.to_h.merge(locale: loc)
-        concat content_tag(:li, (link_to lang_presentation(loc), locale_param), class: (I18n.locale == loc ? 'active' : ''))
+      I18n.available_locales.each do |locale_code|
+        concat content_tag(:li, link_to(lang_presentation(locale_code), locale: locale_code), class: (I18n.locale == locale_code ? 'active' : ''))
       end
     end
   end
