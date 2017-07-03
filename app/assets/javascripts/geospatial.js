9dd09c20067b748aa9139f9c22762d3dc87d6576
Aleksandr Kunin <skyksandr@gmail.com>
2017-07-03 12:44:25 +0300

    fixed bearing calculations

--- app/assets/javascripts/geospatial.js
+++ app/assets/javascripts/geospatial.js
@@ -9,9 +9,10 @@ Geospatial = {
     return n * (180 / Math.PI);
   },
 
-  destiantion_by_bearing_and_distance: function(latitude_deg, longitude_deg, bearing, distance) {
+  destiantion_by_bearing_and_distance: function(latitude_deg, longitude_deg, bearing_deg, distance) {
     var start_lat = this.radians(latitude_deg),
-        start_lon = this.radians(longitude_deg);
+        start_lon = this.radians(longitude_deg),
+        bearing = this.radians(bearing_deg);
 
     var dest_lat = Math.asin( 
       Math.sin(start_lat) * Math.cos(distance / this.EARTH_RADIUS) +
@@ -48,4 +49,20 @@ Geospatial = {
     return (this.degrees(Math.atan2(d_lon, d_phi)) + 360.0) % 360.0;
   },
 
+  distance: function(lat1_deg, lon1_deg, lat2_deg, lon2_deg) {
+    var lat1 = this.radians(lat1_deg),
+        lat2 = this.radians(lat2_deg),
+        lon1 = this.radians(lon1_deg),
+        lon2 = this.radians(lon2_deg);
+
+    var d_lat = lat2 - lat1;
+    var d_lon = lon2 - lon1;
+
+    var a = Math.sin(d_lat / 2) * Math.sin(d_lat / 2) +
+            Math.cos(lat1) * Math.cos(lat2) *
+            Math.sin(d_lon / 2) * Math.sin(d_lon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    return this.EARTH_RADIUS * c;
+  },
 };
