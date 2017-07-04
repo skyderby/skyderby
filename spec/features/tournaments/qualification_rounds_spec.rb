7e49fe578604db2cbfc29ad8cffd579549b75d6a
Aleksandr Kunin <skyksandr@gmail.com>
2017-07-04 11:36:41 +0300

    added test for qualification round deletion

--- spec/features/tournaments/qualification_rounds_spec.rb
+++ spec/features/tournaments/qualification_rounds_spec.rb
@@ -12,4 +12,23 @@ feature 'Qualification rounds', js: true do
 
     expect(page).to have_css('td', text: 'Round 1')
   end
+
+  scenario 'delete round' do
+    user = create :user
+    sign_in user
+
+    tournament = create :tournament,
+                        responsible: user.profile,
+                        has_qualification: true
+
+    create :qualification_round, tournament: tournament
+
+    visit tournament_qualification_path(tournament)
+
+    # open dropdown
+    find('.round-cell__actions .btn-link').click
+    click_button I18n.t('general.delete')
+
+    expect(page).to have_no_css('td', text: 'Round 1')
+  end
 end
