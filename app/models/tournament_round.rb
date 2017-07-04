edd4763db1af2fc23f67ed4e02a5b80bff5df354
Aleksandr Kunin <skyksandr@gmail.com>
2017-07-04 21:24:37 +0300

    ability to add tournament round

--- app/models/tournament_round.rb
+++ app/models/tournament_round.rb
@@ -12,4 +12,14 @@
 class TournamentRound < ApplicationRecord
   belongs_to :tournament
   has_many :matches, class_name: 'TournamentMatch'
+
+  before_create :set_order
+
+  def set_order
+    self.order = max_order_within_event + 1
+  end
+
+  def max_order_within_event
+    TournamentRound.where(tournament_id: tournament_id).maximum(:order) || 0
+  end
 end
