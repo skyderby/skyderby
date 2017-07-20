117b2771817c4109677bcb800c9c43cd3f8d8ef8
Aleksandr Kunin <skyksandr@gmail.com>
2017-07-20 11:17:01 +0300

    added reset password mail, cleanup

--- lib/mailer_previews/devise_mailer_preview.rb
+++ lib/mailer_previews/devise_mailer_preview.rb
@@ -6,8 +6,4 @@ class DeviseMailerPreview < ActionMailer::Preview
   def reset_password_instructions
     DeviseMailer.reset_password_instructions(User.first, 'faketoken', {})
   end
-
-  def unlock_instructions
-    DeviseMailer.unlock_instructions(User.first, 'faketoken', {})
-  end
 end
