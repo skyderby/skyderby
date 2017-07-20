57f4482bb971db005a4249d49ecb65f8c0ad66e0
Aleksandr Kunin <skyksandr@gmail.com>
2017-07-20 14:05:08 +0300

    added mailer host options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'skyderby.ru' }
+  config.action_mailer.asset_host = ENV['MAILER_ASSET_HOST']
+  config.action_mailer.default_url_options = { host: ENV['MAILER_URL_HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: ENV['SMTP_ADDRESS'],
