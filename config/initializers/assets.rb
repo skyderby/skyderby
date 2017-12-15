4d5ca09aaed0367ca0522e17cc4c746b9882c8f2
Aleksandr Kunin <skyksandr@gmail.com>
2017-12-15 15:22:39 +0300

    added achievement icons font

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,8 @@ Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path
 Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'images')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w[.svg .eot .woff .ttf]
