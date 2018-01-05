dfda5a74521e1aad4a916fd753f00978214966c8
Aleksandr Kunin <skyksandr@gmail.com>
2018-01-06 01:32:54 +0300

    Improved places helper spec

---
+++ spec/helpers/places_helper_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe PlacesHelper do
+  it 'returns place presentation with country code' do
+    country = create :country, name: 'Norway', code: 'NOR'
+    place = create :place, name: 'Gridset', country: country
+
+    expect(helper.place_presentation(place)).to eq(
+      '<span>Gridset</span> (<span class="text-warning" data-toggle="tooltip" title="Norway">NOR</span>)'
+    )
+  end
+end
