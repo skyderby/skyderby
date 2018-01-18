b425c452a66c1265ba123338a4c7ddb29402d663
Aleksandr Kunin <skyksandr@gmail.com>
2018-01-18 10:44:55 +0300

    refactoring: extract conditions into methods

--- app/models/tracks/base_presenter.rb
+++ app/models/tracks/base_presenter.rb
@@ -137,13 +137,23 @@ module Tracks
       @weather_data ||=
         if track.weather_data.any?
           track.weather_data
-        elsif track.competitive? && track.event.weather_data.for_time(start_time).any?
+        elsif weather_data_from_competition?(start_time)
           track.event.weather_data.for_time(start_time)
-        elsif track.place && track.place.weather_data.for_time(start_time).any?
+        elsif weather_data_from_place?(start_time)
           track.place.weather_data.for_time(start_time)
         else
           []
         end
+    end
+
+    def weather_data_from_competition?(start_time)
+      return false unless track.competitive?
+      track.event.weather_data.for_time(start_time).any?
+    end
+
+    def weather_data_from_place?(start_time)
+      return false unless track.place
+      track.place.weather_data.for_time(start_time).any?
     end
 
     def zerowind_points
