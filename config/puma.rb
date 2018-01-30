7b668be762136671409d1e9f489f11d1d867466b
Aleksandr Kunin <skyksandr@gmail.com>
2018-01-30 20:23:56 +0300

    puma threads = 1 for test

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 workers ENV.fetch('WEB_CONCURRENCY') { 0 }.to_i
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+
+default_threads_count = Rails.env.test? ? 1 : 5
+threads_count = ENV.fetch('RAILS_MAX_THREADS') { default_threads_count }.to_i
 threads threads_count, threads_count
 
 preload_app!
