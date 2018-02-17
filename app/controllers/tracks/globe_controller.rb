add3b73d55cc5ef582fcb63a8fa07373368e6d43
Aleksandr Kunin <skyksandr@gmail.com>
2018-02-17 10:40:09 +0300

    fix syntax error

--- app/controllers/tracks/globe_controller.rb
+++ app/controllers/tracks/globe_controller.rb
@@ -1,17 +1,18 @@
 module Tracks
   class GlobeController < ApplicationController
-  before_action :authorize_track
+    before_action :authorize_track
 
-  def show
-    @track = Track.find(params[:track_id])
+    def show
+      @track = Track.find(params[:track_id])
 
-    authorize @track
+      authorize @track
 
-    respond_to do |format|
-      format.html { redirect_to @track unless @track.ge_enabled }
-      format.json { @globe_data = Tracks::GlobePresenter.new(@track) }
+      respond_to do |format|
+        format.html { redirect_to @track unless @track.ge_enabled }
+        format.json { @globe_data = Tracks::GlobePresenter.new(@track) }
+      end
+    rescue Pundit::NotAuthorizedError
+      redirect_to tracks_url, notice: t('tracks.index.track_not_found', id: params[:track_id])
     end
-  rescue Pundit::NotAuthorizedError
-    redirect_to tracks_url, notice: t('tracks.index.track_not_found', id: params[:track_id])
   end
 end
