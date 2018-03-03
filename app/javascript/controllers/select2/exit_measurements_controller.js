a5a8ac09d4685dc492449a6389f571c857268211
Aleksandr Kunin <skyksandr@gmail.com>
2018-03-03 15:40:23 +0300

    select2--exit-measurements

---
+++ app/javascript/controllers/select2/exit_measurements_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from 'stimulus'
+import 'select2'
+import { fix_open_on_clear } from 'utils/select2/fix_open_on_clear'
+import common_options from 'utils/select2/common_options'
+
+export default class extends Controller {
+  connect() {
+    var options = Object.assign(common_options, {
+      placeholder: $(this.element).attr('placeholder'),
+      ajax: {
+        url: '/api/v1/places/exit_measurements',
+        cache: true
+      }
+    })
+
+    const $element = $(this.element)
+    fix_open_on_clear($element);
+    $element.select2(options);
+
+    $(document).one('turbolinks:before-cache', this.teardown.bind(this))
+  }
+
+  teardown() {
+    const $element = $(this.element)
+    if ($element.next().is('.select2')) $element.select2('destroy')
+  }
+}
