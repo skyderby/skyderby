bdf32f68379021c8e38148502912498c420e355a
Aleksandr Kunin <skyksandr@gmail.com>
2018-03-16 11:14:07 +0300

    add footer, locale menu and some js

--- app/javascript/controllers/mobile/header_controller.js
+++ app/javascript/controllers/mobile/header_controller.js
@@ -1,11 +1,13 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ 'menu', 'overlay' ]
+  static targets = [ 'menu', 'overlay', 'locale_menu' ]
 
   connect() {
     let menu_width = this.menu.offsetWidth
     this.menu.style.right = `${-menu_width}px`
+
+    document.addEventListener('turbolinks:before-cache', this.close_menu.bind(this), {once: true})
   }
 
   open_menu() {
@@ -14,8 +16,13 @@ export default class extends Controller {
   }
 
   close_menu() {
+    console.log(111)
     this.menu.classList.remove('active')
     this.overlay.classList.add('overlay--hidden')
+  }
+
+  toggle_locale_menu() {
+    this.locale_menu.classList.toggle('active')
   }
 
   get menu() {
@@ -24,5 +31,9 @@ export default class extends Controller {
 
   get overlay() {
     return this.overlayTarget
+  }
+
+  get locale_menu() {
+    return this.locale_menuTarget
   }
 }
