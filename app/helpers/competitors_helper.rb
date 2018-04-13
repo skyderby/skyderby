16d08f89803075f036b54ca4324d18f5b8825719
Aleksandr Kunin <skyksandr@gmail.com>
2018-04-13 10:26:02 +0300

    small refactoring, reorder add round buttons

--- app/helpers/competitors_helper.rb
+++ app/helpers/competitors_helper.rb
@@ -1,4 +1,14 @@
 module CompetitorsHelper
+  def new_competitor_link(event, display_raw_results)
+    link_to(new_event_competitor_path(event),
+            remote: true,
+            'data-params': { 'display_raw_results' => display_raw_results }.to_param,
+            class: 'btn btn-default') do
+      concat content_tag(:i, nil, class: 'fa fa-plus')
+      concat t('activerecord.models.competitor')
+    end
+  end
+
   def edit_competitor_link(event, competitor, display_raw_results)
     link_to content_tag(:i, nil, class: 'fa fa-pencil'),
             edit_event_competitor_path(event, competitor),
