8473b0915e06b9d89c8296caff857894ca502161
Aleksandr Kunin <skyksandr@gmail.com>
2018-05-29 11:52:37 +0300

    remove duplication

--- app/models/tracks/wind_cancellation.rb
+++ app/models/tracks/wind_cancellation.rb
@@ -5,13 +5,7 @@ module Tracks
     end
 
     def weather_data
-      min_gps_time = points.first&.fetch(:gps_time) || track.recorded_at
-      return [] if min_gps_time.blank? || track.place.blank?
-
-      @weather_data ||= begin
-        start_time = min_gps_time.beginning_of_hour
-        track.place.weather_data.for_time(start_time)
-      end
+      @weather_data ||= track.weather_data
     end
 
     def zerowind_points
