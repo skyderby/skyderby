3547294782b468747437f54ab3cd0455438b635c
Aleksandr Kunin <skyksandr@gmail.com>
2018-06-25 21:46:16 +0300

    fix flight profiles caching issue

--- app/controllers/flight_profiles_controller.rb
+++ app/controllers/flight_profiles_controller.rb
@@ -3,7 +3,7 @@ class FlightProfilesController < ApplicationController
 
   def show
     authorize @scope
-    fresh_when etags_for(@scope)
+    fresh_when etags_for(scope_tracks)
 
     @tracks = scope_tracks.paginate(per_page: 25, page: 1)
   end
