e9db931e124029ab7bf73d9faa15dca032a86d26
Aleksandr Kunin <skyksandr@gmail.com>
2018-08-12 17:50:32 +0300

    Add place validation, bugfixes, cleanup

--- app/javascript/controllers/events/form_controller.js
+++ app/javascript/controllers/events/form_controller.js
@@ -1,7 +1,33 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
+  static targets = [ 'only_hungary_boogie' ]
+
   connect() {
+    this.set_form_validation()
+
+    let rules = this.element.querySelector('[name="event[rules]"]:checked').value
+    this.set_fields_visibility(rules)
+  }
+
+  on_change_rules(event) {
+    if (event.currentTarget.hasAttribute('disabled')) {
+      event.stopPropagation()
+      return
+    }
+
+    let rules = event.currentTarget.querySelector('input').value
+    this.set_fields_visibility(rules)
+  }
+
+  set_fields_visibility(rules) {
+    let visibility = rules === 'hungary_boogie' ? 'block' : 'none'
+    this.only_hungary_boogieTargets.forEach( (element) => {
+      element.style.display = visibility
+    })
+  }
+
+  set_form_validation() {
     $(this.element).validate({
       ignore: 'input[type=hidden]',
       rules: {
@@ -15,6 +41,9 @@ export default class extends Controller {
         'event[range_to]': {
           required: true
         },
+        'event[place_id]': {
+          required: true
+        }
       },
       highlight: function(element) {
         $(element).closest('.form-group').addClass('has-error');
@@ -22,6 +51,13 @@ export default class extends Controller {
       unhighlight: function(element) {
         $(element).closest('.form-group').removeClass('has-error');
       },
+      errorPlacement: (error, element) => {
+        if (element.attr('name') == 'event[place_id]') {
+          error.appendTo(element.closest('div'))
+        } else {
+          error.insertAfter(element)
+        }
+      }
     });
   }
 }
