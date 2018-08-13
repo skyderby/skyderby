f083b2e8519dfd4934969d5204fd030ae2409e51
Aleksandr Kunin <skyksandr@gmail.com>
2018-08-13 15:48:01 +0300

    wip

---
+++ app/helpers/events/sections_helper.rb
@@ -0,0 +1,53 @@
+module Events
+  module SectionsHelper
+    def new_section_link(event)
+      link_to(new_event_section_path(event_id: event.id),
+              remote: true,
+              'data-params': display_event_params.to_param,
+              class: 'btn btn-default') do
+        concat content_tag(:i, nil, class: 'fa fa-plus text-muted')
+        concat ' '
+        concat t('activerecord.models.event/section')
+      end
+    end
+
+    def edit_section_link(event, section)
+      link_to content_tag(:i, nil, class: 'fa fa-pencil text-muted'),
+              edit_event_section_path(event, section),
+              remote: true,
+              'data-params': display_event_params.to_param,
+              class: 'section__control'
+    end
+
+    def move_section_upper_button(event, section)
+      button_to(move_upper_event_section_path(event, section),
+                remote: true,
+                params: display_event_params,
+                method: :patch,
+                class: 'btn-link section__control') do
+        content_tag(:i, nil, class: 'fa fa-chevron-up text-muted')
+      end
+    end
+
+    def move_section_lower_button(event, section)
+      button_to(move_lower_event_section_path(event, section),
+                remote: true,
+                params: display_event_params,
+                method: :patch,
+                class: 'btn-link section__control') do
+        content_tag(:i, nil, class: 'fa fa-chevron-down text-muted')
+      end
+    end
+
+    def delete_section_button(event, section)
+      button_to(event_section_path(event, section),
+                method: :delete,
+                params: display_event_params,
+                confirm: t('sections.show.delete_confirmation'),
+                class: 'btn-link section__control',
+                remote: true) do
+        content_tag(:i, nil, class: 'fa fa-times text-muted')
+      end
+    end
+  end
+end
--- spec/system/competitions/create_competition_spec.rb
+++ spec/system/competitions/create_competition_spec.rb
@@ -2,22 +2,20 @@ require 'spec_helper'
 
 feature 'Creating new competitions', type: :system do
   scenario 'Registered user able to create new competition', js: true do
-    user = create :user
-    sign_in user
+    sign_in users(:regular_user)
     visit events_path
-    click_link 'Competition'
 
-    sleep 0.5
+    click_link 'Competition'
+    expect(page).to have_css('.modal-title', text: "#{I18n.t('activerecord.models.event')}: New")
 
     within '#new_event_form' do
       fill_in :event_name, with: 'Test event'
       fill_in :event_range_from, with: 3000
       fill_in :event_range_to, with: 2000
+      select2 places(:gridset), from: 'place_id'
 
       find('input[type="submit"]').click
     end
-
-    sleep 0.5
 
     expect(page).to have_css('#event-header h1 span', text: 'Test event')
   end
