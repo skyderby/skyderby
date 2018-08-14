30bdaa79ac8de04ba0fbdb1ad4d6ec83c828c1bc
Aleksandr Kunin <skyksandr@gmail.com>
2018-08-14 15:20:53 +0300

    refactor shared tooltip to stimulus

---
+++ app/javascript/controllers/synchronized_charts_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = [ 'chart' ]
+
+  refresh_tooltip(event) {
+    this.charts.forEach( (chart) => { this.draw_tooltip(event, chart) })
+  }
+
+  draw_tooltip(event, chart) {
+    if (!chart) return
+
+    let normalized_event = chart.pointer.normalize(event)
+    let points = this.hovered_points(normalized_event, chart)
+
+    if (points.length > 0) {
+      points[0].onMouseOver()
+      chart.tooltip.refresh(points)
+      chart.xAxis[0].drawCrosshair(normalized_event, points[0])
+    }
+  }
+
+  hovered_points(event, chart) {
+    return chart.series.reduce((result, series) => {
+      if (!series.visible || series.enableMouseTracking === false) return result
+
+      let point = series.searchPoint(event, true)
+      if (point) result.push(point)
+
+      return result
+    }, [])
+  }
+
+  get charts() {
+    return this.chartTargets.map( (el) => {
+      return Highcharts.charts.find( (chart) => { return chart.renderTo === el } )
+    })
+  }
+}
