10e3459bf471d96d1d77e62f69f95d3b86c53289
Aleksandr Kunin <skyksandr@gmail.com>
2018-08-23 11:20:14 +0300

    cleanup, rewrite range selector to stimulus

--- app/assets/javascripts/app/helpers/init_youtube_api.js
+++
@@ -1,7 +0,0 @@
-Skyderby.helpers.init_youtube_api = function() {
-    if (window.Skyderby.youtube_api_ready) {
-        window.Skyderby.trigger('youtube_api_ready');
-    } else {
-        Skyderby.helpers.load_script('https://www.youtube.com/iframe_api');
-    }
-};
