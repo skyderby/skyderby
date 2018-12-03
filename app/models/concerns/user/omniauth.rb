5b189ed682e6b0265573e9a7b2532bb912c86a09
Aleksandr Kunin <skyksandr@gmail.com>
2018-12-03 11:45:27 +0300

    Lookup user by email first #23

--- app/models/concerns/user/omniauth.rb
+++ app/models/concerns/user/omniauth.rb
@@ -2,23 +2,30 @@ module User::Omniauth
   extend ActiveSupport::Concern
 
   def add_data_from_facebook(auth)
-    profile.userpic = URI.parse(auth.info.image).open unless profile.userpic.present?
-    self.provider = auth.provider
-    self.uid = auth.uid
+    profile.userpic = URI.parse(auth.info.image).open if profile.userpic.blank?
+    assign_attributes(provider: auth.provider, uid: auth.uid)
   end
 
   class_methods do
     def from_omniauth(auth)
-      user = where(provider: auth.provider, uid: auth.uid).first_or_initialize do |user|
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0, 20]
-        user.skip_confirmation!
-      end
-      return user if user.persisted?
-      user.profile.name = [auth.info.first_name, auth.info.last_name].join(' ')
-      user.profile.userpic = URI.parse(auth.info.image).open
-      user.profile.save
-      user
+      search_keys = { provider: auth.provider, uid: auth.uid }
+
+      find_by(**search_keys) ||
+        find_by(email: auth.info.email).tap { |user| user&.update(**search_keys) } ||
+        create_from_omniauth(auth)
+    end
+
+    def create_from_omniauth(auth)
+      User.create! \
+        provider: auth.provider,
+        uid: auth.uid,
+        email: auth.info.email,
+        password: Devise.friendly_token[0, 20],
+        confirmed_at: Time.now.utc,
+        profile_attributes: {
+          name: [auth.info.first_name, auth.info.last_name].join(' '),
+          userpic: (URI.parse(auth.info.image).open if auth.info.image.present?)
+        }
     end
   end
 end
