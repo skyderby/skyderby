477b1289bebf7fe65ae7b2595d3ea505b2a0efe9
Aleksandr Kunin <skyksandr@gmail.com>
2019-01-30 10:18:28 +0300

    Unified devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,4 +12,14 @@ class ApplicationController < ActionController::Base
   include AuthorizationErrorHandling
 
   include RackMiniProfiler
+
+  layout :layout_by_resource
+
+  def layout_by_resource
+    if devise_controller?
+      'sessions'
+    else
+      'application'
+    end
+  end
 end
