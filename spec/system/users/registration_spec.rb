e2001a63c2e500fd6b896dc0afd83faa43828422
Aleksandr Kunin <skyksandr@gmail.com>
2019-07-03 23:51:19 +0200

    Explicitly specify resource in devise helpers

--- spec/system/users/registration_spec.rb
+++ spec/system/users/registration_spec.rb
@@ -2,12 +2,10 @@ describe 'User registration', type: :system, js: true do
   it 'register if all filled' do
     visit new_user_registration_path
 
-    within '#new_user' do
-      fill_in 'user[profile_attributes][name]', with: 'Ivan Ivanov'
-      fill_in 'user[email]', with: 'some@example.com'
-      fill_in 'user[password]', with: '123456'
-      fill_in 'user[password_confirmation]', with: '123456'
-    end
+    fill_in 'user[profile_attributes][name]', with: 'Ivan Ivanov'
+    fill_in 'user[email]', with: 'some@example.com'
+    fill_in 'user[password]', with: '123456'
+    fill_in 'user[password_confirmation]', with: '123456'
 
     expect { click_button I18n.t('devise.registrations.new.sign_up') }.to change { User.count }.by(1)
     expect(User.last.name).to eq('Ivan Ivanov')
