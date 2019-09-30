b531194380de91b4b5fcc578c0dda34e91e107fe
Aleksandr Kunin <skyksandr@gmail.com>
2019-09-30 11:58:57 +0300

    Make test locale independent

--- spec/helpers/qualification_jumps_helper_spec.rb
+++ spec/helpers/qualification_jumps_helper_spec.rb
@@ -2,7 +2,7 @@ describe QualificationJumpsHelper do
   it '#qualification_jump_presentation' do
     jump = qualification_jumps(:qualification_jump_1)
     expect(helper.qualification_jump_presentation(jump)).to eq(
-      'Result: John | Qualification - 1'
+      "#{I18n.t('activerecord.models.event/result')}: John | Qualification - 1"
     )
   end
 end
