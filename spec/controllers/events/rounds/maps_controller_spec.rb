87cccd051e2ab1b5a44d056d7bdf000d9fe254aa
Aleksandr Kunin <skyksandr@gmail.com>
2019-10-25 23:16:29 +0300

    Cleanup old implementation

--- spec/controllers/events/rounds/maps_controller_spec.rb
+++ spec/controllers/events/rounds/maps_controller_spec.rb
@@ -1,4 +1,4 @@
-describe Events::Rounds::GlobeController do
+describe Events::Rounds::MapsController do
   it '#show' do
     event = events(:published_public)
     round = event_rounds(:distance_round_1)
