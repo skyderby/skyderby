e330b455d0d2e8b37759b740cc6d4a023eacbd9e
Aleksandr Kunin <skyksandr@gmail.com>
2019-10-25 13:17:22 +0300

    Header, integration with redux devTools, update deps

--- app/javascript/components/ui/BackLink.js
+++ app/javascript/components/ui/BackLink.js
@@ -16,8 +16,13 @@ BackLink.propTypes = {
 export default styled(BackLink)`
   color: #555;
   border-radius: 4px;
+  font-family: 'Proxima Nova Regular';
   font-size: 24px;
   padding: 0px 12px 0px 10px;
+
+  i {
+    font-size: 21px;
+  }
 
   &:hover {
     background-color: #f3f3f3;
