8a3bb74a1acca4043fac28de86986501cdd10153
Aleksandr Kunin <skyksandr@gmail.com>
2019-10-26 00:03:36 +0300

    Make tests locale independent

--- spec/helpers/tournaments/matches/slots_helper_spec.rb
+++ spec/helpers/tournaments/matches/slots_helper_spec.rb
@@ -1,8 +1,6 @@
 describe Tournaments::Matches::SlotsHelper do
   it '#match_slot_presentation' do
     slot = tournament_match_slots(:slot_1)
-    expect(helper.match_slot_presentation(slot)).to eq(
-      'Result: John | Round - 1'
-    )
+    expect(helper.match_slot_presentation(slot)).to include('John | Round - 1')
   end
 end
