9535616d24922efc445e61f485c83fed85179070
Aleksandr Kunin <skyksandr@gmail.com>
2019-11-04 12:01:56 +0300

    Remove unused import

--- app/javascript/components/RoundReplay/index.js
+++ app/javascript/components/RoundReplay/index.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react'
-import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import { useDispatch, useSelector } from 'react-redux'
 
