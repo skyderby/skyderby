441b8b4586e48aebd39b8ffab021eab8d0e65231
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-02 15:41:42 +0300

    Introduce RTL, start covering components with tests

---
+++ app/javascript/utils/unitsConversion.js
@@ -0,0 +1,3 @@
+export const msToKmh = value => value * 3.6
+export const msToMph = value => value * 2.23694
+export const metersToFeet = value => value * 3.28084
---
+++ spec/javascript/components/TrackShow/Summary/Time.spec.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import Time from 'components/TrackShow/Summary/Time'
+
+describe('Summary/Time', () => {
+  it('shows time rounded to 1 digit', () => {
+    const { getByTestId } = render(<Time value={92.373} />)
+
+    expect(getByTestId('value')).toHaveTextContent('92.4')
+  })
+
+  it('shows placeholder if no value given', () => {
+    const { getByTestId } = render(<Time />)
+
+    expect(getByTestId('value')).toHaveTextContent('--.-')
+  })
+
+  it('shows placeholder in case of NaN', () => {
+    const { getByTestId } = render(<Time value={0 / 0} />)
+
+    expect(getByTestId('value')).toHaveTextContent('--.-')
+  })
+})
