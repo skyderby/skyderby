bfc8e7ef36d6ac67eb56eebf1fc132af32234008
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-06 09:52:19 +0300

    Improve JS linting, fix eslint errors

--- spec/javascript/redux/userPreferences/chartMode.spec.js
+++ spec/javascript/redux/userPreferences/chartMode.spec.js
@@ -3,19 +3,19 @@ import { UPDATE_PREFERENCES } from 'redux/userPreferences/actionTypes'
 
 describe('chartMode reducer', () => {
   it('when dispatched allowed value - update', () => {
-    const result = reducer(
-      undefined,
-      { type: UPDATE_PREFERENCES, payload: { chartMode: SINGLE_CHART } }
-    )
+    const result = reducer(undefined, {
+      type: UPDATE_PREFERENCES,
+      payload: { chartMode: SINGLE_CHART }
+    })
 
     expect(result).toEqual(SINGLE_CHART)
   })
 
   it('when dispatched invalid value - returns default', () => {
-    const result = reducer(
-      undefined,
-      { type: UPDATE_PREFERENCES, payload: { chartMode: 'INVALID' } }
-    )
+    const result = reducer(undefined, {
+      type: UPDATE_PREFERENCES,
+      payload: { chartMode: 'INVALID' }
+    })
 
     expect(result).toEqual(MULTI_CHART)
   })
--- spec/javascript/redux/userPreferences/unitSystem.spec.js
+++ spec/javascript/redux/userPreferences/unitSystem.spec.js
@@ -3,19 +3,19 @@ import { UPDATE_PREFERENCES } from 'redux/userPreferences/actionTypes'
 
 describe('chartMode reducer', () => {
   it('when dispatched allowed value - update', () => {
-    const result = reducer(
-      undefined,
-      { type: UPDATE_PREFERENCES, payload: { unitSystem: IMPERIAL } }
-    )
+    const result = reducer(undefined, {
+      type: UPDATE_PREFERENCES,
+      payload: { unitSystem: IMPERIAL }
+    })
 
     expect(result).toEqual(IMPERIAL)
   })
 
   it('when dispatched invalid value - returns default', () => {
-    const result = reducer(
-      undefined,
-      { type: UPDATE_PREFERENCES, payload: { unitSystem: 'INVALID' } }
-    )
+    const result = reducer(undefined, {
+      type: UPDATE_PREFERENCES,
+      payload: { unitSystem: 'INVALID' }
+    })
 
     expect(result).toEqual(METRIC)
   })
