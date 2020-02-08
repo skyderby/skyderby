0667102ec4e1360d7fb8daf394ae4414952a3f49
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-08 12:12:11 +0300

    Implement wind cancellation on frontend side

---
+++ spec/javascript/fixtures/windData.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    actualOn: 1484812800000,
+    altitude: 215.8984,
+    windSpeed: 5.9163,
+    windDirection: 47.33
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 447.6772,
+    windSpeed: 6.5275,
+    windDirection: 49.72
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 683.8254,
+    windSpeed: 7.1218,
+    windDirection: 51.84
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 924.5823,
+    windSpeed: 7.3885,
+    windDirection: 52.48
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 1420.7836,
+    windSpeed: 8.0781,
+    windDirection: 54.42
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 1938.4462,
+    windSpeed: 8.8711,
+    windDirection: 58.08
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 2482.2362,
+    windSpeed: 9.5716,
+    windDirection: 73.55
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 3058.6869,
+    windSpeed: 9.0293,
+    windDirection: 87.4
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 3671.2931,
+    windSpeed: 8.3454,
+    windDirection: 83.39
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 4325.4879,
+    windSpeed: 6.5091,
+    windDirection: 101.43
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 5025.4384,
+    windSpeed: 6.4661,
+    windDirection: 129.35
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 5779.9804,
+    windSpeed: 7.4895,
+    windDirection: 136.03
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 6599.9954,
+    windSpeed: 6.7227,
+    windDirection: 130.66
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 7498.1824,
+    windSpeed: 6.9029,
+    windDirection: 141.47
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 8492.5154,
+    windSpeed: 8.0362,
+    windDirection: 163.37
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 9606.5624,
+    windSpeed: 8.4149,
+    windDirection: 176.59
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 10871.5984,
+    windSpeed: 11.1018,
+    windDirection: 181.03
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 12342.5274,
+    windSpeed: 13.9904,
+    windDirection: 189.46
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 14118.9244,
+    windSpeed: 16.643,
+    windDirection: 172.75
+  },
+  {
+    actualOn: 1484812800000,
+    altitude: 16461.7474,
+    windSpeed: 7.6359,
+    windDirection: 107.06
+  }
+]
