b712b7bdda3ff35cf736a9e102476f02e2c5e2a9
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-08 16:28:24 +0300

    Finish wind cancellation in Summary. Improve tests.

--- spec/javascript/components/TrackShow/Summary/HorizontalSpeed.spec.js
+++ spec/javascript/components/TrackShow/Summary/HorizontalSpeed.spec.js
@@ -11,28 +11,46 @@ describe('Summary/VerticalSpeed', () => {
       return renderWithRedux(<HorizontalSpeed {...props} />, initialState)
     }
 
-    it('average value', () => {
-      const { getByTestId } = renderComponent({
+    it('hide wind effect if no zeroWindValue provided', () => {
+      const { queryByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[avg]')).toHaveTextContent('84')
+      expect(queryByLabelText('wind cancelled value')).not.toBeInTheDocument()
+    })
+
+    it('average value', () => {
+      const { getByLabelText } = renderComponent({
+        value: { avg: 23.373, min: 20, max: 30 }
+      })
+
+      expect(getByLabelText('average horizontal speed').textContent).toBe('84')
+    })
+
+    it('wind effect value', () => {
+      const { getByLabelText } = renderComponent({
+        value: { avg: 23.373, min: 20, max: 30 },
+        zeroWindValue: 20
+      })
+
+      expect(getByLabelText('wind cancelled value').textContent).toBe('72')
+      expect(getByLabelText('wind effect').textContent).toBe('+12')
     })
 
     it('max value', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[max]')).toHaveTextContent('108')
+      expect(getByLabelText('maximum horizontal speed').textContent).toBe('108')
     })
 
     it('min value', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[min]')).toHaveTextContent('72')
+      expect(getByLabelText('minimum horizontal speed').textContent).toBe('72')
     })
   })
 
@@ -43,27 +61,37 @@ describe('Summary/VerticalSpeed', () => {
     }
 
     it('average value', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[avg]')).toHaveTextContent('52')
+      expect(getByLabelText('average horizontal speed').textContent).toBe('52')
+    })
+
+    it('wind effect value', () => {
+      const { getByLabelText } = renderComponent({
+        value: { avg: 23.373, min: 20, max: 30 },
+        zeroWindValue: 20
+      })
+
+      expect(getByLabelText('wind cancelled value').textContent).toBe('45')
+      expect(getByLabelText('wind effect').textContent).toBe('+8')
     })
 
     it('max value', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[max]')).toHaveTextContent('67')
+      expect(getByLabelText('maximum horizontal speed').textContent).toBe('67')
     })
 
     it('min value', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[min]')).toHaveTextContent('45')
+      expect(getByLabelText('minimum horizontal speed').textContent).toBe('45')
     })
   })
 
@@ -74,27 +102,27 @@ describe('Summary/VerticalSpeed', () => {
     }
 
     it('average value - NaN', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 0 / 0, min: 20, max: 30 }
       })
 
-      expect(getByTestId('value[avg]')).toHaveTextContent('---')
+      expect(getByLabelText('average horizontal speed').textContent).toBe('---')
     })
 
     it('max value - undefined', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: 20, max: undefined }
       })
 
-      expect(getByTestId('value[max]')).toHaveTextContent('---')
+      expect(getByLabelText('maximum horizontal speed').textContent).toBe('---')
     })
 
     it('min value - null', () => {
-      const { getByTestId } = renderComponent({
+      const { getByLabelText } = renderComponent({
         value: { avg: 23.373, min: null, max: 30 }
       })
 
-      expect(getByTestId('value[min]')).toHaveTextContent('---')
+      expect(getByLabelText('minimum horizontal speed').textContent).toBe('---')
     })
   })
 })
