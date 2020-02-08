c5e4984ee1e2a07e480c73695a75dee2dea086d6
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-08 16:28:24 +0300

    Finish wind cancellation in Summary. Improve tests.

--- spec/javascript/components/TrackShow/Summary/Time.spec.js
+++ spec/javascript/components/TrackShow/Summary/Time.spec.js
@@ -6,26 +6,26 @@ import Time from 'components/TrackShow/Summary/Time'
 
 describe('Summary/Time', () => {
   it('shows time rounded to 1 digit', () => {
-    const { getByTestId } = render(<Time value={92.373} />)
+    const { getByLabelText } = render(<Time value={92.373} />)
 
-    expect(getByTestId('value')).toHaveTextContent('92.4')
+    expect(getByLabelText('duration').textContent).toBe('92.4')
   })
 
   it('shows 1 decimal digit when whole number', () => {
-    const { getByTestId } = render(<Time value={92} />)
+    const { getByLabelText } = render(<Time value={92} />)
 
-    expect(getByTestId('value')).toHaveTextContent('92.0')
+    expect(getByLabelText('duration').textContent).toBe('92.0')
   })
 
   it('shows placeholder if no value given', () => {
-    const { getByTestId } = render(<Time />)
+    const { getByLabelText } = render(<Time />)
 
-    expect(getByTestId('value')).toHaveTextContent('--.-')
+    expect(getByLabelText('duration').textContent).toBe('--.-')
   })
 
   it('shows placeholder in case of NaN', () => {
-    const { getByTestId } = render(<Time value={0 / 0} />)
+    const { getByLabelText } = render(<Time value={0 / 0} />)
 
-    expect(getByTestId('value')).toHaveTextContent('--.-')
+    expect(getByLabelText('duration').textContent).toBe('--.-')
   })
 })
