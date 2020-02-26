d0cb7daa80494eb095f8739c4e05ca94aefe7d7a
Aleksandr Kunin <skyksandr@gmail.com>
2020-02-26 11:35:01 +0300

    Add TrackVideo to Navbar

--- app/javascript/components/TrackShow/index.js
+++ app/javascript/components/TrackShow/index.js
@@ -6,47 +6,27 @@ import { selectTrack } from 'redux/tracks'
 import { usePageContext } from 'components/PageContext'
 import TrackInsights from 'components/TrackInsights'
 import TrackMap from 'components/TrackMap'
+import TrackVideo from 'components/TrackVideo'
 
 import Header from './Header'
 import Navbar from './Navbar'
 import { Container } from './elements'
 
 const TrackShow = () => {
-  const { trackId, altitudeFrom, altitudeTo, straightLine } = usePageContext()
+  const { trackId } = usePageContext()
 
   const track = useSelector(state => selectTrack(state, trackId))
 
-  if (!track) return null
+  if (!track || track.status === 'loading') return null
 
   return (
     <Container>
       <Header track={track} />
-      <Navbar trackId={trackId} />
+      <Navbar track={track} />
       <Switch>
-        <Route
-          exact
-          path="/tracks/:id"
-          component={() => (
-            <TrackInsights
-              trackId={trackId}
-              altitudeFrom={altitudeFrom}
-              altitudeTo={altitudeTo}
-              straightLine={straightLine}
-            />
-          )}
-        />
-
-        <Route
-          path="/tracks/:id/map"
-          component={() => (
-            <TrackMap
-              trackId={trackId}
-              altitudeFrom={altitudeFrom}
-              altitudeTo={altitudeTo}
-            />
-          )}
-        />
-
+        <Route exact path="/tracks/:id" component={TrackInsights} />
+        <Route path="/tracks/:id/map" component={TrackMap} />
+        <Route path="/tracks/:id/video" component={TrackVideo} />
       </Switch>
     </Container>
   )
