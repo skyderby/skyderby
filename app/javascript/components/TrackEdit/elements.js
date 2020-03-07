5d31876aef1d5f362cba52d3183a4d24b550a152
Aleksandr Kunin <skyksandr@gmail.com>
2020-03-07 13:14:20 +0300

    WIP Tracks edit

---
+++ app/javascript/components/TrackEdit/elements.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+import { devices } from 'styles/devices'
+
+export const PageContainer = styled.div`
+  width: 100%;
+  margin: 0 auto;
+
+  @media ${devices.large} {
+    width: 1200px;
+  }
+`
+
+export const FormContainer = styled.div`
+  background-color: #fff;
+  width: 100%;
+  padding: 1rem;
+  border: solid 1px transparent;
+  border-top-color: #e0e0e0;
+  border-bottom-color: #e0e0e0;
+
+  @media ${devices.large} {
+    border-left-color: #e0e0e0;
+    border-right-color: #e0e0e0;
+  }
+
+`
