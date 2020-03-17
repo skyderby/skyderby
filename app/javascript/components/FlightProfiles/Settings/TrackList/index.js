1e4253cd233ea2e522d6ed3690b7cff362540ea8
Aleksandr Kunin <skyksandr@gmail.com>
2020-03-17 11:42:58 +0300

    WIP: New flight profiles

---
+++ app/javascript/components/FlightProfiles/Settings/TrackList/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+import { selectAllTracks } from 'redux/flightProfiles/tracksList'
+import Item from './Item'
+import { Container } from './elements'
+
+const TrackList = () => {
+  const tracks = useSelector(selectAllTracks)
+
+  return (
+    <Container>
+      {tracks.map(({ id }) => <Item key={id} trackId={id} />)}
+    </Container>
+  )
+}
+
+export default TrackList
