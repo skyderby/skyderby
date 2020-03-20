25167629f4fef9915aa95029fedb027e10a94ad4
Aleksandr Kunin <skyksandr@gmail.com>
2020-03-20 11:03:42 +0300

    Display terrain profiles

--- app/javascript/components/FlightProfiles/Settings/index.js
+++ app/javascript/components/FlightProfiles/Settings/index.js
@@ -1,15 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
+import { selectTerrainProfile } from 'redux/flightProfiles'
 import TrackList from './TrackList'
 import TerrainProfileSelect from './TerrainProfileSelect'
 import { Container, Settings } from './elements'
 
 const Sidebar = () => {
+  const dispatch = useDispatch()
+  const handleTerrainProfileChange = ({ value }) => dispatch(selectTerrainProfile(value))
+
   return (
     <Container>
       <TrackList />
       <Settings>
-        <TerrainProfileSelect />
+        <TerrainProfileSelect onChange={handleTerrainProfileChange} />
       </Settings>
     </Container>
   )
--- app/javascript/redux/countries/selectors.js
+++ app/javascript/redux/countries/selectors.js
@@ -1,5 +1,5 @@
 export const selectCountry = (state, countryId) => {
-  const { byId } = state.country
+  const { byId } = state.countries
 
   return byId[countryId]
 }
