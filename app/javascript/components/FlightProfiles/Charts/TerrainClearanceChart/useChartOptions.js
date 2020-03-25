ed01a5fa03eca282e3c6d62ff8a59401753372b0
Aleksandr Kunin <skyksandr@gmail.com>
2020-03-25 19:57:30 +0300

    Sync charts

---
+++ app/javascript/components/FlightProfiles/Charts/TerrainClearanceChart/useChartOptions.js
@@ -0,0 +1,70 @@
+const useChartOptions = (zoomLevel) => {
+  const options = {
+    chart: {
+      type: 'spline',
+      height: '150px'
+    },
+    title: {
+      text: null
+    },
+    plotOptions: {
+      spline: {
+        marker: {
+          enabled: false
+        }
+      },
+      series: {
+        marker: {
+          radius: 1
+        }
+      }
+    },
+    tooltip: {
+      crosshairs: true
+    },
+    yAxis: {
+      title: {
+        text: 'Terrain clearance'
+      },
+      gridLineWidth: 1,
+      tickInterval: 25,
+      min: 0,
+      max: 125,
+      plotBands: [
+        {
+          color: '#efcdcb',
+          from: 0,
+          to: 25
+        },
+        {
+          color: '#e4f1de',
+          from: 100,
+          to: 125
+        }
+      ]
+    },
+    xAxis: {
+      title: {
+        text: null
+      },
+      tickInterval: 100,
+      tickLength: 0,
+      gridLineWidth: 1,
+      min: zoomLevel?.min || 0,
+      max: zoomLevel?.max,
+      labels: {
+        enabled: false
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    legend: {
+      enabled: false
+    }
+  }
+
+  return options
+}
+
+export default useChartOptions
