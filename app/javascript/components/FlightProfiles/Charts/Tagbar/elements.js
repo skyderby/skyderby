eeaf662772775fee6de1770f356c55f25762392e
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-09 10:03:05 +0300

    WIP: Custom flight profiles legend

---
+++ app/javascript/components/FlightProfiles/Charts/Tagbar/elements.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const TagList = styled.ul`
+  background-color: #fff;
+  display: flex;
+  padding: 0.5rem 0.75rem;
+  margin: 0;
+
+  >:not(:last-child) {
+    margin-right: 0.5rem;
+  }
+`
+
+export const Tag = styled.li`
+  border-radius: 2px;
+  color: rgba(0, 0, 0, 0.55);
+  background-color: #f8f8f8;
+  padding: 2px 7px;
+`
