6615e6f53f30e960c242d6ef07b84f55d30ead39
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-09 10:03:05 +0300

    WIP: Custom flight profiles legend

---
+++ app/javascript/components/TokenizedSearchField/OptionsDropdown.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Dropdown, DropdownMenu, DropdownItem } from './elements'
+
+const OptionsDropdown = ({ options, onSelect }) => {
+  return (
+    <Dropdown>
+      <DropdownMenu>
+        {options.map(({ label, icon, value }) => (
+          <DropdownItem key={value} onClick={() => onSelect(value)} tabIndex={1}>
+            {icon}
+            <span>{label}</span>
+          </DropdownItem>
+        ))}
+      </DropdownMenu>
+    </Dropdown>
+  )
+}
+
+OptionsDropdown.propTypes = {
+  onSelect: PropTypes.func.isRequired
+}
+
+export default OptionsDropdown
