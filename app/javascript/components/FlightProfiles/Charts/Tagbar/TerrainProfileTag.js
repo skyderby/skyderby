b83764c75d6f31ee1826a8ba313dd6cf962b4dbc
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2020-04-14 10:08:03 +0300

    Implement deleting from custom chart legend && fix bug in measurements

--- app/javascript/components/FlightProfiles/Charts/Tagbar/TerrainProfileTag.js
+++ app/javascript/components/FlightProfiles/Charts/Tagbar/TerrainProfileTag.js
@@ -1,32 +1,37 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
 
 import IconTimes from 'icons/times.svg'
 
 import { createTerrainProfileSelector } from 'redux/terrainProfiles'
 import { Tag, DeleteButton, Label } from './elements'
 
-const handleDelete = () => {
-  console.log("Kill")
-}
-
-const TerrainProfile = ({ terrainProfileId }) => {
+const TerrainProfile = ({ terrainProfileId, onDelete }) => {
   const terrainProfile = useSelector(createTerrainProfileSelector(terrainProfileId))
 
   if (!terrainProfile) return null
 
-  const { place: { name: placeName }, name } = terrainProfile
+  const {
+    place: { name: placeName },
+    name
+  } = terrainProfile
 
   return (
     <Tag>
       <Label>
         {placeName} - {name}
       </Label>
-      <DeleteButton type="button" onClick={handleDelete}>
+      <DeleteButton type="button" onClick={onDelete}>
         <IconTimes />
       </DeleteButton>
     </Tag>
   )
+}
+
+TerrainProfile.propTypes = {
+  terrainProfileId: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired
 }
 
 export default TerrainProfile
--- app/javascript/components/FlightProfiles/Charts/Tagbar/TrackTag.js
+++ app/javascript/components/FlightProfiles/Charts/Tagbar/TrackTag.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import PropTypes from 'prop-types'
+
+import IconTimes from 'icons/times.svg'
 
 import { selectTrack } from 'redux/flightProfiles'
-import { Tag } from './elements'
+import { Tag, DeleteButton, Label } from './elements'
 
-const TrackTag = ({ trackId }) => {
+const TrackTag = ({ trackId, onDelete }) => {
   const track = useSelector(state => selectTrack(state, trackId))
 
   if (!track) return null
 
   return (
     <Tag>
-      {track.pilotName} - #{trackId}
+      <Label>
+        {track.pilotName} - #{trackId}
+      </Label>
+      <DeleteButton type="button" onClick={onDelete}>
+        <IconTimes />
+      </DeleteButton>
     </Tag>
   )
+}
+
+TrackTag.propTypes = {
+  trackId: PropTypes.number.isRequired,
+  onDelete: PropTypes.func.isRequired
 }
 
 export default TrackTag
