2471a32d6c4c69d7c9ded6c7866e88b5d15ffe9d
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-23 09:06:35 +0300

    WIP Video edit form

---
+++ app/javascript/components/ui/buttons/Primary.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import DefaultButton from './Default'
+
+export default styled(DefaultButton)`
+  background-color: #5CB85C;
+  border-color: #5CB85C;
+  color: #fff;
+`
