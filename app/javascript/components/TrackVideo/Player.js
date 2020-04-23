c06849b56fdba1d569da9c640c5af1fdc84159fa
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-23 09:06:35 +0300

    WIP Video edit form

---
+++ app/javascript/components/TrackVideo/Player.js
@@ -0,0 +1,51 @@
+import React, { useRef, useEffect, useImperativeHandle, forwardRef } from 'react'
+
+import useYoutubeApi from 'utils/useYoutubeApi'
+
+const defaultPlayerOptions = {
+  height: '390',
+  width: '640',
+  playerVars: {
+    autoplay: 0,
+    fs: 0,
+    iv_load_policy: 3,
+    rel: 0
+  }
+}
+
+
+const Player = forwardRef(({ videoId }, ref) => {
+  const playerContainerRef = useRef()
+  const playerRef = useRef()
+
+  const YT = useYoutubeApi()
+
+  useImperativeHandle(ref, () => ({
+    getPlayerTime: () => playerRef.current.getCurrentTime()
+  }))
+
+  useEffect(() => {
+    if (!YT) return
+
+    if (playerRef.current) {
+      playerRef.current.cueVideoById({ videoId })
+    } else {
+      playerRef.current = new YT.Player(
+        playerContainerRef.current,
+        { ...defaultPlayerOptions, videoId }
+      )
+    }
+  }, [YT, videoId])
+
+  useEffect(() => {
+    return () => playerRef.current?.destroy()
+  }, [])
+
+  return (
+    <div ref={playerContainerRef} />
+  )
+})
+
+Player.displayName = 'Player'
+
+export default Player
