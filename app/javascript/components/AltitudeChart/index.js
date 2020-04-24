d7d66e398adae7226511636bf817c3eeeb33229b
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2020-04-24 10:29:22 +0300

    WIP: Altitude chart on video edit page

---
+++ app/javascript/components/AltitudeChart/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import Highchart from 'components/Highchart'
+import useChartOptions from './useChartOptions'
+import { ChartContainer } from './elements'
+
+const AltitudeChart = ({ points, children }) => {
+  const options = useChartOptions(points)
+
+  return (
+    <>
+      <ChartContainer>
+        <Highchart options={options}>{children}</Highchart>
+      </ChartContainer>
+    </>
+  )
+}
+
+AltitudeChart.propTypes = {
+  points: PropTypes.arrayOf(
+    PropTypes.shape({
+      flTime: PropTypes.number,
+      altitude: PropTypes.number,
+      hSpeed: PropTypes.number,
+      vSpeed: PropTypes.number
+    })
+  ).isRequired,
+  children: PropTypes.node
+}
+
+export default AltitudeChart
