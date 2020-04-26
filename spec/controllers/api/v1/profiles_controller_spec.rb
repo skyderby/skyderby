786f7f188cfe70776a11369a33152afe8fb91d8a
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-26 18:44:26 +0300

    Fix non-system specs

--- spec/controllers/api/v1/profiles_controller_spec.rb
+++ spec/controllers/api/v1/profiles_controller_spec.rb
@@ -5,21 +5,37 @@ describe Api::V1::ProfilesController, type: :controller do
     get :show, params: { id: profile.id }, format: :json
 
     response_json = JSON.parse(response.body)
-    expected_json = JSON.parse(expected_result.to_json)
+
+    expected_json =
+      {
+        id: profile.id,
+        name: profile.name,
+        photo: {
+          original: '/images/original/missing.png',
+          medium: '/images/medium/missing.png',
+          thumb: '/images/thumb/missing.png'
+        }
+      }.with_indifferent_access
 
     expect(response_json).to eq(expected_json)
   end
 
-  def expected_result
-    {
-      id: profile.id,
-      name: profile.name,
-      photo: {
-        original: '/images/original/missing.png',
-        medium: '/images/medium/missing.png',
-        thumb: '/images/thumb/missing.png'
-      }
-    }
+  describe '#index' do
+    it 'returns none if no search term provided' do
+      get :index, format: :json
+
+      response_json = JSON.parse(response.body)
+
+      expect(response_json['items']).to eq([])
+    end
+
+    it 'returns collection if search term specified' do
+      get :index, params: { search: 'ale' }, format: :json
+
+      response_json = JSON.parse(response.body)
+
+      expect(response_json['items']).to include(hash_including({ 'name' => 'Alex' }))
+    end
   end
 
   def profile
