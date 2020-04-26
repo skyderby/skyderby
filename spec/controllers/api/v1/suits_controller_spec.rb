65573c57417d3401d8094102d907a433407d3be9
Aleksandr Kunin <skyksandr@gmail.com>
2020-04-26 18:44:26 +0300

    Fix non-system specs

--- spec/controllers/api/v1/suits_controller_spec.rb
+++ spec/controllers/api/v1/suits_controller_spec.rb
@@ -5,10 +5,10 @@ describe Api::V1::SuitsController do
     get :index, format: :json
 
     response_json = JSON.parse(response.body)
-    fields = response_json.map(&:keys).flatten.uniq
+    fields = response_json['items'].map(&:keys).flatten.uniq
 
     expect(fields).to match(
-      %w[id name make make_code category]
+      %w[id name make makeCode category]
     )
   end
 end
