aeb94b7d20d66d91588cd8c44bfae39174f52f36
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2020-05-01 22:40:21 +0300

    WIP: Player events && track card

--- app/javascript/components/AltitudeChart/useChartOptions.js
+++ app/javascript/components/AltitudeChart/useChartOptions.js
@@ -1,22 +1,19 @@
 import { useMemo } from 'react'
 import I18n from 'i18n-js'
+import merge from 'lodash.merge'
 
 import { restoreSeriesVisibility, saveSeriesVisibility } from 'utils/chartSeriesSettings'
 import { msToKmh } from 'utils/unitsConversion'
 
 const chartName = 'AltitudeRangeSelect'
 
-const baseOptions = onClick => ({
+const baseOptions = () => ({
   chart: {
     marginLeft: 16,
     marginRight: 16,
     events: {
       load: function() {
         restoreSeriesVisibility(chartName, this.series)
-      },
-      click: event => {
-        if (event.target.textContent) return
-        onClick(event)
       }
     }
   },
@@ -74,80 +71,73 @@ const baseOptions = onClick => ({
   }
 })
 
-const useChartOptions = (points, onClick) => {
-  const altitudePoints = useMemo(
-    () =>
-      points.map(el => [
-        Math.round((el.flTime - points[0].flTime) * 10) / 10,
-        Math.round(el.altitude)
-      ]),
-    [points]
-  )
+const buildSeries = points => {
+  const altitudePoints = points.map(el => [
+    Math.round((el.flTime - points[0].flTime) * 10) / 10,
+    Math.round(el.altitude)
+  ])
 
-  const horizontalSpeed = useMemo(
-    () =>
-      points.map(el => [
-        Math.round((el.flTime - points[0].flTime) * 10) / 10,
-        Math.round(msToKmh(el.hSpeed))
-      ]),
-    [points]
-  )
+  const horizontalSpeed = points.map(el => [
+    Math.round((el.flTime - points[0].flTime) * 10) / 10,
+    Math.round(msToKmh(el.hSpeed))
+  ])
 
-  const verticalSpeed = useMemo(
-    () =>
-      points.map(el => [
-        Math.round((el.flTime - points[0].flTime) * 10) / 10,
-        Math.round(msToKmh(el.vSpeed))
-      ]),
-    [points]
-  )
+  const verticalSpeed = points.map(el => [
+    Math.round((el.flTime - points[0].flTime) * 10) / 10,
+    Math.round(msToKmh(el.vSpeed))
+  ])
 
-  const options = useMemo(
-    () => ({
-      ...baseOptions(onClick),
-      series: [
-        {
-          name: I18n.t('charts.all_data.series.height'),
-          type: 'area',
-          code: 'height',
-          data: altitudePoints,
-          yAxis: 0,
-          color: '#9ec8f1',
-          tooltip: {
-            valueSuffix: ` ${I18n.t('units.m')}`,
-            valueDecimals: 0
-          }
-        },
-        {
-          name: I18n.t('charts.all_data.series.horiz_speed'),
-          type: 'spline',
-          code: 'ground_speed',
-          data: horizontalSpeed,
-          yAxis: 1,
-          color: '#52A964',
-          lineWidth: 1,
-          tooltip: {
-            valueSuffix: ` ${I18n.t('units.kmh')}`,
-            valueDecimals: 0
-          }
-        },
-        {
-          name: I18n.t('charts.all_data.series.vert_speed'),
-          type: 'spline',
-          code: 'vertical_speed',
-          data: verticalSpeed,
-          yAxis: 1,
-          color: '#A7414E',
-          lineWidth: 1,
-          tooltip: {
-            valueSuffix: ` ${I18n.t('units.kmh')}`,
-            valueDecimals: 0
-          }
+  return {
+    series: [
+      {
+        name: I18n.t('charts.all_data.series.height'),
+        type: 'area',
+        code: 'height',
+        data: altitudePoints,
+        yAxis: 0,
+        color: '#9ec8f1',
+        tooltip: {
+          valueSuffix: ` ${I18n.t('units.m')}`,
+          valueDecimals: 0
         }
-      ]
-    }),
-    [altitudePoints, horizontalSpeed, verticalSpeed]
-  )
+      },
+      {
+        name: I18n.t('charts.all_data.series.horiz_speed'),
+        type: 'spline',
+        code: 'ground_speed',
+        data: horizontalSpeed,
+        yAxis: 1,
+        color: '#52A964',
+        lineWidth: 1,
+        tooltip: {
+          valueSuffix: ` ${I18n.t('units.kmh')}`,
+          valueDecimals: 0
+        }
+      },
+      {
+        name: I18n.t('charts.all_data.series.vert_speed'),
+        type: 'spline',
+        code: 'vertical_speed',
+        data: verticalSpeed,
+        yAxis: 1,
+        color: '#A7414E',
+        lineWidth: 1,
+        tooltip: {
+          valueSuffix: ` ${I18n.t('units.kmh')}`,
+          valueDecimals: 0
+        }
+      }
+    ]
+  }
+}
+
+const useChartOptions = (points, additionalOptions) => {
+  const series = useMemo(() => buildSeries(points), [points])
+
+  const options = useMemo(() => merge(baseOptions(), additionalOptions, series), [
+    additionalOptions,
+    series
+  ])
 
   return options
 }
--- app/javascript/components/TrackVideo/index.js
+++ app/javascript/components/TrackVideo/index.js
@@ -1,25 +1,15 @@
-import React, { useRef } from 'react'
-import { Formik, Field } from 'formik'
+import React from 'react'
+import { Formik } from 'formik'
 
-import Input from 'components/ui/Input'
 import DefaultButton from 'components/ui/buttons/Default'
 import PrimaryButton from 'components/ui/buttons/Primary'
-import { videoCodeFromUrl } from 'utils/youtube'
-import Player from './Player'
-import { Container, Section, Description, Controls, Footer } from './elements'
+import { Container, Footer } from './elements'
 
-import AltitudeChart from 'components/AltitudeChart'
-import { useTrackPoints } from 'components/AltitudeRangeSelect'
-import { usePageContext } from 'components/PageContext'
-
-import PlotLine from 'components/Highchart/Plotline'
+import VideoSetup from './VideoSetup'
+import TrackOffset from './TrackOffset'
 
 const TrackVideo = () => {
-  const playerRef = useRef()
-
   const handleSubmit = values => console.log(values)
-  const { trackId } = usePageContext()
-  const points = useTrackPoints(trackId, { trimed: false })
 
   return (
     <Container>
@@ -28,81 +18,11 @@ const TrackVideo = () => {
         onSubmit={handleSubmit}
       >
         {({ values, handleSubmit, setFieldValue }) => {
-          const handleUrlChange = e => {
-            const { value } = e.target
-
-            setFieldValue('url', value)
-            setFieldValue('videoId', videoCodeFromUrl(value))
-          }
-
-          const setTimeFromVideo = () => {
-            setFieldValue('startTime', playerRef.current.getPlayerTime())
-          }
-
-          const handleOnClick = event => {
-            const offset = Math.round(event.xAxis[0].value * 10) / 10
-            setFieldValue('trackOffset', offset)
-          }
-
           return (
             <form onSubmit={handleSubmit}>
-              <Section>
-                <Description>
-                  <h2>Youtube video</h2>
-                </Description>
-                <Controls>
-                  <Field
-                    as={Input}
-                    name="url"
-                    placeholder="Enter Youtube link here"
-                    onChange={handleUrlChange}
-                  />
-                </Controls>
-              </Section>
+              <VideoSetup setFieldValue={setFieldValue} value={values.videoId} />
+              <TrackOffset setFieldValue={setFieldValue} value={values.trackOffset} />
 
-              <Section>
-                <Description>
-                  <h2>Start of the jump on video</h2>
-                  <p>
-                    Play video until jump started, pause the video at the beginning of the
-                    jump, then press button &quot;Set&quot; in the field to paste current
-                    time. Value in the field is a seconds from beginning of video.
-                  </p>
-                </Description>
-                <Controls>
-                  <Player ref={playerRef} videoId={values.videoId} />
-                  <Field as={Input} name="startTime" />
-                  <DefaultButton type="button" onClick={setTimeFromVideo}>
-                    Set!
-                  </DefaultButton>
-                </Controls>
-              </Section>
-
-              <Section>
-                <Description>
-                  <h2>Start of the jump on track</h2>
-                  <p>
-                    Find start of the jump on the chart and click on it. For precious
-                    changing use chart zooming feature or buttons in the field. Value in
-                    the field is a seconds from when you start recording jump.
-                  </p>
-                </Description>
-                <Controls>
-                  <AltitudeChart points={points} onClick={handleOnClick}>
-                    {chart => (
-                      <PlotLine
-                        chart={chart}
-                        color="#FF0000"
-                        id="track-offset"
-                        width={2}
-                        value={Number(values.trackOffset)}
-                        zIndex={8}
-                      />
-                    )}
-                  </AltitudeChart>
-                  <Field as={Input} name="trackOffset" />
-                </Controls>
-              </Section>
               <Footer>
                 <DefaultButton type="button">Delete</DefaultButton>
                 <PrimaryButton type="submit">Save</PrimaryButton>
