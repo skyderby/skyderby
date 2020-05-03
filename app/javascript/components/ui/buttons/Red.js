8c7675b3858cb9d0b44be05d20c55b4073beabd3
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 17:40:16 +0300

    Fix linter offences

--- app/javascript/components/ui/buttons/Red.js
+++ app/javascript/components/ui/buttons/Red.js
@@ -5,32 +5,27 @@ import DefaultButton from './Default'
 const RedButton = styled(DefaultButton)`
   border-color: var(--red-70);
 
-  ${props => props.outlined
-    ? css`
-      background-color: var(--white);
-      color: var(--red-70);
-    `
-    : css`
-      background-color: var(--red-70);
-      color: var(--white);
-    `
-  }
-
-
   &:hover {
     border-color: var(--red-80);
-
-    ${props => props.outlined
-      ? css`
-        background-color: var(--white);
-        color: var(--red-80);
-      `
-      : css`
-        background-color: var(--red-80);
-        color: var(--white);
-      `
-    }
   }
+
+  ${props =>
+    props.outlined
+      ? css`
+          color: var(--red-70);
+
+          &:hover {
+            color: var(--red-80);
+          }
+        `
+      : css`
+          background-color: var(--red-70);
+          color: var(--white);
+
+          &:hover {
+            background-color: var(--red-80);
+          }
+        `}
 `
 
 export default RedButton
