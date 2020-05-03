f4329580de81b8b09a099fc27d2a9dcc46edf65a
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 17:29:37 +0300

    Remove duplication of track show content container

--- app/javascript/components/TrackMap/elements.js
+++ app/javascript/components/TrackMap/elements.js
@@ -1,20 +1,6 @@
 import styled from 'styled-components'
 import { devices } from 'styles/devices'
 
-export const Container = styled.div`
-  background-color: #fff;
-  border: solid 1px transparent;
-  border-top-color: var(--border-color);
-  border-bottom-color: var(--border-color);
-  position: relative;
-  width: 100%;
-
-  @media ${devices.large} {
-    border-left-color: var(--border-color);
-    border-right-color: var(--border-color);
-  }
-`
-
 export const MapElement = styled.div`
   height: 500px;
   width: 100%;
