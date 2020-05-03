fe9a479cf666bc69856f1646504b07c3430cec6c
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 11:58:07 +0300

    Organize colors, use css vars

---
+++ app/javascript/components/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  :root {
+    --blue-50: #00a7ff;
+    --blue-60: #008ed9;
+
+    --red-70: #ff5e5e;
+    --red-80: #d61e1e;
+
+    --green-50: #349e34;
+    --green-40: #5cb85c;
+
+    --grey-90: #333;
+    --grey-80: #555;
+    --grey-30: #d3d3d3;
+    --grey-20: #e8e8e8;
+
+    --white: #fff;
+
+    --border-color: var(--grey-30);
+    --text-color: var(--grey-90);
+  }
+`
+
+export default GlobalStyle
