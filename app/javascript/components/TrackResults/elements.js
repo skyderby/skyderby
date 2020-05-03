9f0dabf17d43172d054ff750d1977f65bbc0ef91
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 17:29:37 +0300

    Remove duplication of track show content container

--- app/javascript/components/TrackResults/elements.js
+++ app/javascript/components/TrackResults/elements.js
@@ -1,22 +1,5 @@
 import styled from 'styled-components'
 
-import { devices } from 'styles/devices'
-
-export const Container = styled.div`
-  background-color: #fff;
-  font-family: 'Proxima Nova Regular';
-  width: 100%;
-  padding: 1rem;
-  border: solid 1px transparent;
-  border-top-color: var(--border-color);
-  border-bottom-color: var(--border-color);
-
-  @media ${devices.large} {
-    border-left-color: var(--border-color);
-    border-right-color: var(--border-color);
-  }
-`
-
 export const Header = styled.h2`
   color: #555;
   font-family: 'Proxima Nova Semibold';
