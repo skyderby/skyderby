dc3848ddd2b4065d247f5bdc8a926f6763300fd4
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 11:58:07 +0300

    Organize colors, use css vars

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,20 +3,26 @@ import { Provider } from 'react-redux'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import store from 'redux/store'
+import GlobalStyle from 'components/GlobalStyle'
 import TracksEdit from 'pages/tracks/Edit'
 import TracksShow from 'pages/tracks/Show'
 import TracksIndex from 'pages/tracks/Index'
 import FlightProfiles from 'pages/FlightProfiles'
+import EventRoundMap from 'pages/events/RoundMap'
 
 const App = () => (
   <Provider store={store}>
     <BrowserRouter>
+      <GlobalStyle />
+
       <Switch>
         <Route path="/flight_profiles" component={FlightProfiles} />
 
         <Route exact path="/tracks" component={TracksIndex} />
         <Route path="/tracks/:id/edit" component={TracksEdit} />
         <Route path="/tracks/:id" component={TracksShow} />
+
+        <Route path="/events/:id/rounds/:roundId/map" component={EventRoundMap} />
 
         {/* Fallback route for server rendered part of the app */}
         <Route
