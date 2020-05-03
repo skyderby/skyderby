080346df599d27fbe91101768d3649cc33169006
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-03 17:29:37 +0300

    Remove duplication of track show content container

--- app/javascript/components/TrackShow/elements.js
+++ app/javascript/components/TrackShow/elements.js
@@ -1,11 +1,26 @@
 import styled from 'styled-components'
 import { devices } from 'styles/devices'
 
-export const Container = styled.div`
+export const PageContainer = styled.div`
   width: 100%;
   margin: 0 auto;
 
   @media ${devices.large} {
     width: 1200px;
+  }
+`
+
+export const ContentContainer = styled.div`
+  background-color: #fff;
+  font-family: 'Proxima Nova Regular';
+  width: 100%;
+  padding: 1rem;
+  border: solid 1px transparent;
+  border-top-color: var(--border-color);
+  border-bottom-color: var(--border-color);
+
+  @media ${devices.large} {
+    border-left-color: var(--border-color);
+    border-right-color: var(--border-color);
   }
 `
