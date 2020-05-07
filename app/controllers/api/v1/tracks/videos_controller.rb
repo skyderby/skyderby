850474c2fb2773d940985c8dbaeb977083f115f2
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-07 10:41:36 +0300

    Create, update, delete video handling

--- app/controllers/api/v1/tracks/videos_controller.rb
+++ app/controllers/api/v1/tracks/videos_controller.rb
@@ -2,12 +2,60 @@ module Api
   module V1
     module Tracks
       class VideosController < ApplicationController
-        def show
-          track = authorize Track.includes(:video).find(params[:track_id])
+        before_action :set_track
 
-          @video = track.video
+        def show
+          authorize @track, :show?
+
+          @video = @track.video
 
           head(:not_found) if @video.blank?
+        end
+
+        def create
+          authorize @track, :update?
+
+          @video = (@track.video || @track.build_video)
+          @video.assign_attributes(video_params)
+
+          respond_to do |format|
+            if @video.save
+              format.json
+            else
+              format.json do
+                render json: { errors: @video.errors }, status: :unprocessable_entity
+              end
+            end
+          end
+        end
+
+        def destroy
+          authorize @track, :update?
+
+          video = @track.video
+
+          return head :no_content if video.blank?
+
+          if video.destroy
+            head :no_content
+          else
+            render json: { errors: video.errors }, status: :unprocessable_entity
+          end
+        end
+
+        private
+
+        def video_params
+          params.require(:track_video).permit(
+            :url,
+            :video_code,
+            :video_offset,
+            :track_offset
+          )
+        end
+
+        def set_track
+          @track = Track.includes(:video).find(params[:track_id])
         end
       end
     end
