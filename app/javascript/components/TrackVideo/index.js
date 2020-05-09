1de81e93c3c40110371391992af22da2600c1912
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-09 12:35:31 +0300

    Handle saving and deleting video from form

--- app/javascript/components/TrackVideo/index.js
+++ app/javascript/components/TrackVideo/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
+import { createTrackSelector } from 'redux/tracks'
 import { createTrackVideoSelector } from 'redux/tracks/videos'
 import { usePageContext } from 'components/PageContext'
 
 const TrackVideo = () => {
   const { trackId } = usePageContext()
+  const track = useSelector(createTrackSelector(trackId))
   const video = useSelector(createTrackVideoSelector(trackId))
+
+  if (!track.hasVideo) {
+    if (track.editable) {
+      return <Redirect to={`/tracks/${trackId}/video/edit`} />
+    } else {
+      return <Redirect to={`/tracks/${trackId}`} />
+    }
+  }
 
   return <h2>Track video, {JSON.stringify(video)}</h2>
 }
--- app/javascript/redux/tracks/videos/actionTypes.js
+++ app/javascript/redux/tracks/videos/actionTypes.js
@@ -4,3 +4,11 @@ export const LOAD_REQUEST = `${prefix} LOAD/REQUEST`
 export const LOAD_SUCCESS = `${prefix} LOAD/SUCCESS`
 export const LOAD_NO_VIDEO = `${prefix} LOAD/NO_VIDEO`
 export const LOAD_ERROR = `${prefix} LOAD/ERROR`
+
+export const SAVE_REQUEST = `${prefix} SAVE/REQUEST`
+export const SAVE_SUCCESS = `${prefix} SAVE/SUCCESS`
+export const SAVE_ERROR = `${prefix} SAVE/ERROR`
+
+export const DELETE_REQUEST = `${prefix} DELETE/REQUEST`
+export const DELETE_SUCCESS = `${prefix} DELETE/SUCCESS`
+export const DELETE_ERROR = `${prefix} DELETE/ERROR`
