749b5b2b95bd11bde6ba61dde614a6c68750c128
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-12 09:52:20 +0300

    Minimal video player

--- app/javascript/components/TrackVideo/index.js
+++ app/javascript/components/TrackVideo/index.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useCallback, useEffect, useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom'
 
 import { createTrackSelector } from 'redux/tracks'
+import { createPointsSelector } from 'redux/tracks/points'
 import { createTrackVideoSelector } from 'redux/tracks/videos'
 import { usePageContext } from 'components/PageContext'
+import YoutubePlayer from 'components/YoutubePlayer'
+import Indicators from './Indicators'
+import { getDataForTime } from './utils'
 
 const TrackVideo = () => {
   const { trackId } = usePageContext()
   const track = useSelector(createTrackSelector(trackId))
+  const points = useSelector(createPointsSelector(trackId))
   const video = useSelector(createTrackVideoSelector(trackId))
+
+  const requestId = useRef()
+  const playerRef = useRef()
+  const indicatorsRef = useRef()
+
+  const drawFrame = useCallback(() => {
+    const currentTime = playerRef.current.getPlayerTime()
+    const currentData = getDataForTime(points, video, currentTime)
+
+    indicatorsRef.current.setData(currentData)
+
+    requestId.current = requestAnimationFrame(drawFrame)
+  }, [points, video])
+
+  const onPlay = useCallback(() => {
+    requestId.current = requestAnimationFrame(drawFrame)
+  }, [drawFrame])
+
+  const onPause = useCallback(() => {
+    cancelAnimationFrame(requestId.current)
+    requestId.current = undefined
+  }, [])
+
+  useEffect(() => {
+    return () => requestId.current && cancelAnimationFrame(requestId.current)
+  }, [])
 
   if (!track.hasVideo) {
     if (track.editable) {
@@ -19,7 +50,19 @@ const TrackVideo = () => {
     }
   }
 
-  return <h2>Track video, {JSON.stringify(video)}</h2>
+  if (!video) return null
+
+  return (
+    <div>
+      <YoutubePlayer
+        ref={playerRef}
+        videoId={video.videoCode}
+        onPlay={onPlay}
+        onPause={onPause}
+      />
+      <Indicators ref={indicatorsRef} />
+    </div>
+  )
 }
 
 export default TrackVideo
