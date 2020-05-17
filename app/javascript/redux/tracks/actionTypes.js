75aea7716a3d04b6248b691482c9139388dea2f6
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-17 18:40:26 +0300

    WIP Track edit page

--- app/javascript/redux/tracks/actionTypes.js
+++ app/javascript/redux/tracks/actionTypes.js
@@ -3,3 +3,7 @@ const prefix = '[tracks]'
 export const LOAD_REQUEST = `${prefix} LOAD/REQUEST`
 export const LOAD_SUCCESS = `${prefix} LOAD/SUCCESS`
 export const LOAD_ERROR = `${prefix} LOAD/ERROR`
+
+export const DELETE_REQUEST = `${prefix} DELETE/REQUEST`
+export const DELETE_SUCCESS = `${prefix} DELETE/SUCCESS`
+export const DELETE_ERROR = `${prefix} DELETE/ERROR`
