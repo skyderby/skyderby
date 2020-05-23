fb9434d01c9b453813f175bcf21d918f86d55e74
Aleksandr Kunin <skyksandr@gmail.com>
2020-05-23 19:55:16 +0300

    Deleting track

--- app/javascript/components/TrackEdit/index.js
+++ app/javascript/components/TrackEdit/index.js
@@ -1,22 +1,48 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 
-import { createTrackSelector } from 'redux/tracks'
+import { createTrackSelector, deleteTrack } from 'redux/tracks'
 import { usePageContext } from 'components/PageContext'
 import BackLink from 'components/ui/BackLink'
 import Form from './Form'
 import { PageContainer, FormContainer } from './elements'
 
 const TrackEdit = () => {
-  const { trackId } = usePageContext()
+  const dispatch = useDispatch()
+  const history = useHistory()
+  const { trackId, locationState } = usePageContext()
   const track = useSelector(createTrackSelector(trackId))
+
+  const handleSubmit = console.log
+
+  const handleDelete = async () => {
+    const confirmed = confirm(I18n.t('tracks.show.delete_confirmation'))
+
+    if (!confirmed) return
+
+    await dispatch(deleteTrack(track.id))
+
+    history.push(locationState?.returnTo || '/tracks')
+  }
+
+  const handleCancel = () => {
+    history.goBack()
+  }
 
   return (
     <PageContainer>
       <BackLink to={`/tracks/${trackId}`}>{I18n.t('general.back')}</BackLink>
 
       <FormContainer>
-        {track?.status === 'loaded' && <Form track={track} />}
+        {track?.status === 'loaded' && (
+          <Form
+            track={track}
+            onSubmit={handleSubmit}
+            onDelete={handleDelete}
+            onCancel={handleCancel}
+          />
+        )}
       </FormContainer>
     </PageContainer>
   )
