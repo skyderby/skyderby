fab4aa87b420bcb42af0f19b0386bd1613012f50
Aleksandr Kunin <skyksandr@gmail.com>
2020-06-01 11:48:53 +0300

    Cover TokenizedSearchField by tests, minor refactorings

--- app/javascript/components/TrackList/Filters/index.js
+++ app/javascript/components/TrackList/Filters/index.js
@@ -3,63 +3,20 @@ import { Container, Header, Body } from './elements'
 import { useHistory, useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
-import ProfileSelect, { useProfileValue } from 'components/ProfileSelect'
-import PlaceSelect, { usePlaceValue } from 'components/PlaceSelect'
-import SuitSelect, { useSuitValue } from 'components/SuitSelect'
+import TokenizedSearchField from 'components/TokenizedSearchField'
+
+import { places, profiles, suits, years } from 'components/TokenizedSearchField/settings'
 
 const Filters = ({ urlBuilder }) => {
-  const history = useHistory()
-  const { profileId, suitId, placeId } = Object.fromEntries(
-    new URLSearchParams(useLocation().search)
-  )
+  const tokens =
+    Array.from(new URLSearchParams(useLocation().search))
+    .map(([key, value]) => ({ type: key.replace(/(Id)?\[\]/, ''), value}))
 
-  const [profile, setProfile] = useProfileValue(profileId)
-  const [place, setPlace] = usePlaceValue(placeId)
-  const [suit, setSuit] = useSuitValue(suitId)
-
-  const updateUrl = params => {
-    const newUrl = urlBuilder({ page: null, ...params })
-    history.push(newUrl)
-  }
-
-  const handleProfileChange = option => {
-    setProfile(option)
-    updateUrl({ profileId: option && option.value })
-  }
-
-  const handleSuitChange = option => {
-    setSuit(option)
-    updateUrl({ suitId: option && option.value })
-  }
-
-  const handlePlaceChange = option => {
-    setPlace(option)
-    updateUrl({ placeId: option && option.value })
-  }
+  const onChange = val => console.log(val)
 
   return (
     <Container>
-      <Header>Filters</Header>
-      <Body>
-        <ProfileSelect
-          isClearable
-          placeholder="by pilot"
-          value={profile}
-          onChange={handleProfileChange}
-        />
-        <SuitSelect
-          isClearable
-          placeholder="by suit"
-          value={suit}
-          onChange={handleSuitChange}
-        />
-        <PlaceSelect
-          isClearable
-          placeholder="by location"
-          value={place}
-          onChange={handlePlaceChange}
-        />
-      </Body>
+      <TokenizedSearchField initialValues={tokens} onChange={onChange} dataTypes={[places, profiles, suits, years]} />
     </Container>
   )
 }
