f36f00899fd6f3029b6e906d6039032b84afbef8
Aleksandr Kunin <skyksandr@gmail.com>
2020-06-01 11:48:53 +0300

    Cover TokenizedSearchField by tests, minor refactorings

---
+++ app/javascript/components/TokenizedSearchField/settings/index.js
@@ -0,0 +1,4 @@
+export { default as places } from './places'
+export { default as profiles } from './profiles'
+export { default as suits } from './suits'
+export { default as years } from './years'
