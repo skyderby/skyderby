1cc40ee226157773a005364c0b12dd8cc982f7d6
Aleksandr Kunin <skyksandr@gmail.com>
2020-06-03 10:15:00 +0300

    WIP Integrate tokenized search field into tracks#index

--- app/javascript/components/TrackList/Filters/index.js
+++ app/javascript/components/TrackList/Filters/index.js
@@ -1,22 +1,28 @@
 import React from 'react'
 import { Container, Header, Body } from './elements'
-import { useHistory, useLocation } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
+import { usePageContext } from 'components/PageContext'
 import TokenizedSearchField from 'components/TokenizedSearchField'
 
 import { places, profiles, suits, years } from 'components/TokenizedSearchField/settings'
 
-const Filters = ({ urlBuilder }) => {
-  const tokens =
-    Array.from(new URLSearchParams(useLocation().search))
-    .map(([key, value]) => ({ type: key.replace(/(Id)?\[\]/, ''), value}))
+const Filters = () => {
+  const {
+    params: { filters },
+    updateFilters
+  } = usePageContext()
 
-  const onChange = val => console.log(val)
+  const tokens = filters.map(([type, value]) => ({ type, value }))
+  const onChange = val => updateFilters(val.map(({ type, value }) => [type, value]))
 
   return (
     <Container>
-      <TokenizedSearchField initialValues={tokens} onChange={onChange} dataTypes={[places, profiles, suits, years]} />
+      <TokenizedSearchField
+        initialValues={tokens}
+        onChange={onChange}
+        dataTypes={[places, profiles, suits, years]}
+      />
     </Container>
   )
 }
