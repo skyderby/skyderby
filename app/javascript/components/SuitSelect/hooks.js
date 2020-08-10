d45fc829945ab3f17e3b05b38db2ff2a90f06e4d
Aleksandr Kunin <skyksandr@gmail.com>
2020-08-10 12:37:47 +0300

    New track form

--- app/javascript/components/SuitSelect/hooks.js
+++ app/javascript/components/SuitSelect/hooks.js
@@ -1,16 +1,31 @@
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback, useRef } from 'react'
 
 import Api from 'api'
 
 export const useSuitValue = suitId => {
   const [suit, setSuit] = useState(null)
+  const suitsCache = useRef({})
+
+  const getSuitData = useCallback(async suitId => {
+    if (!suitsCache[suitId]) {
+      const data = await Api.Suit.findRecord(suitId)
+      suitsCache[suitId] = data
+    }
+
+    return suitsCache[suitId]
+  }, [])
 
   const fetchInitialSuit = useCallback(async () => {
+    if (suit && !suitId) {
+      setSuit(null)
+      return
+    }
+
     if (suit || !suitId) return
 
-    const data = await Api.Suit.findRecord(suitId)
+    const data = await getSuitData(suitId)
     setSuit({ value: data.id, label: data.name, ...data })
-  }, [suit, suitId])
+  }, [suit, suitId, getSuitData])
 
   useEffect(() => {
     fetchInitialSuit()
