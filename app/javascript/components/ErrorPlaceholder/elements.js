62424cbb5a809b0047fc9433fc9cb072ea09a3e0
Aleksandr Kunin <skyksandr@gmail.com>
2020-08-20 10:07:31 +0300

    Track#show 4xx and 5xx handling

---
+++ app/javascript/components/ErrorPlaceholder/elements.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components'
+import { devices } from 'styles/devices'
+
+export const Container = styled.div`
+  background-color: var(--white);
+  padding: 4rem 2rem;
+  text-align: center;
+  width: 100%;
+  margin: 4rem auto;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+
+  @media ${devices.small} {
+    max-width: 768px;
+    padding: 4rem;
+    border-radius: var(--border-radius-lg);
+  }
+`
+
+export const Title = styled.h1`
+  color: var(--grey-60);
+  font-family: 'Proxima Nova Bold';
+  font-size: 5rem;
+  margin: 0;
+  padding: 0;
+`
+
+export const Description = styled.p`
+  font-family: 'Proxima Nova Semibold';
+  font-size: 2rem;
+`
