5bdf724ee5ec263e5596028f47515779abab9d7d
Aleksandr Kunin <skyksandr@gmail.com>
2020-08-26 19:15:27 +0300

    Style improvements and bug fixes

--- app/javascript/components/Pagination/elements.js
+++ app/javascript/components/Pagination/elements.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Link } from 'react-router-dom'
 
 export const Container = styled.div`
   align-items: baseline;
-  font-family: 'Proxima Nova Regular';
+  font-family: 'Proxima Nova Light';
   display: flex;
   justify-content: center;
   margin: 1rem 0;
 
   > :not(:last-child) {
-    margin-right: 1.5rem;
+    margin-right: 0.75rem;
   }
 `
 
@@ -18,7 +18,7 @@ export const Arrow = styled.div`
   display: flex;
 
   svg {
-    height: 1.25rem;
+    height: 1rem;
     path {
       fill: #999;
     }
@@ -26,13 +26,43 @@ export const Arrow = styled.div`
 `
 
 export const Page = styled(({ active, ...props }) => <Link {...props} />)`
+  border-radius: var(--border-radius-md);
+  color: var(${props => (props.active ? '--blue-40' : '--grey-80')});
   font-size: 1.75rem;
-  padding: 0 0.25rem;
-  line-height: 2.5rem;
-  color: ${props => (props.active ? '#75A4BA' : '#555')};
-  border-bottom: solid 3px ${props => (props.active ? '#75A4BA' : 'transparent')};
+  height: 3rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 0;
+  padding: 0 0.75rem;
+  position: relative;
 
   :focus {
     outline: none;
   }
+
+  :hover {
+    color: var(${props => (props.active ? '--blue-40' : '--grey-80')});
+  }
+
+  ${props =>
+    !props.active && !props.disabled &&
+    css`
+      :hover {
+        background-color: var(--grey-20);
+      }
+  `}
+
+  ${props => props.active && css`
+    ::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0.25rem;
+      right: 0.25rem;
+      height: 3px;
+      background-color: var(--blue-40);
+      opacity: 0.9;
+    }
+  `}
 `
