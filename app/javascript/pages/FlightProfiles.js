ab35b0f63e37888864b929fcee3966943173a9b0
Aleksandr Kunin <skyksandr@gmail.com>
2020-08-28 16:21:31 +0300

    Make pages/FlightProfiles slimmer

--- app/javascript/pages/FlightProfiles.js
+++ app/javascript/pages/FlightProfiles.js
@@ -1,81 +1,44 @@
-import React, { useState, useRef, useEffect, useCallback } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { useHistory } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import isEqual from 'lodash.isequal'
 import PropTypes from 'prop-types'
 
-import { IndexParams } from 'api/Track'
+import { PageParams } from 'api/FlightProfiles'
 import FlightProfiles from 'components/FlightProfiles'
 import useTracksApi from 'hooks/useTracksApi'
-import { loadTerrainProfiles } from 'redux/terrainProfiles'
 import { loadTrack } from 'redux/tracks'
 import { loadTrackPoints } from 'redux/tracks/points'
 import { loadTerrainProfileMeasurement } from 'redux/terrainProfiles/measurements'
-
-const extractFromUrl = search => {
-  const tracksParams = {
-    ...IndexParams.extractFromUrl(search, 'tracks'),
-    activity: 'base'
-  }
-  const urlParams = new URLSearchParams(search)
-
-  const selectedTracks = urlParams.get('selectedTracks')
-    ? urlParams.get('selectedTracks').split(',').map(Number)
-    : []
-
-  const selectedTerrainProfile =
-    urlParams.get('selectedTerrainProfile') &&
-    Number(urlParams.get('selectedTerrainProfile'))
-
-  const straightLine = urlParams.get('straight-line') === 'true'
-
-  return { tracksParams, selectedTracks, selectedTerrainProfile, straightLine }
-}
-
-const mapToUrl = params => {
-  const { activity, ...mergedTracksParams } = params.tracksParams
-  const tracksParams = IndexParams.mapToUrl(mergedTracksParams, 'tracks')
-
-  const urlParams = new URLSearchParams(tracksParams)
-
-  if (params.selectedTracks.length > 0) {
-    urlParams.set('selectedTracks', params.selectedTracks.join(','))
-  }
-
-  if (params.selectedTerrainProfile) {
-    urlParams.set('selectedTerrainProfile', params.selectedTerrainProfile)
-  }
-
-  return urlParams.toString() === '' ? '' : '?' + urlParams.toString()
-}
 
 const FlightProfilesPage = ({ location }) => {
   const dispatch = useDispatch()
   const history = useHistory()
 
-  const [params, setParams] = useState(() => extractFromUrl(location.search))
-
-  const previousTracksParams = useRef()
+  const [params, setParams] = useState(() => PageParams.extractFromUrl(location.search))
 
   useEffect(() => {
-    const parsedParams = extractFromUrl(location.search)
+    const parsedParams = PageParams.extractFromUrl(location.search)
 
     if (isEqual(params, parsedParams)) return
 
     setParams(parsedParams)
   }, [params, setParams, location.search])
 
-  const { tracks, loadTracks, loadMoreTracks } = useTracksApi(params.tracksParams)
+  const { tracks, loadMoreTracks } = useTracksApi(params.tracksParams)
 
   useEffect(() => {
-    const skipLoad = isEqual(previousTracksParams.current, params.tracksParams)
+    params.selectedTracks.map(async id => {
+      dispatch(loadTrack(id))
+      dispatch(loadTrackPoints(id))
+    })
+  }, [dispatch, params.selectedTracks])
 
-    if (skipLoad) return
+  useEffect(() => {
+    if (!params.selectedTerrainProfile) return
 
-    loadTracks()
-
-    previousTracksParams.current = params.tracksParams
-  }, [loadTracks, params])
+    dispatch(loadTerrainProfileMeasurement(params.selectedTerrainProfile))
+  }, [dispatch, params.selectedTerrainProfile])
 
   const buildUrl = useCallback(
     newParams => {
@@ -88,7 +51,7 @@ const FlightProfilesPage = ({ location }) => {
         }
       }
 
-      return mapToUrl(mergedParams)
+      return PageParams.mapToUrl(mergedParams)
     },
     [params]
   )
@@ -116,23 +79,6 @@ const FlightProfilesPage = ({ location }) => {
     },
     [params, buildUrl, history, location.pathname]
   )
-
-  useEffect(() => {
-    dispatch(loadTerrainProfiles())
-  }, [dispatch])
-
-  useEffect(() => {
-    params.selectedTracks.map(async id => {
-      dispatch(loadTrack(id))
-      dispatch(loadTrackPoints(id))
-    })
-  }, [dispatch, params.selectedTracks])
-
-  useEffect(() => {
-    if (!params.selectedTerrainProfile) return
-
-    dispatch(loadTerrainProfileMeasurement(params.selectedTerrainProfile))
-  }, [dispatch, params.selectedTerrainProfile])
 
   return (
     <FlightProfiles
