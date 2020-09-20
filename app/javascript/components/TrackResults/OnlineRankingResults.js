f175c87665be3b89d384b8166b94aefd40a56559
Aleksandr Kunin <skyksandr@gmail.com>
2020-09-21 00:00:22 +0300

    Refactoring, update dependencies

--- app/javascript/components/TrackResults/OnlineRankingResults.js
+++ app/javascript/components/TrackResults/OnlineRankingResults.js
@@ -3,18 +3,20 @@ import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import PropTypes from 'prop-types'
 
+import { useI18n } from 'components/TranslationsProvider'
 import { selectOnlineRankingResults } from 'redux/tracks/results'
 import { formatResult } from './formatResult'
 import { Header } from './elements'
 
 const OnlineRankingResults = ({ trackId }) => {
+  const { t } = useI18n()
   const records = useSelector(state => selectOnlineRankingResults(state, trackId))
 
   if (records.length === 0) return null
 
   return (
     <div>
-      <Header>{I18n.t('tracks.show.online_comp_results')}</Header>
+      <Header>{t('tracks.show.online_comp_results')}</Header>
       <ul>
         {records.map(record => (
           <li key={record.id}>
@@ -22,7 +24,7 @@ const OnlineRankingResults = ({ trackId }) => {
               {[record.groupName, record.rankingName].filter(el => el).join(' - ')}
             </Link>
             :&nbsp;
-            {formatResult(record.result, record.task)}
+            {formatResult(record.result, record.task, t)}
           </li>
         ))}
       </ul>
