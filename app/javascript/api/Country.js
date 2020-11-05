a4942059dd3e05e1add66fbf825392ba90a3cdd0
Aleksandr Kunin <skyksandr@gmail.com>
2020-11-05 12:41:17 +0300

    WIP bulk load countries, places, suits

--- app/javascript/api/Country.js
+++ app/javascript/api/Country.js
@@ -7,6 +7,21 @@ const Country = {
     const { data } = await axios.get(`${endpoint}/${id}`)
 
     return data
+  },
+
+  pickAll: async ids => {
+    const uniqueIds = Array.from(new Set(ids))
+
+    const params = uniqueIds.reduce((acc, id) => {
+      acc.append('ids[]', id)
+      return acc
+    }, new URLSearchParams())
+
+    params.set('perPage', uniqueIds.length)
+
+    const { data } = await axios.get(`${endpoint}?${params.toString()}`)
+
+    return data
   }
 }
 
