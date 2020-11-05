9ea82155402bd94d6bac33d8843e455a2f037a4f
Aleksandr Kunin <skyksandr@gmail.com>
2020-11-05 12:41:17 +0300

    WIP bulk load countries, places, suits

---
+++ spec/controllers/api/v1/countries_controller_spec.rb
@@ -0,0 +1,28 @@
+describe Api::V1::CountriesController do
+  render_views
+
+  describe '#index' do
+    it 'returns correct fields' do
+      get :index, format: :json
+
+      response_json = JSON.parse(response.body)
+      fields = response_json['items'].map(&:keys).flatten.uniq
+
+      expect(fields).to match(%w[id name code])
+    end
+
+    it 'filters by ids' do
+      norway = countries(:norway)
+      russia = countries(:russia)
+
+      3.times { |idx| Country.create!(name: 'test', code: idx) }
+
+      get :index, format: :json, params: { ids: [norway.id, russia.id] }
+
+      expect(response).to be_successful
+
+      names = response.parsed_body.fetch('items').map { |el| el['name'] }
+      expect(names).to match_array([norway.name, russia.name])
+    end
+  end
+end
--- spec/controllers/api/v1/suits_controller_spec.rb
+++ spec/controllers/api/v1/suits_controller_spec.rb
@@ -1,12 +1,29 @@
 describe Api::V1::SuitsController do
   render_views
 
-  it '#index' do
-    get :index, format: :json
+  describe '#index' do
+    it 'returns correct fields' do
+      get :index, format: :json
 
-    response_json = JSON.parse(response.body)
-    fields = response_json['items'].map(&:keys).flatten.uniq
+      response_json = JSON.parse(response.body)
+      fields = response_json['items'].map(&:keys).flatten.uniq
 
-    expect(fields).to match(%w[id name makeId category])
+      expect(fields).to match(%w[id name makeId category editable])
+    end
+
+    it 'filters by ids' do
+      manufacturer = manufacturers(:tony)
+      apache = suits(:apache)
+      nala = suits(:nala)
+
+      3.times { Suit.create!(name: 'test', manufacturer: manufacturer) }
+
+      get :index, format: :json, params: { ids: [apache.id, nala.id] }
+
+      expect(response).to be_successful
+
+      names = response.parsed_body.fetch('items').map { |el| el['name'] }
+      expect(names).to match_array([apache.name, nala.name])
+    end
   end
 end
