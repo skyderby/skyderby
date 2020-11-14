4f6c73fbcb0914d94d3b9e5259a52d0fef15815b
Aleksandr Kunin <skyksandr@gmail.com>
2020-11-14 15:49:40 +0300

    Pagination in Events index and bug fixes in bulk loading places and countries

--- app/javascript/pages/events/Index.js
+++ app/javascript/pages/events/Index.js
@@ -1,15 +1,31 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import isEqual from 'lodash.isequal'
+import PropTypes from 'prop-types'
 
 import AppShell from 'components/AppShell'
 import EventsIndex from 'components/EventsIndex'
 import { bulkLoadPlaces } from 'redux/places'
 import useEventsApi from 'hooks/useEventsApi'
 
-const Index = () => {
+const extractParamsFromUrl = search => {
+  const urlParams = new URLSearchParams(search)
+  const page = urlParams.get('page') || '1'
+
+  return { page }
+}
+
+const mapParamsToUrl = ({ page }) => {
+  const params = new URLSearchParams()
+  params.set('page', page)
+
+  return params.toString() === '' ? '' : '?' + params.toString()
+}
+
+const Index = ({ location }) => {
   const dispatch = useDispatch()
-  const { events, pagination } = useEventsApi({})
-  const buildUrl = () => {}
+  const [params, setParams] = useState(() => extractParamsFromUrl(location.search))
+  const { events, pagination } = useEventsApi(params)
 
   useEffect(() => {
     const placeIds = events.map(event => event.placeId).filter(Boolean)
@@ -18,11 +34,26 @@ const Index = () => {
     dispatch(bulkLoadPlaces(placeIds))
   }, [dispatch, events])
 
+  useEffect(() => {
+    const parsedParams = extractParamsFromUrl(location.search)
+
+    if (isEqual(params, parsedParams)) return
+
+    setParams(parsedParams)
+  }, [params, setParams, location.search])
+
   return (
     <AppShell>
-      <EventsIndex buildUrl={buildUrl} events={events} pagination={pagination} />
+      <EventsIndex buildUrl={mapParamsToUrl} events={events} pagination={pagination} />
     </AppShell>
   )
+}
+
+Index.propTypes = {
+  location: PropTypes.shape({
+    search: PropTypes.string,
+    pathname: PropTypes.string
+  })
 }
 
 export default Index
