8efd6a32f31fe379906391d2f3135e921c8018d6
Aleksandr Kunin <skyksandr@gmail.com>
2020-11-14 11:17:55 +0300

    Events index markup

--- app/javascript/pages/events/Index.js
+++ app/javascript/pages/events/Index.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 import AppShell from 'components/AppShell'
 import EventsIndex from 'components/EventsIndex'
+import { bulkLoadPlaces } from 'redux/places'
 import useEventsApi from 'hooks/useEventsApi'
 
 const Index = () => {
+  const dispatch = useDispatch()
   const { events, pagination } = useEventsApi({})
   const buildUrl = () => {}
+
+  useEffect(() => {
+    const placeIds = events.map(event => event.placeId).filter(Boolean)
+    if (placeIds.length === 0) return
+
+    dispatch(bulkLoadPlaces(placeIds))
+  }, [dispatch, events])
 
   return (
     <AppShell>
