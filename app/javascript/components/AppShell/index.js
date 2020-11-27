d8d3811a790a50f7452915110e83c5103252e3f8
Aleksandr Kunin <skyksandr@gmail.com>
2020-11-27 12:30:25 +0300

    Fix flight profiles styles, add basic footer

--- app/javascript/components/AppShell/index.js
+++ app/javascript/components/AppShell/index.js
@@ -1,21 +1,24 @@
 import React from 'react'
+import cx from 'clsx'
 import PropTypes from 'prop-types'
 
 import ErrorBoundary from 'components/ErrorBoundary'
 import Navbar from './Navbar'
+import Footer from './Footer'
 import styles from './styles.module.scss'
 
-const AppShell = ({ children }) => {
+const AppShell = ({ fullScreen, children }) => {
   return (
-    <div className={styles.container}>
+    <div className={cx(styles.container, fullScreen && styles.fullScreen)}>
       <Navbar />
-
       <ErrorBoundary>{children}</ErrorBoundary>
+      <Footer />
     </div>
   )
 }
 
 AppShell.propTypes = {
+  fullScreen: PropTypes.bool,
   children: PropTypes.node.isRequired
 }
 
