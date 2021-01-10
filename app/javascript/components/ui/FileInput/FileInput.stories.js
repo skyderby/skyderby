e0064d6fdda4a9a1432a471fe86aec577e34a16d
Aleksandr Kunin <skyksandr@gmail.com>
2021-01-10 17:16:39 +0300

    Fix styles for FileInput

--- app/javascript/components/ui/FileInput/FileInput.stories.js
+++ app/javascript/components/ui/FileInput/FileInput.stories.js
@@ -5,9 +5,14 @@ import FileInput from './'
 export default {
   title: 'components/FileInput',
   component: FileInput,
+  decorators: [story => <div style={{ padding: '2rem' }}>{story()}</div>],
   argTypes: {
     onChange: { action: 'onChange' }
+  },
+  args: {
+    loading: false,
+    isInvalid: false
   }
 }
 
-export const Default = () => <FileInput />
+export const Default = args => <FileInput {...args} />
