deca0b60cd7db94675253fe6cf193dcdab83d7e1
Aleksandr Kunin <skyksandr@gmail.com>
2021-01-10 16:57:13 +0300

    Add AppShell to storybook and fix found errors in styles

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,22 @@
+import React from 'react'
+import { MemoryRouter as Router } from 'react-router-dom'
+
 import 'styles/globalStyles.scss'
+import TranslationsProvider from '../app/javascript/components/TranslationsProvider'
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  layout: 'fullscreen'
 }
+
+export const decorators = [
+  Story => (
+    <Router>
+      <div id="modal-root" />
+      <div id="dropdowns-root" />
+      <TranslationsProvider>
+        <Story />
+      </TranslationsProvider>
+    </Router>
+  )
+]
---
+++ app/javascript/components/AppShell/AppShell.stories.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+
+import { createStore } from 'redux/store'
+import PageWrapper from 'components/PageWrapper'
+import AppShell from './'
+
+const session = {
+  loaded: true,
+  currentUser: {
+    authorized: true,
+    userId: 3,
+    email: 'aleksandr@kunin.ru',
+    profileId: 3,
+    name: 'Aleksandr Kunin',
+    countryId: 3,
+    photo: {
+      thumb: 'https://loremflickr.com/34/34/selfie'
+    }
+  }
+}
+
+export default {
+  title: 'components/AppShell',
+  component: AppShell
+}
+
+export const NotLoggedIn = () => (
+  <Provider store={createStore()}>
+    <AppShell>App</AppShell>
+  </Provider>
+)
+
+export const LoggedIn = () => (
+  <Provider store={createStore({ session })}>
+    <AppShell>App</AppShell>
+  </Provider>
+)
+
+export const LoadingState = () => (
+  <Provider store={createStore()}>
+    <AppShell>
+      <PageWrapper status="loading">App</PageWrapper>
+    </AppShell>
+  </Provider>
+)
