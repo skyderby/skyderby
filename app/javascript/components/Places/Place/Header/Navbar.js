6f8ca65639acb91b6613843724e0effbd13d44de
Aleksandr Kunin <skyksandr@gmail.com>
2021-05-20 14:28:45 +0300

    Place page - header

---
+++ app/javascript/components/Places/Place/Header/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import PageNavbar from 'components/PageNavbar'
+import CogIcon from 'icons/cog.svg';
+
+const Navbar = ({ place }) => {
+  const placeUrl = `/places/${place.id}`
+
+  return (
+    <PageNavbar>
+      <PageNavbar.Item>
+        <NavLink exact to={placeUrl}>
+          <span>Overview</span>
+        </NavLink>
+      </PageNavbar.Item>
+
+      <PageNavbar.Item>
+        <NavLink exact to={`${placeUrl}/videos`}>
+          <span>Videos</span>
+        </NavLink>
+      </PageNavbar.Item>
+
+      <PageNavbar.Item>
+        <NavLink exact to={`${placeUrl}/tracks`}>
+          <span>Tracks</span>
+        </NavLink>
+      </PageNavbar.Item>
+
+      <PageNavbar.Spacer />
+
+      <PageNavbar.Item right>
+        <NavLink to={`${placeUrl}/edit`}>
+          <span>
+            <CogIcon />
+            Edit
+          </span>
+        </NavLink>
+      </PageNavbar.Item>
+    </PageNavbar>
+  )
+}
+
+Navbar.propTypes = {
+  place: PropTypes.shape({
+    id: PropTypes.number.isRequired
+  }).isRequired
+}
+
+export default Navbar
--- app/models/concerns/place/photos.rb
+++ app/models/concerns/place/photos.rb
@@ -9,5 +9,25 @@ class Place < ApplicationRecord
     def photo
       photos.first || Photo.new
     end
+
+    def photo_url
+      photo.image.present? ? photo.image.url(:large) : google_map_image
+    end
+
+    def google_map_image
+      endpoint = 'https://maps.googleapis.com/maps/api/staticmap'
+      coordinates = [latitude, longitude].join(',')
+      params = %W[
+        center=#{coordinates}
+        zoom=13
+        maptype=hybrid
+        size=640x250
+        scale=2
+        markers=color:red|#{coordinates}
+        key=#{ENV['MAPS_API_KEY']}
+      ].join('&')
+
+      "#{endpoint}?#{params}"
+    end
   end
 end
