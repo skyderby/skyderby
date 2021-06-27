83a03cd4d89e056612a0a1f4bb8b93e3d56ed52d
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-27 23:33:26 +0300

    Speed skydiving competition - form

---
+++ app/javascript/components/Events/SpeedSkydivingCompetition/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import NewEvent from './NewEvent'
+import Show from './Show'
+
+const SpeedSkydivingCompetition = ({ match }) => {
+  return (
+    <Switch>
+      <Route exact path={`${match.path}/new`} component={NewEvent} />
+      <Route path={`${match.path}/:id`} component={Show} />
+    </Switch>
+  )
+}
+
+SpeedSkydivingCompetition.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired
+  }).isRequired
+}
+
+export default SpeedSkydivingCompetition
