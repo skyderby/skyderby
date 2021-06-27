b38267dc8f793d1585127bfbcd1cf3880a89a6ba
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-27 23:33:04 +0300

    Refactor PlacesSelect and related queries

--- app/javascript/components/Events/PerformanceCompetition/index.js
+++ app/javascript/components/Events/PerformanceCompetition/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
+import PropTypes from 'prop-types'
 
 import NewEvent from './NewEvent'
 import Show from './Show'
 
-const PerformanceCompetition = () => (
+const PerformanceCompetition = ({ match }) => (
   <Switch>
-    <Route exact path="/events/performance/new" component={NewEvent} />
-    <Route path="/events/performance/:id" component={Show} />
+    <Route exact path={`${match.path}/new`} component={NewEvent} />
+    <Route path={`${match.path}/:id`} component={Show} />
   </Switch>
 )
+
+PerformanceCompetition.propTypes = {
+  match: PropTypes.shape({
+    path: PropTypes.string.isRequired
+  }).isRequired
+}
 
 export default PerformanceCompetition
