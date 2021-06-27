230c02cb20984ac9e74031935ff14257cf9f0505
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-27 23:33:26 +0300

    Speed skydiving competition - form

--- app/javascript/components/Events/index.js
+++ app/javascript/components/Events/index.js
@@ -4,13 +4,15 @@ import { Switch, Route } from 'react-router-dom'
 import EventsIndex from './EventsIndex'
 import EventTypeSelect from './EventTypeSelect'
 import PerformanceCompetition from './PerformanceCompetition'
+import SpeedSkydivingCompetition from './SpeedSkydivingCompetition'
 
 const Events = () => {
   return (
     <Switch>
       <Route exact path="/events" component={EventsIndex} />
+      <Route exact path="/events/new" component={EventTypeSelect} />
       <Route path="/events/performance" component={PerformanceCompetition} />
-      <Route path="/events/new" component={EventTypeSelect} />
+      <Route path="/events/speed_skydiving" component={SpeedSkydivingCompetition} />
     </Switch>
   )
 }
