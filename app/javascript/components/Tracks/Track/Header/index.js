f168a51ede33447f64cec02faaf22153ca788d42
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-29 11:51:13 +0300

    Refactor PlaceLabel

--- app/javascript/components/Tracks/Track/Header/index.js
+++ app/javascript/components/Tracks/Track/Header/index.js
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types'
 
 import Profile from './Profile'
 import Suit from './Suit'
-import Place from './Place'
 import Navbar from './Navbar'
 import styles from './styles.module.scss'
+import PlaceLabel from 'components/PlaceLabel'
 
 const Header = ({ track }) => (
   <div className={styles.container}>
     <div className={styles.content}>
       <div className={styles.row}>
         <Profile profileId={track.profileId} pilotName={track.pilotName} />
-        <Place placeId={track.placeId} location={track.location} />
+        <PlaceLabel withIcon placeId={track.placeId} fallbackName={track.location} />
         <Suit suitId={track.suitId} suitName={track.suitName} />
         <div className={styles.trackId}>#{track.id}</div>
       </div>
