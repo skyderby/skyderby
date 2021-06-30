d554657534cf9212b084e6bc64d54989e071a483
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-30 23:31:06 +0300

    Cleanup and refactor session and login from redux

--- app/javascript/pages/users/SuccessRegistration.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import SuccessRegistration from 'components/Users/SuccessRegistration'
-
-const SuccessRegistrationPage = () => <SuccessRegistration />
-
-export default SuccessRegistrationPage
