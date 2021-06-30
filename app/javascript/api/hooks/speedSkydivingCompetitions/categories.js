3d3b10de8f13bdb92a4aae0bc22132caa6d66f57
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-30 18:19:14 +0300

    Edit categories, deleting, moving up/down

--- app/javascript/api/hooks/speedSkydivingCompetitions/categories.js
+++ app/javascript/api/hooks/speedSkydivingCompetitions/categories.js
@@ -3,10 +3,17 @@ import { useQuery, useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 
 const endpoint = eventId => `/api/v1/speed_skydiving_competitions/${eventId}/categories`
+const categoryUrl = (eventId, id) => `${endpoint(eventId)}/${id}`
+const categoryPositionUrl = (eventId, id) => `${categoryUrl(eventId, id)}/position`
 
 const getCategories = eventId => axios.get(endpoint(eventId))
 const createCategory = ({ eventId, ...category }) =>
   axios.post(endpoint(eventId), { category })
+const updateCategory = ({ eventId, id, ...category }) =>
+  axios.put(categoryUrl(eventId, id), { category })
+const deleteCategory = ({ eventId, id }) => axios.delete(categoryUrl(eventId, id))
+const updateCategoryPosition = ({ eventId, id, direction }) =>
+  axios.put(categoryPositionUrl(eventId, id), { direction })
 
 const queryKey = eventId => ['speedSkydivingCompetitions', eventId, 'categories']
 
@@ -34,6 +41,38 @@ export const useNewCategoryMutation = () => {
     onSuccess(response, { eventId }) {
       const data = queryClient.getQueryData(queryKey(eventId))
       queryClient.setQueryData(queryKey(eventId), [...data, response.data])
+    }
+  })
+}
+
+export const useEditCategoryMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(updateCategory, {
+    onSuccess(response, { eventId, id }) {
+      queryClient.setQueryData(queryKey(eventId), categories =>
+        categories.map(category => (category.id === id ? response.data : category))
+      )
+    }
+  })
+}
+
+export const useDeleteCategoryMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(deleteCategory, {
+    onSuccess(response, { eventId }) {
+      return queryClient.invalidateQueries(queryKey(eventId))
+    }
+  })
+}
+
+export const useChangePositionMutation = () => {
+  const queryClient = useQueryClient()
+
+  return useMutation(updateCategoryPosition, {
+    onSuccess(response, { eventId }) {
+      return queryClient.invalidateQueries(queryKey(eventId))
     }
   })
 }
