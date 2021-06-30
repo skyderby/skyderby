aebdc88246f7e68e36d34390d1006cc100290c9c
Aleksandr Kunin <skyksandr@gmail.com>
2021-06-30 18:19:14 +0300

    Edit categories, deleting, moving up/down

--- app/models/speed_skydiving_competition/category.rb
+++ app/models/speed_skydiving_competition/category.rb
@@ -5,9 +5,18 @@ class SpeedSkydivingCompetition::Category < ApplicationRecord
            inverse_of: :category,
            dependent: :restrict_with_error
 
+  scope :sorted, ->{ order(:position) }
   validates :name, presence: true
 
   before_create :set_position
+
+  def move_upper
+    swap_position_with previous_category
+  end
+
+  def move_lower
+    swap_position_with next_category
+  end
 
   private
 
@@ -16,4 +25,18 @@ class SpeedSkydivingCompetition::Category < ApplicationRecord
   end
 
   def last_position_within_event = event.categories.maximum(:position) || 0
+
+  def swap_position_with(other_section)
+    return unless other_section
+
+    tmp_position = position
+    ActiveRecord::Base.transaction do
+      update!(position: other_section.position)
+      other_section.update!(position: tmp_position)
+    end
+  end
+
+  def next_category = event.categories.where('position > ?', position).limit(1).order(:position).first
+
+  def previous_category = event.categories.where('position < ?', position).limit(1).order(position: :desc).first
 end
