3af57abc04c5827d80d93f451d84caf43c0f8432
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2021-07-03 21:43:43 +0300

    Add new form for places

---
+++ app/javascript/components/Places/Form/index.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Formik, Field, ErrorMessage } from 'formik'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import ErrorText from 'components/ui/ErrorMessage'
+import { useI18n } from 'components/TranslationsProvider'
+import CountrySelect from 'components/CountrySelect'
+import RadioButtonGroup from 'components/ui/RadioButtonGroup'
+import validationSchema from './validationSchema'
+import styles from './styles.module.scss'
+
+const Form = props => {
+  const { t } = useI18n()
+
+  return (
+    <Formik {...props} validationSchema={validationSchema}>
+      {({ handleSubmit }) => (
+        <form onSubmit={handleSubmit}>
+          <div className={styles.formFields}>
+            <label>{t('activerecord.attributes.place.name')}</label>
+            <div>
+              <Field name="name" className={styles.input} />
+              <ErrorMessage name="name" component={ErrorText} />
+            </div>
+
+            <label>{t('activerecord.attributes.place.kind')}</label>
+            <Field
+              as={RadioButtonGroup}
+              name="kind"
+              options={[
+                {
+                  value: 'skydive',
+                  label: t('activerecord.attributes.place.kinds.skydive')
+                },
+                {
+                  value: 'base',
+                  label: t('activerecord.attributes.place.kinds.base')
+                }
+              ]}
+            />
+
+            <label>{t('activerecord.attributes.place.country')}</label>
+            <div>
+              <Field name="countryId">
+                {({ field: { name, ...props }, form: { setFieldValue } }) => (
+                  <CountrySelect
+                    {...props}
+                    aria-label={t('activerecord.attributes.place.country.placeholder')}
+                    onChange={({ value }) => setFieldValue(name, value)}
+                  />
+                )}
+              </Field>
+              <ErrorMessage name="placeId" component={ErrorText} />
+            </div>
+
+            <label>{t('activerecord.attributes.place.latitude')}</label>
+            <div>
+              <Field name="latitude" className={styles.input} />
+              <ErrorMessage name="latitude" component={ErrorText} />
+            </div>
+
+            <label>{t('activerecord.attributes.place.longitude')}</label>
+            <div>
+              <Field name="longitude" className={styles.input} />
+              <ErrorMessage name="longitude" component={ErrorText} />
+            </div>
+
+            <label>{t('activerecord.attributes.place.msl')}</label>
+            <div>
+              <Field name="msl" className={styles.input} />
+              <ErrorMessage name="msl" component={ErrorText} />
+            </div>
+          </div>
+
+          <div className={styles.footer}>
+            <button type="submit" className={styles.primaryButton}>
+              {t('general.save')}
+            </button>
+            <Link to="/events/new" className={styles.secondaryButton}>
+              {t('general.cancel')}
+            </Link>
+          </div>
+        </form>
+      )}
+    </Formik>
+  )
+}
+
+Form.propTypes = {
+  initialValues: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    kind: PropTypes.oneOf(['skydive', 'base']).isRequired,
+    latitude: PropTypes.number.isRequired,
+    longitude: PropTypes.number.isRequired,
+    msl: PropTypes.number.isRequired
+  })
+}
+
+export default Form
--- db/schema.rb
+++ db/schema.rb
@@ -2,8 +2,8 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
@@ -148,24 +148,23 @@ ActiveRecord::Schema.define(version: 2022_05_21_092055) do
 
   create_table "events", id: :serial, force: :cascade do |t|
     t.string "name", limit: 510
+    t.integer "range_from"
+    t.integer "range_to"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer "status", default: 0
+    t.integer "profile_id"
     t.integer "place_id"
     t.boolean "is_official"
     t.integer "rules", default: 0
     t.date "starts_at"
     t.boolean "wind_cancellation", default: false
     t.integer "visibility", default: 0
-    t.integer "responsible_id"
-    t.integer "range_from"
-    t.integer "range_to"
     t.integer "number_of_results_for_total"
+    t.integer "responsible_id"
     t.integer "designated_lane_start", default: 1, null: false
-    t.boolean "apply_penalty_to_score"
-    t.boolean "use_teams"
-    t.bigint "profile_id"
-    t.index ["profile_id"], name: "index_events_on_profile_id"
+    t.boolean "apply_penalty_to_score", default: false, null: false
+    t.boolean "use_teams", default: false, null: false
   end
 
   create_table "manufacturers", id: :serial, force: :cascade do |t|
@@ -673,6 +672,7 @@ ActiveRecord::Schema.define(version: 2022_05_21_092055) do
   add_foreign_key "event_competitors", "event_teams", column: "team_id"
   add_foreign_key "event_competitors", "profiles"
   add_foreign_key "event_results", "tracks"
+  add_foreign_key "events", "profiles"
   add_foreign_key "performance_competition_series", "users", column: "responsible_id"
   add_foreign_key "performance_competition_series_included_competitions", "events"
   add_foreign_key "performance_competition_series_included_competitions", "performance_competition_series"
