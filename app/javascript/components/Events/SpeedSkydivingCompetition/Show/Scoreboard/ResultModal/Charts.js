1a33684af9194bd8c105ff4e09b71ccfa6727b18
Aleksandr Kunin <skyksandr@gmail.com>
2021-07-09 13:28:22 +0300

    Add vertical accuracy to result chart

--- app/javascript/components/Events/SpeedSkydivingCompetition/Show/Scoreboard/ResultModal/Charts.js
+++ app/javascript/components/Events/SpeedSkydivingCompetition/Show/Scoreboard/ResultModal/Charts.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { parseISO, differenceInMilliseconds } from 'date-fns'
 import PropTypes from 'prop-types'
 
+import { useI18n } from 'components/TranslationsProvider'
 import { useTrackPointsQuery } from 'api/hooks/tracks/points'
 import TrackCharts from 'components/TrackCharts/CombinedChart'
 import Highchart from 'components/Highchart'
 import Modal from 'components/ui/Modal'
-import { useI18n } from 'components/TranslationsProvider'
 import styles from './styles.module.scss'
 
 const breakoffAltitude = 1707 // 5600 ft
 const windowHeight = 2256 // 7400 ft
+const validationWindowHeight = 1000
 
 const findPositionForAltitude = (points, altitude) => {
   const idx = points.findIndex(point => point.altitude <= altitude)
@@ -36,61 +37,95 @@ const findPlotbandPosition = (firstPoint, windowStart, windowEnd) => {
   }
 }
 
+const buildAccuracySeries = (points, windowEndAltitude) => {
+  const validationWindowStart = windowEndAltitude + validationWindowHeight
+
+  const data = points
+    .filter(
+      point =>
+        point.altitude <= validationWindowStart && point.altitude >= windowEndAltitude
+    )
+    .map(point => [point.flTime - points[0].flTime, point.verticalAccuracy])
+
+  return {
+    name: 'Vertical Accuracy',
+    type: 'column',
+    yAxis: 3,
+    zones: [
+      { value: 0, color: '#ccc' },
+      { value: 3.25, color: '#ccc' },
+      { color: 'red' }
+    ],
+    data
+  }
+}
+
 const Charts = ({ event, result, deleteResult, hide, tabBar }) => {
   const { t } = useI18n()
   const { data: points = [], isLoading } = useTrackPointsQuery(result.trackId, {
     originalFrequency: true
   })
 
-  if (isLoading) return null
-
   const windowEndAltitude = Math.max(result.exitAltitude - windowHeight, breakoffAltitude)
-  const plotLineValue = findPositionForAltitude(points, windowEndAltitude)
-  const plotBandPosition = findPlotbandPosition(
-    points[0],
-    result.windowStartTime,
-    result.windowEndTime
-  )
+
+  const plotLineValue = isLoading
+    ? null
+    : findPositionForAltitude(points, windowEndAltitude)
+
+  const plotBandPosition = isLoading
+    ? null
+    : findPlotbandPosition(points[0], result.windowStartTime, result.windowEndTime)
+
+  const accuracySeries = useMemo(() => buildAccuracySeries(points, windowEndAltitude), [
+    points
+  ])
 
   return (
     <>
       <Modal.Body>
         {tabBar}
+        {tabBar && <hr />}
 
-        <hr />
-
-        <TrackCharts points={points}>
-          {chart => (
-            <>
-              <Highchart.Plotline
-                chart={chart}
-                id="windowEnd"
-                value={plotLineValue}
-                color="red"
-                label={{
-                  text: `End of window - ${windowEndAltitude.toFixed()} ${t('units.m')}`,
-                  style: { color: 'red' },
-                  y: 100
-                }}
-              />
-              <Highchart.Plotband
-                id="result"
-                chart={chart}
-                color="rgba(0, 150, 0, 0.25)"
-                zIndex={8}
-                label={{
-                  text: `Best speed: ${result.result.toFixed(2)} ${t('units.kmh')}`,
-                  rotation: 90,
-                  textAlign: 'left',
-                  y: 100
-                }}
-                {...plotBandPosition}
-              />
-            </>
-          )}
-        </TrackCharts>
+        <div className={styles.trackChart}>
+          <TrackCharts points={points} additionalSeries={[accuracySeries]}>
+            {chart => (
+              <>
+                {plotLineValue && (
+                  <Highchart.Plotline
+                    chart={chart}
+                    id="windowEnd"
+                    value={plotLineValue}
+                    color="red"
+                    label={{
+                      text: `End of window - ${windowEndAltitude.toFixed()} ${t(
+                        'units.m'
+                      )}`,
+                      style: { color: 'red' },
+                      y: 100
+                    }}
+                  />
+                )}
+                {plotBandPosition && (
+                  <Highchart.Plotband
+                    id="result"
+                    chart={chart}
+                    color="rgba(0, 150, 0, 0.25)"
+                    zIndex={8}
+                    label={{
+                      text: `Best speed: ${result.result.toFixed(2)} ${t('units.kmh')}`,
+                      rotation: 90,
+                      textAlign: 'left',
+                      y: 100
+                    }}
+                    {...plotBandPosition}
+                  />
+                )}
+              </>
+            )}
+          </TrackCharts>
+        </div>
       </Modal.Body>
-      <Modal.Footer spaceBetween>
+      <Modal.Footer spaceBetween={event.permissions.canEdit}>
         {event.permissions.canEdit && (
           <button className={styles.deleteButton} onClick={deleteResult}>
             {t('general.delete')}
@@ -118,7 +153,8 @@ Charts.propTypes = {
     result: PropTypes.number.isRequired
   }).isRequired,
   deleteResult: PropTypes.func.isRequired,
-  hide: PropTypes.func.isRequired
+  hide: PropTypes.func.isRequired,
+  tabBar: PropTypes.object
 }
 
 export default Charts
