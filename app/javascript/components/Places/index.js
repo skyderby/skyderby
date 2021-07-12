bbfd51a3ec90be4390a667212bbc543b1ae88254
Dmitriy Vinogradov <di.vinogradov@gmail.com>
2021-07-12 21:40:18 +0300

    Minor fixes after review #248

--- app/javascript/components/Places/index.js
+++ app/javascript/components/Places/index.js
@@ -5,20 +5,25 @@ import AppShell from 'components/AppShell'
 import PlacesIndex from 'components/Places/PlacesIndex'
 import Place from 'components/Places/Place'
 import NewPlace from './NewPlace'
+import Loading from 'components/PageWrapper/Loading'
 import { useCurrentUserQuery } from 'api/hooks/sessions'
 
 const Places = () => {
-  const { data: currentUser } = useCurrentUserQuery()
+  const { data: currentUser, isLoading } = useCurrentUserQuery()
   const canCreatePlace = currentUser?.permissions.canCreatePlace
 
   return (
     <AppShell fullScreen>
-      <Switch>
-        <Route exact path="/places" component={PlacesIndex} />
-        {canCreatePlace && <Route exact path="/places/new" component={NewPlace} />}
-        <Route path="/places/:id(\d+)" component={Place} />
-        <Route component={() => <Redirect to="/places" />} />
-      </Switch>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <Switch>
+          <Route exact path="/places" component={PlacesIndex} />
+          {canCreatePlace && <Route exact path="/places/new" component={NewPlace} />}
+          <Route path="/places/:id(\d+)" component={Place} />
+          <Route component={() => <Redirect to="/places" />} />
+        </Switch>
+      )}
     </AppShell>
   )
 }
