de2dc6833c49bcf8b0e226dfb9ed62025142a869
Aleksandr Kunin <skyksandr@gmail.com>
2021-07-20 20:20:36 +0300

    GPS Performance - Competitors

--- app/controllers/api/v1/performance_competitions/competitors_controller.rb
+++ app/controllers/api/v1/performance_competitions/competitors_controller.rb
@@ -1,9 +1,72 @@
-class Api::V1::PerformanceCompetitions::CompetitorsController < ApplicationController
+class Api::V1::PerformanceCompetitions::CompetitorsController < Api::ApplicationController
+  before_action :set_event
+
   def index
-    event = Event.speed_distance_time.find(params[:performance_competition_id])
+    authorize @event, :show?
 
-    authorize event, :show?
+    @competitors = @event.competitors
+  end
 
-    @competitors = event.competitors
+  def create
+    authorize @event, :update?
+
+    @competitor = @event.competitors.new(competitor_params)
+
+    respond_to do |format|
+      if @competitor.save
+        format.json
+      else
+        format.json { render json: { errors: @competitor.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    authorize @event, :update?
+
+    @competitor = @event.competitors.find(params[:id])
+
+    respond_to do |format|
+      if @competitor.update(competitor_params)
+        format.json
+      else
+        format.json { render json: { errors: @competitor.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    authorize @event, :update?
+
+    @competitor = @event.competitors.find(params[:id])
+
+    respond_to do |format|
+      if @competitor.destroy
+        format.json
+      else
+        format.json { render json: { errors: @competitor.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.speed_distance_time.find(params[:performance_competition_id])
+  end
+
+  def competitor_params
+    permitted_params = [
+      :profile_id,
+      :category_id,
+      :suit_id,
+      :assigned_number,
+      { profile_attributes: [:name, :country_id] }
+    ]
+
+    params.require(:competitor).permit(permitted_params).tap do |attrs|
+      attrs[:profile_attributes][:owner] = @event if attrs[:profile_attributes].present?
+      attrs[:section_id] = attrs.delete(:category_id)
+    end
   end
 end
