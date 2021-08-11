eb7c7b57f50009365a657ba6cb3896a5c0d3b12f
Aleksandr Kunin <skyksandr@gmail.com>
2021-08-11 11:16:20 +0700

    Rounds - creating and deleting

--- app/controllers/api/v1/performance_competitions/rounds_controller.rb
+++ app/controllers/api/v1/performance_competitions/rounds_controller.rb
@@ -1,9 +1,47 @@
 class Api::V1::PerformanceCompetitions::RoundsController < ApplicationController
+  before_action :set_event
+
   def index
-    event = Event.speed_distance_time.find(params[:performance_competition_id])
+    authorize @event, :show?
 
-    authorize event, :show?
+    @rounds = @event.rounds.order(:number, :created_at)
+  end
 
-    @rounds = event.rounds.order(:number, :created_at)
+  def create
+    authorize @event, :update?
+
+    @round = @event.rounds.new(round_params)
+
+    respond_to do |format|
+      if @round.save
+        format.json
+      else
+        format.json { render json: { errors: @round.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    authorize @event, :update?
+
+    @round = @event.rounds.find(params[:id])
+
+    respond_to do |format|
+      if @round.destroy
+        format.json
+      else
+        format.json { render json: { errors: @round.errors }, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def set_event
+    @event = Event.speed_distance_time.find(params[:performance_competition_id])
+  end
+
+  def round_params
+    params.require(:round).permit(:discipline)
   end
 end
