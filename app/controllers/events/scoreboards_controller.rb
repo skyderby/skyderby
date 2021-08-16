04e7deb5d1056de062fb47105cb44257f46f2576
Aleksandr Kunin <skyksandr@gmail.com>
2021-08-16 08:49:10 +0700

    Downgrade jquery, sketch xml standings, RPF for RUS

--- app/controllers/events/scoreboards_controller.rb
+++ app/controllers/events/scoreboards_controller.rb
@@ -2,22 +2,34 @@ module Events
   class ScoreboardsController < ApplicationController
     include EventScoped
 
+    before_action :set_event, :set_scoreboard
+
     def show
-      load_event(params[:event_id])
-
-      authorize @event
-
-      @scoreboard = Events::Scoreboards.for(@event, scoreboard_params(@event))
-
       fresh_when etags_for(@event), last_modified: @event.updated_at
 
       respond_to do |format|
         format.js
         format.html { redirect_to @event }
+        format.xml do
+          formatted_date = Time.zone.now.to_date.iso8601
+          response.headers['Content-Disposition'] =
+            "attachment; filename=#{formatted_date} - #{@event.name.to_param}.xml"
+        end
         format.xlsx do
-          response.headers['Content-Disposition'] = "attachment; filename='#{@event.name.to_param}.xlsx'"
+          response.headers['Content-Disposition'] =
+            "attachment; filename=#{@event.name.to_param}.xlsx"
         end
       end
+    end
+
+    private
+
+    def set_event
+      @event = authorize Event.find(params[:event_id])
+    end
+
+    def set_scoreboard
+      @scoreboard = Events::Scoreboards.for(@event, scoreboard_params(@event))
     end
   end
 end
---
+++ app/views/events/scoreboards/show.xml.builder
@@ -0,0 +1,4 @@
+xml.instruct!
+xml.EventResult do
+  xml.UniqueCode 'f584841e-28e1-4fd7-b42d-b81c080e9230'
+end
