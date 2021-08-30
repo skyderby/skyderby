4505b7d2f39fb7e1fb8094e41ba28b4540f37993
Aleksandr Kunin <skyksandr@gmail.com>
2021-08-30 19:04:48 +0300

    Cover initial track visibility with system tests

--- app/views/api/v1/speed_skydiving_competitions/competitors/index.json.jbuilder
+++ app/views/api/v1/speed_skydiving_competitions/competitors/index.json.jbuilder
@@ -1,1 +1,9 @@
-json.array! @competitors, partial: 'competitor', as: :competitor
+json.items @competitors, partial: 'competitor', as: :competitor
+
+json.relations do
+  profiles = @competitors.map(&:profile).compact
+  json.profiles profiles, partial: 'api/v1/profiles/profile', as: :profile
+
+  countries = profiles.map(&:country).compact
+  json.countries countries, partial: 'api/v1/countries/country', as: :country
+end
---
+++ spec/system/events/speed_skydiving/tracks_visibility_spec.rb
@@ -0,0 +1,69 @@
+describe 'Events - Speed Skydiving' do
+  let(:event) { speed_skydiving_competitions(:nationals) }
+
+  describe 'Initial track visibility' do
+    let(:competitor) { speed_skydiving_competition_competitors(:hinton) }
+
+    it 'new upload is public for public event' do
+      event.public_event!
+
+      sign_in users(:event_responsible)
+      visit "/events/speed_skydiving/#{event.id}"
+      submit_result(competitor, 1, file_fixture('tracks/speed_skydiving_411.csv'))
+
+      expect(page).to have_css('button', text: '411.71')
+
+      result = event.results.find_by \
+        competitor: competitor,
+        round: speed_skydiving_competition_rounds(:nationals_round_1)
+
+      expect(result.track.public_track?).to be_truthy
+    end
+
+    it 'new upload is unlisted for unlisted event' do
+      event.unlisted_event!
+
+      sign_in users(:event_responsible)
+      visit "/events/speed_skydiving/#{event.id}"
+      submit_result(competitor, 1, file_fixture('tracks/speed_skydiving_411.csv'))
+
+      expect(page).to have_css('button', text: '411.71')
+
+      result = event.results.find_by \
+        competitor: competitor,
+        round: speed_skydiving_competition_rounds(:nationals_round_1)
+
+      expect(result.track.unlisted_track?).to be_truthy
+    end
+
+    it 'new upload is unlisted for private event' do
+      event.private_event!
+
+      sign_in users(:event_responsible)
+      visit "/events/speed_skydiving/#{event.id}"
+      submit_result(competitor, 1, file_fixture('tracks/speed_skydiving_411.csv'))
+
+      expect(page).to have_css('button', text: '411.71')
+
+      result = event.results.find_by \
+        competitor: competitor,
+        round: speed_skydiving_competition_rounds(:nationals_round_1)
+
+      expect(result.track.unlisted_track?).to be_truthy
+    end
+  end
+
+  def submit_result(competitor, round, file)
+    find(
+      :button,
+      title: "Submit result for #{competitor.name} in round #{round}",
+      visible: false
+    ).tap do |button|
+      button.hover
+      button.click
+    end
+
+    find('[type="file"]', visible: false).attach_file file, make_visible: true
+    find_button(I18n.t('general.save')).click
+  end
+end
