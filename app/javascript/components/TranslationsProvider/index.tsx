7a6d74133438a7865ded379a2e4a192aefcdf36e
Aleksandr Kunin <skyksandr@gmail.com>
2021-09-01 18:04:44 +0300

    TypeScript: Enable strict mode

--- app/javascript/components/TranslationsProvider/index.tsx
+++ app/javascript/components/TranslationsProvider/index.tsx
@@ -12,7 +12,7 @@ import { enUS, ru, it, fr, es, de } from 'date-fns/locale'
 import Cookie from 'js-cookie'
 import supportedLocales from 'virtual-modules/i18n/supportedLocales'
 
-const dateLocales: { [key: string]: Locale } = {
+const dateLocales: { [key in ApplicationLocale]: Locale } = {
   en: enUS,
   ru,
   it,
@@ -21,39 +21,35 @@ const dateLocales: { [key: string]: Locale } = {
   de
 }
 
-interface TranslationEntry {
-  [key: string]: string | TranslationEntry
-}
-
-type Translations = {
-  [locale: string]: TranslationEntry
-}
-
 type TranslationsDict = {
-  [locale: string]: Translations
+  [locale in ApplicationLocale]?: Translations
 }
 
 type I18nContextType = {
   t: (key: string) => string
-  supportedLocales: string[]
-  locale?: string
+  supportedLocales: ApplicationLocale[]
+  locale?: ApplicationLocale
   formatDate?: (date: Date, format: string) => string
-  changeLocale?: (locale: string) => void
+  changeLocale?: (locale: ApplicationLocale) => void
 }
-
-const defaultLocale = 'en'
 
 const TranslationsContext = createContext<I18nContextType>({
   t: I18n.t,
   supportedLocales
 })
 
-const getInitialLocale = () => {
-  const chosenLocale = Cookie.get('locale')
-  if (chosenLocale && supportedLocales.includes(chosenLocale)) return chosenLocale
+const isSupportedLocale = (key: string): key is ApplicationLocale => {
+  return supportedLocales.includes(key as ApplicationLocale)
+}
+
+const getInitialLocale = (): ApplicationLocale => {
+  const defaultLocale = 'en'
+
+  const chosenLocale = String(Cookie.get('locale'))
+  if (isSupportedLocale(chosenLocale)) return chosenLocale
 
   const browserLocale = window.navigator.language.slice(0, 2).toLowerCase()
-  if (supportedLocales.includes(browserLocale)) return browserLocale
+  if (isSupportedLocale(browserLocale)) return browserLocale
 
   return defaultLocale
 }
@@ -64,11 +60,11 @@ const TranslationsProvider = ({
   children: React.ReactNode
 }): JSX.Element | null => {
   const initialized = useRef(false)
-  const [locale, setLocale] = useState(getInitialLocale)
+  const [locale, setLocale] = useState<ApplicationLocale>(getInitialLocale)
 
   const [translations, setTranslations] = useState<TranslationsDict>({})
 
-  const changeLocale = (newLocale: string) => {
+  const changeLocale = (newLocale: ApplicationLocale) => {
     if (!supportedLocales.includes(newLocale)) {
       console.warn(
         `Unsupported locale ${newLocale}, supported locales are: ${supportedLocales}`
@@ -82,7 +78,7 @@ const TranslationsProvider = ({
   }
 
   const loadTranslations = useCallback(
-    async newLocale => {
+    async (newLocale: ApplicationLocale) => {
       if (translations[newLocale]) return
 
       const records = await import(`virtual-modules/i18n/translations/${newLocale}`)
@@ -101,7 +97,7 @@ const TranslationsProvider = ({
 
   if (!translations[locale]) return null
 
-  I18n.translations = translations[locale]
+  I18n.translations = Object.assign(I18n.translations, translations[locale])
   I18n.locale = locale
 
   const formatDate = (date: Date, formatString: string): string =>
