bb70fd28960c5854354e9ce39d8e4e3d529b4518
Aleksandr Kunin <skyksandr@gmail.com>
2021-10-20 09:53:44 +0300

    Add Privacy Policy page on English

---
+++ app/javascript/components/Legal/Privacy/privacyLabel.en.ts
@@ -0,0 +1,91 @@
+export default [
+  {
+    title: 'Collection and Sale of Data',
+    entries: [
+      {
+        question: 'Do we sell your personal information?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we share aggregate information?',
+        answer: 'Yes'
+      },
+      {
+        question: 'Do we sell aggregate information?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we share your data with third party API partners?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we use your contact list?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we delete your data when you request account deletion?',
+        answer: 'No'
+      },
+      {
+        question:
+          'Do we retain your data for as long as we need it unless you request deletion?',
+        answer: 'Yes'
+      }
+    ]
+  },
+  {
+    title: 'Privacy Tools and Controls',
+    entries: [
+      {
+        question: 'Can you control who sees your activity and content?',
+        answer: 'Yes'
+      },
+      {
+        question: 'Are your activity set to “Public” by default?',
+        answer: 'Yes'
+      },
+      {
+        question: 'Can you download and delete your data?',
+        answer: 'Yes'
+      },
+      {
+        question: 'Do all members worldwide have the same suite of tools and controls?',
+        answer: 'Yes'
+      }
+    ]
+  },
+  {
+    title: 'Tracking',
+    entries: [
+      {
+        question: 'Do we track your device location when you use Skyderby?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we use cookies?',
+        answer: 'Yes'
+      },
+      {
+        question: 'Do we track your browsing activities on other sites?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we listen to you using your device microphone?',
+        answer: 'No'
+      }
+    ]
+  },
+  {
+    title: 'Communication',
+    entries: [
+      {
+        question: 'Do we send you marketing communications?',
+        answer: 'No'
+      },
+      {
+        question: 'Do we send you push notifications?',
+        answer: 'No'
+      }
+    ]
+  }
+]
