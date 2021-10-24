1bb4e3cc299dba170deda2dfb055938cb2a931b6
Aleksandr Kunin <skyksandr@gmail.com>
2021-10-24 11:31:09 +0300

    Reorganize backward compatibility and ping routes

--- config/routes/static.rb
+++
@@ -1,3 +0,0 @@
-match '/competitions', to: 'static_pages#competitions', as: :competitions, via: :get
-match '/about', to: 'static_pages#about', as: :about, via: :get
-get '/ping' => 'static_pages#ping'
