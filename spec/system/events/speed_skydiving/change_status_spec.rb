d576fedcc79ff239c0bfbb9edaa35342c0325fc9
Aleksandr Kunin <skyksandr@gmail.com>
2021-10-28 16:13:43 +0300

    Cover create/edit/status change of speed skydiving event with tests

---
+++ spec/system/events/speed_skydiving/change_status_spec.rb
@@ -0,0 +1,20 @@
+describe 'Changing Speed Skydiving event status' do
+  let(:event) { speed_skydiving_competitions(:nationals) }
+
+  it 'with valid values' do
+    sign_in event.responsible
+    event.draft!
+    visit "/events/speed_skydiving/#{event.id}"
+
+    click_button status_button_text('draft')
+
+    click_button I18n.t('event_status.finished')
+
+    expect(page).to have_css('button', text: status_button_text('finished'))
+    expect(event.reload.status).to eq('finished')
+  end
+
+  def status_button_text(status)
+    "#{I18n.t('activerecord.attributes.event.status')}: #{I18n.t("event_status.#{status}")}"
+  end
+end
