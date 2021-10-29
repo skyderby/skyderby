2988a9e9d134b30d80be39d3bf7f358562f425a1
Aleksandr Kunin <skyksandr@gmail.com>
2021-10-29 09:06:41 +0300

    Fix standings error if result is nil

--- app/models/speed_skydiving_competition/scoreboard/standings.rb
+++ app/models/speed_skydiving_competition/scoreboard/standings.rb
@@ -1,4 +1,4 @@
-class SpeedSkydivingCompetition::Scoreboard::Standings
+class SpeedSkydivingCompetition::Scoreboard:: Standings
   def self.build(*args)
     new(*args).build
   end
@@ -12,7 +12,7 @@ class SpeedSkydivingCompetition::Scoreboard::Standings
   def build
     standings = competitors.map do |competitor|
       competitor_results = results.select { |result| result.competitor == competitor }
-      total = competitor_results.sum(&:result)
+      total = competitor_results.sum { |record| record.result || 0.0 }
       average = competitor_results.any? ? total / competitor_results.size : 0
 
       { competitor: competitor, total: total.round(2), average: average.round(2) }
