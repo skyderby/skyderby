b13e5fcb99c76028a7feb4c944d5272ce4c04949
Aleksandr Kunin <skyksandr@gmail.com>
2021-11-11 14:27:43 +0300

    Fix incorrect caching behaviour in Safari

--- app/javascript/components/AppShell/Navbar/RightMenuDesktop.tsx
+++ app/javascript/components/AppShell/Navbar/RightMenuDesktop.tsx
@@ -12,55 +12,58 @@ import styles from './styles.module.scss'
 const RightMenuDesktop = (): JSX.Element => {
   const { t } = useI18n()
   const [showModal, setShowModal] = useState(false)
-  const { data: currentUser } = useCurrentUserQuery()
+  const { data: currentUser, isLoading } = useCurrentUserQuery()
   const logoutMutation = useLogoutMutation()
 
   const handleLogout = () => logoutMutation.mutate({})
 
   return (
     <ul className={styles.rightMenuDesktop}>
-      {currentUser?.authorized ? (
+      {!isLoading && (
         <>
-          <li className={styles.menuItem}>
-            <button className={styles.uploadTrack} onClick={() => setShowModal(true)}>
-              {t('application.header.upload_track')}
-            </button>
-            <NewTrackForm isShown={showModal} onHide={() => setShowModal(false)} />
-          </li>
+          {currentUser?.authorized ? (
+            <>
+              <li className={styles.menuItem}>
+                <button className={styles.uploadTrack} onClick={() => setShowModal(true)}>
+                  {t('application.header.upload_track')}
+                </button>
+                <NewTrackForm isShown={showModal} onHide={() => setShowModal(false)} />
+              </li>
 
-          <li>
-            <CurrentUser user={currentUser} />
-          </li>
+              <li>
+                <CurrentUser user={currentUser} />
+              </li>
 
-          <li className={styles.menuItem}>
-            <button
-              onClick={handleLogout}
-              className={styles.logoutButton}
-              title={t('application.header.sign_out')}
-            >
-              <ExitIcon />
-            </button>
-          </li>
-        </>
-      ) : (
-        <>
-          <li className={styles.menuItem}>
-            <Link
-              to={location => ({
-                pathname: '/users/sign-in',
-                state: { afterLoginUrl: location.pathname }
-              })}
-            >
-              {t('application.header.sign_in')}
-            </Link>
-          </li>
+              <li className={styles.menuItem}>
+                <button
+                  onClick={handleLogout}
+                  className={styles.logoutButton}
+                  title={t('application.header.sign_out')}
+                >
+                  <ExitIcon />
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className={styles.menuItem}>
+                <Link
+                  to={location => ({
+                    pathname: '/users/sign-in',
+                    state: { afterLoginUrl: location.pathname }
+                  })}
+                >
+                  {t('application.header.sign_in')}
+                </Link>
+              </li>
 
-          <li className={styles.menuItem}>
-            <Link to="/users/sign-up">{t('application.header.sign_up')}</Link>
-          </li>
+              <li className={styles.menuItem}>
+                <Link to="/users/sign-up">{t('application.header.sign_up')}</Link>
+              </li>
+            </>
+          )}
         </>
       )}
-
       <LocaleSelector className={styles.menuItem} />
     </ul>
   )
