23c3a1ceec7bcdcec08bd4c3c71b203b46f96b2f
Aleksandr Kunin <skyksandr@gmail.com>
2021-11-20 12:39:38 +0300

    Remove no longer needed specs

--- spec/controllers/api/application_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-  class ApplicationController < ::ApplicationController
-    before_action :underscore_params!
-
-    def underscore_params!
-      params.deep_transform_keys!(&:underscore)
-    end
-  end
-end
