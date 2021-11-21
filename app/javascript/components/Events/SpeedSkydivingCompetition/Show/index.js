03393f1a4576d9d7f45ecd0867725d55380a7dea
Aleksandr Kunin <skyksandr@gmail.com>
2021-11-21 17:53:40 +0300

    Display error page in case of 403 or 404

--- app/javascript/components/Events/SpeedSkydivingCompetition/Show/index.js
+++ app/javascript/components/Events/SpeedSkydivingCompetition/Show/index.js
@@ -8,13 +8,13 @@ import {
   preloadCompetitors,
   preloadResults,
   preloadRounds,
-  preloadSpeedSkydivingCompetition,
   preloadTeams,
   useSpeedSkydivingCompetitionQuery,
   preloadStandings
 } from 'api/speedSkydivingCompetitions'
 import AppShell from 'components/AppShell'
 import PageLoading from 'components/PageWrapper/Loading'
+import ErrorPage from 'components/ErrorPage'
 import Header from './Header'
 import Scoreboard from './Scoreboard'
 import TeamsScoreboard from './TeamsScoreboard'
@@ -26,44 +26,51 @@ import styles from './styles.module.scss'
 const Show = ({ match }) => {
   const eventId = Number(match.params.eventId)
   const queryClient = useQueryClient()
-  const [isLoading, setIsLoading] = useState(true)
-  const { data: event } = useSpeedSkydivingCompetitionQuery(eventId)
+  const [associationsLoaded, setAssociationsLoaded] = useState(false)
+  const { data: event, isLoading, isError, error } = useSpeedSkydivingCompetitionQuery(
+    eventId
+  )
 
   useEffect(() => {
+    if (isLoading || isError) return
+
     Promise.all([
-      preloadSpeedSkydivingCompetition(eventId, queryClient),
       preloadCategories(eventId, queryClient),
       preloadRounds(eventId, queryClient),
       preloadCompetitors(eventId, queryClient),
       preloadResults(eventId, queryClient),
       preloadTeams(eventId, queryClient),
       preloadStandings(eventId, queryClient)
-    ]).then(() => setIsLoading(false))
-  }, [eventId, queryClient])
+    ]).then(() => setAssociationsLoaded(true))
+  }, [eventId, isLoading, queryClient])
+
+  if (isError) return ErrorPage.forError(error, { linkBack: '/events' })
 
   return (
     <AppShell>
-      {!event || isLoading ? (
+      {isLoading ? (
         <PageLoading />
       ) : (
         <div className={styles.container}>
           <Header event={event} />
 
-          <Switch>
-            <Route exact path={`${match.path}/`} component={Scoreboard} />
-            {event.useOpenScoreboard && (
-              <Route path={`${match.path}/open_event`} component={OpenScoreboard} />
-            )}
-            {event.useTeams && (
-              <Route path={`${match.path}/teams`} component={TeamsScoreboard} />
-            )}
-            {event.permissions.canEdit && (
-              <>
-                <Route path={`${match.path}/downloads`} component={Downloads} />
-                <Route path={`${match.path}/edit`} component={Edit} />
-              </>
-            )}
-          </Switch>
+          {associationsLoaded && (
+            <Switch>
+              <Route exact path={`${match.path}/`} component={Scoreboard} />
+              {event.useOpenScoreboard && (
+                <Route path={`${match.path}/open_event`} component={OpenScoreboard} />
+              )}
+              {event.useTeams && (
+                <Route path={`${match.path}/teams`} component={TeamsScoreboard} />
+              )}
+              {event.permissions.canEdit && (
+                <>
+                  <Route path={`${match.path}/downloads`} component={Downloads} />
+                  <Route path={`${match.path}/edit`} component={Edit} />
+                </>
+              )}
+            </Switch>
+          )}
         </div>
       )}
     </AppShell>
