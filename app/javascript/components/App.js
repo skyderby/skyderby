0a30088cf2ba71f91535cbefe935eb5c82c683ca
Aleksandr Kunin <skyksandr@gmail.com>
2021-11-21 17:53:40 +0300

    Display error page in case of 403 or 404

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -14,7 +14,11 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 60 * 1000,
-      refetchOnMount: false
+      refetchOnMount: false,
+      retry: (failureCount, error) => {
+        if ([401, 403, 404, 500].includes(error?.response?.status)) return false
+        return failureCount < 3
+      }
     }
   }
 })
