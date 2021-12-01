10026f4c5e79a4d56216ed22437e8af88eb6c880
Aleksandr Kunin <skyksandr@gmail.com>
2021-12-01 18:00:02 +0300

    Upgrade typescript-eslint, correct warnings and errors

--- app/javascript/components/Tracks/Track/TrackVideoSettings/index.tsx
+++ app/javascript/components/Tracks/Track/TrackVideoSettings/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Formik } from 'formik'
 
 import { useTrackQuery } from 'api/tracks'
