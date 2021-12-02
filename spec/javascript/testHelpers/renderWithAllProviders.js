e1e2bc3884650cd4784f8c6079d200fcffb8d568
Aleksandr Kunin <skyksandr@gmail.com>
2021-12-02 13:41:26 +0300

    Fix frontend tests

--- spec/javascript/testHelpers/renderWithAllProviders.js
+++ spec/javascript/testHelpers/renderWithAllProviders.js
@@ -3,8 +3,7 @@ import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { Provider } from 'react-redux'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { createMemoryHistory } from 'history'
-import { Router } from 'react-router-dom'
+import { MemoryRouter } from 'react-router-dom'
 
 import { createStore } from 'redux/store'
 import TranslationsProvider from 'components/TranslationsProvider'
@@ -13,19 +12,18 @@ export const queryClient = new QueryClient()
 
 const renderWithAllProviders = (ui, initialState) => {
   const store = createStore(initialState)
-  const history = createMemoryHistory()
 
   const screen = render(
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
         <TranslationsProvider>
-          <Router history={history}>{ui}</Router>
+          <MemoryRouter>{ui}</MemoryRouter>
         </TranslationsProvider>
       </Provider>
     </QueryClientProvider>
   )
 
-  return { ...screen, history }
+  return { ...screen }
 }
 
 export default renderWithAllProviders
