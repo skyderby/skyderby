07a2b8122938b1990ae72a82dd416a97b468cfe5
Aleksandr Kunin <skyksandr@gmail.com>
2021-12-03 10:44:19 +0300

    Fix redirect if user does not have permissions to create place

--- app/javascript/components/Places/index.js
+++ app/javascript/components/Places/index.js
@@ -1,24 +1,16 @@
 import React from 'react'
-import { Route, Routes, Navigate } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 
 import PlacesIndex from 'components/Places/PlacesIndex'
 import Place from 'components/Places/Place'
 import NewPlace from './NewPlace'
-import Loading from 'components/PageWrapper/Loading'
-import { useCurrentUserQuery } from 'api/sessions'
 
 const Places = () => {
-  const { data: currentUser, isLoading } = useCurrentUserQuery()
-  const canCreatePlace = currentUser?.permissions.canCreatePlace
-
-  if (isLoading) return <Loading />
-
   return (
     <Routes>
       <Route index element={<PlacesIndex />} />
       <Route path=":id/*" element={<Place />} />
-      {canCreatePlace && <Route path="new" element={<NewPlace />} />}
-      <Route render={() => <Navigate to="/places" />} />
+      <Route path="new" element={<NewPlace />} />
     </Routes>
   )
 }
