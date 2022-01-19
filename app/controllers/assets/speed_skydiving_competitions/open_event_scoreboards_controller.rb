8a6514030a6e450682d26be902158d4cb8212e07
Aleksandr Kunin <skyksandr@gmail.com>
2022-01-19 10:26:57 +0300

    Basic open standings unloading

--- app/controllers/assets/speed_skydiving_competitions/open_event_scoreboards_controller.rb
+++ app/controllers/assets/speed_skydiving_competitions/open_event_scoreboards_controller.rb
@@ -1,7 +1,32 @@
 module Assets
   module SpeedSkydivingCompetitions
     class OpenEventScoreboardsController < ApplicationController
+      before_action :set_event
+
       def show
+        authorize @event, :download?
+
+        @scoreboard = SpeedSkydivingCompetition::OpenScoreboard.new(@event)
+
+        respond_to do |format|
+          format.xml do
+            response.headers['Content-Disposition'] = "attachment; filename=#{filename}.xml"
+          end
+          format.xlsx do
+            response.headers['Content-Disposition'] = "attachment; filename=#{filename}.xlsx"
+          end
+        end
+      end
+
+      private
+
+      def filename
+        formatted_date = Time.zone.now.to_date.iso8601
+        "#{formatted_date} - #{@event.name.to_param}"
+      end
+
+      def set_event
+        @event = SpeedSkydivingCompetition.find(params[:speed_skydiving_competition_id])
       end
     end
   end
