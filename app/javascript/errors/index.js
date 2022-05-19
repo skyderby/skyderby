1886ff593e8a43d71cb23a34c359830e6a6b9bd4
Aleksandr Kunin <skyksandr@gmail.com>
2022-05-19 18:03:33 +0400

    Cleanup unused files and packages

--- app/javascript/errors/index.js
+++
@@ -1,1 +0,0 @@
-export { default as ForbiddenError } from './ForbiddenError'
--- app/javascript/redux/events/teams/actionTypes.js
+++
@@ -1,9 +0,0 @@
-const prefix = '[event/teams]'
-
-export const LOAD_REQUEST = `${prefix} LOAD/REQUEST`
-export const LOAD_SUCCESS = `${prefix} LOAD/SUCCESS`
-export const LOAD_ERROR = `${prefix} LOAD/ERROR`
-
-export const TEAM_CREATED = `${prefix} CREATE/SUCCESS`
-export const TEAM_UPDATED = `${prefix} UPDATE/SUCCESS`
-export const TEAM_DELETED = `${prefix} DELETE/SUCCESS`
