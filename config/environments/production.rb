0e27bd39ef98b4decab95b21d45ee6bd9f9bc3b5
Aleksandr Kunin <skyksandr@gmail.com>
2022-07-25 15:39:52 +0700

    Add skyderby.io to allowed hosts
    (cherry picked from commit 291c66e3030dc77966496ae30508fdccc9b2d6ac)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,6 +101,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter = :sidekiq
 
+  config.hosts << '.skyderby.io'
   config.hosts << '.skyderby.ru'
   config.hosts << '.skyder.by'
 
