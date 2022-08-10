82e41d75920421ed0f1589869bcd3ea009a8d739
Aleksandr Kunin <skyksandr@gmail.com>
2022-08-10 23:19:50 +0700

    Fix specs

--- spec/system/users/managing_spec.rb
+++ spec/system/users/managing_spec.rb
@@ -24,25 +24,25 @@ describe 'User management' do
     it 'search by name' do
       fill_in 'searchTerm', with: users(:regular_user).name
 
-      expect(page).to have_css('td', text: users(:regular_user).email)
-      expect(page).not_to have_css('td', text: users(:admin).email)
-      expect(page).not_to have_css('td', text: users(:event_responsible).email)
+      expect(page).to have_css('div', text: users(:regular_user).email)
+      expect(page).not_to have_css('div', text: users(:admin).email)
+      expect(page).not_to have_css('div', text: users(:event_responsible).email)
     end
 
     it 'search by email' do
       fill_in 'searchTerm', with: users(:admin).email
 
-      expect(page).to have_css('td', text: users(:admin).email)
-      expect(page).not_to have_css('td', text: users(:regular_user).email)
-      expect(page).not_to have_css('td', text: users(:event_responsible).email)
+      expect(page).to have_css('div', text: users(:admin).email)
+      expect(page).not_to have_css('div', text: users(:regular_user).email)
+      expect(page).not_to have_css('div', text: users(:event_responsible).email)
     end
 
     it 'search by id' do
       fill_in 'searchTerm', with: users(:event_responsible).id
 
-      expect(page).to have_css('td', text: users(:event_responsible).email)
-      expect(page).not_to have_css('td', text: users(:admin).email)
-      expect(page).not_to have_css('td', text: users(:regular_user).email)
+      expect(page).to have_css('div', text: users(:event_responsible).email)
+      expect(page).not_to have_css('div', text: users(:admin).email)
+      expect(page).not_to have_css('div', text: users(:regular_user).email)
     end
   end
 end
