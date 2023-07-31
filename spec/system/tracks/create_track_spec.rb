923db169a09f61e5843a32ee3fb8a76f0974f6b6
Aleksandr Kunin <skyksandr@gmail.com>
2023-07-31 10:42:56 +0700

    Flysight v2 file format support

--- spec/system/tracks/create_track_spec.rb
+++ spec/system/tracks/create_track_spec.rb
@@ -5,6 +5,12 @@ describe 'Upload tracks', js: true do
     expect(page).to have_css('a.btn-tab.active', text: I18n.t('tracks.show.charts'))
   end
 
+  it 'Flysight2 file' do
+    upload_track 'fs2-track.csv'
+
+    expect(page).to have_css('a.btn-tab.active', text: I18n.t('tracks.show.charts'))
+  end
+
   it 'Columbus file' do
     upload_track 'columbus.csv'
 
