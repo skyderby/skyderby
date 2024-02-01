41ad77fe2e8316f6626fceaa8e81d23e24ddb0f6
Aleksandr Kunin <skyksandr@gmail.com>
2024-02-01 14:49:58 +0800

    Search events by name

--- app/controllers/api/v1/events_controller.rb
+++ app/controllers/api/v1/events_controller.rb
@@ -8,6 +8,7 @@ module Api
 
         @events =
           policy_scope(EventList.all)
+          .then { |rel| apply_search(rel) }
           .includes(:place)
           .paginate(page: current_page, per_page: rows_per_page)
       end
@@ -20,6 +21,12 @@ module Api
 
       def set_event
         @event = Event.find(params[:id])
+      end
+
+      def apply_search(events)
+        return events if params[:term].blank?
+
+        events.where('name ILIKE ?', "%#{params[:term]}%")
       end
     end
   end
--- app/javascript/components/Events/EventsIndex/index.tsx
+++ app/javascript/components/Events/EventsIndex/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { startTransition, useRef } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
+import throttle from 'lodash.throttle'
 
-import useEventsQuery, { mapParamsToUrl, extractParamsFromUrl } from 'api/events'
+import useEventsQuery, { mapParamsToUrl } from 'api/events'
 import { useI18n } from 'components/TranslationsProvider'
 import Pagination from 'components/Pagination'
 import PlusIcon from 'icons/plus'
@@ -10,15 +11,41 @@ import styles from './styles.module.scss'
 
 const EventsIndex = (): JSX.Element => {
   const { t } = useI18n()
-  const location = useLocation()
-  const { page } = extractParamsFromUrl(location.search)
+  const [searchParams, setSearchParams] = useSearchParams()
+  const page = Number(searchParams.get('page') || 1)
+  const searchTerm = searchParams.get('term') || ''
+
+  const handleSearchTermInput = useRef(
+    throttle(e => {
+      const value = e.target.value
+      startTransition(() => {
+        setSearchParams(params => {
+          if (value) {
+            params.set('term', e.target.value)
+          } else {
+            params.delete('term')
+          }
+          return params
+        })
+      })
+    }, 200)
+  ).current
+
   const {
     data: { items: events, currentPage, totalPages, permissions }
-  } = useEventsQuery({ page })
+  } = useEventsQuery({ page, searchTerm })
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>{t('application.header.competitions')}</h1>
+
+      <input
+        className={styles.input}
+        type="search"
+        placeholder="Search competition by name..."
+        defaultValue={searchTerm}
+        onInput={handleSearchTermInput}
+      />
 
       {events.map((event, idx) => (
         <Item key={`${event.type}/${event.id}`} event={event} delayIndex={idx} />
