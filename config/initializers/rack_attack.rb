daed36883a438c0a11facc711bd3d0c8962225f5
Aleksandr Kunin <skyksandr@gmail.com>
2024-03-21 13:29:16 +0800

    Fix condition in rack attack

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -2,6 +2,6 @@ class Rack::Attack
   Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
 
   throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip if req.env['warden'].user&.admin?
+    req.ip unless req.env['warden'].user&.admin?
   end
 end
