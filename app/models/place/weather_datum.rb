a17939dee1b01f15ebec0e91b06771a4ca94b05d
Aleksandr Kunin <skyksandr@gmail.com>
2024-05-06 09:23:41 +0800

    Debug

--- app/models/place/weather_datum.rb
+++ app/models/place/weather_datum.rb
@@ -1,8 +1,6 @@
 class Place::WeatherDatum < ApplicationRecord
   belongs_to :place
 
-  before_save :set_altitude, :set_wind_speed
-
   validates :actual_on, :wind_direction, presence: true
   validates :altitude, :wind_speed, numericality: { greater_than_or_equal_to: 0 }
   validates :wind_direction, numericality: { greater_than_or_equal_to: 0, less_than: 360 }
