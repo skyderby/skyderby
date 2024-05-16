8b3435619b606aaee06bf8c4e87b05085532f1f6
Aleksandr Kunin <skyksandr@gmail.com>
2024-05-16 18:01:15 +0900

    fix lint errors

--- app/javascript/api/tracks/videos/index.ts
+++ app/javascript/api/tracks/videos/index.ts
@@ -1,4 +1,5 @@
 export { default as useDeleteVideoMutation } from './useDeleteVideoMutation'
 export { default as useUpdateVideoMutation } from './useUpdateVideoMutation'
 export { default as useTrackVideoQuery } from './useTrackVideoQuery'
-export { videoSchema, TrackVideo } from './common'
+export { videoSchema } from './common'
+export type { TrackVideo } from './common'
