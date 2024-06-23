fe4830d0d3dbb95a1d631ac3470dde54798c09ef
Aleksandr Kunin <skyksandr@gmail.com>
2024-06-23 15:37:40 +0800

    fix rubocop

--- test/services/track_format_detector_test.rb
+++ test/services/track_format_detector_test.rb
@@ -1,16 +1,6 @@
 require 'test_helper'
 
 class TrackFormatDetectorTest < ActiveSupport::TestCase
-  examples = {
-    'two_tracks.gpx' => 'gpx',
-    'dual_xgps160.kml' => 'kml',
-    'wintec.tes' => 'wintec',
-    'flysight.csv' => 'flysight',
-    'fs2-track.csv' => 'flysight2',
-    'columbus.csv' => 'columbus',
-    'cyber_eye.csv' => 'cyber_eye'
-  }.freeze
-
   test 'determines correct format' do
     assert_equal 'gpx', TrackFormatDetector.call(fixture_file_upload('tracks/two_tracks.gpx'), 'gpx')
     assert_equal 'kml', TrackFormatDetector.call(fixture_file_upload('tracks/dual_xgps160.kml'), 'kml')
