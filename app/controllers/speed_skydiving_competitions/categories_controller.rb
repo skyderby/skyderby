6afcf57ea81149faef01b7b573eb17be5a8af361
Aleksandr Kunin <skyksandr@gmail.com>
2024-11-27 11:51:48 +0800

    Create and delete speed skydiving category

--- app/controllers/speed_skydiving_competitions/categories_controller.rb
+++ app/controllers/speed_skydiving_competitions/categories_controller.rb
@@ -1,4 +1,49 @@
 class SpeedSkydivingCompetitions::CategoriesController < ApplicationController
+  before_action :set_event
+
   def new
+    @category = @event.categories.new
+  end
+
+  def create
+    @category = @event.categories.new(category_params)
+
+    if @category.save
+      broadcast_scoreboard
+      render
+    else
+      respond_with_errors(@category)
+    end
+  end
+
+  def destroy
+    @category = @event.categories.find(params[:id])
+
+    if @category.destroy
+      broadcast_scoreboard
+      head :no_content
+    else
+      respond_with_errors(@category)
+    end
+  end
+
+  private
+
+  def category_params = params.require(:category).permit(:name)
+
+  def broadcast_scoreboard
+    Turbo::StreamsChannel.broadcast_replace_to @event, :scoreboard, :editable,
+                                               target: 'scoreboard',
+                                               partial: 'speed_skydiving_competitions/scoreboard',
+                                               locals: { event: @event, editable: true }
+
+    Turbo::StreamsChannel.broadcast_replace_to @event, :scoreboard, :read_only,
+                                               target: 'scoreboard',
+                                               partial: 'speed_skydiving_competitions/scoreboard',
+                                               locals: { event: @event, editable: false }
+  end
+
+  def set_event
+    @event = SpeedSkydivingCompetition.find(params[:speed_skydiving_competition_id])
   end
 end
--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,7 +1,38 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
+  static targets = ['dialog']
+
+  initialize() {
+    super.initialize()
+    this.forceClose = this.forceClose.bind(this)
+  }
+
   connect() {
-    $(document).one('turbo:before-cache', () => $(this.element).modal('hide'))
+    document.addEventListener('turbo:before-render', this.forceClose)
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        this.close()
+      }
+    })
+  }
+
+  disconnect() {
+    document.removeEventListener('turbo:before-render', this.forceClose)
+  }
+
+  close() {
+    this.dialogTarget.setAttribute('closing', '')
+
+    Promise.all(
+      this.dialogTarget.getAnimations().map(animation => animation.finished)
+    ).then(() => {
+      this.dialogTarget.removeAttribute('closing')
+      this.element.remove()
+    })
+  }
+
+  forceClose() {
+    this.element.remove()
   }
 }
