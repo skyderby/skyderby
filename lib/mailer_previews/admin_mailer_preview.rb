0d59e0d4412f5cf2eecbc92d4c08db4d6501f653
Aleksandr Kunin <skyksandr@gmail.com>
2024-12-02 10:25:38 +0800

    Fix mailer templates

--- lib/mailer_previews/admin_mailer_preview.rb
+++ lib/mailer_previews/admin_mailer_preview.rb
@@ -1,6 +1,6 @@
 class MailerPreviews::AdminMailerPreview < ActionMailer::Preview
   def verify_online_competition_result
-    track = PersonalTopScore.where(rank: 1).first.track
+    track = VirtualCompetition::PersonalTopScore.where(rank: 1).first.track
 
     AdminMailer.verify_online_competition_result(track)
   end
