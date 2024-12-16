75d92147a0872da7972ea33c1cce1a26eafdd951
Aleksandr Kunin <skyksandr@gmail.com>
2024-12-16 11:49:33 +0800

    Rubocop

--- app/models/concerns/track/weather_data.rb
+++ app/models/concerns/track/weather_data.rb
@@ -2,14 +2,15 @@ class Track < ApplicationRecord
   module WeatherData
     extend ActiveSupport::Concern
 
-    def weather_data
+    def weather_data # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
       return Place::WeatherDatum.none if place.blank? || base?
 
       time_on_recording_start = points.trimmed.first&.gps_time
       time_on_recording_end = points.trimmed.last&.gps_time
       return Place::WeatherDatum.none if time_on_recording_start.blank? || time_on_recording_end.blank?
 
-      weather_data = (time_on_recording_start.beginning_of_hour..time_on_recording_end.end_of_hour).step(1.hour).map do |time|
+      time_range = time_on_recording_start.beginning_of_hour..time_on_recording_end.beginning_of_hour
+      weather_data = time_range.step(1.hour).map do |time|
         place.weather_data.for_time(time)
       end
       return Place::WeatherDatum.none if weather_data.any?(&:blank?)
