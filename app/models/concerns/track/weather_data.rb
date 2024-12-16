d283f0dfea729f8e5acd855209b96ba398dd2f3a
Aleksandr Kunin <skyksandr@gmail.com>
2024-12-16 11:37:36 +0800

    Fix error when missing weather data for one hour out of two

--- app/models/concerns/track/weather_data.rb
+++ app/models/concerns/track/weather_data.rb
@@ -5,9 +5,16 @@ class Track < ApplicationRecord
     def weather_data
       return Place::WeatherDatum.none if place.blank? || base?
 
-      weather_time = (start_time || recorded_at).beginning_of_hour
+      time_on_recording_start = points.trimmed.first&.gps_time
+      time_on_recording_end = points.trimmed.last&.gps_time
+      return Place::WeatherDatum.none if time_on_recording_start.blank? || time_on_recording_end.blank?
 
-      @weather_data ||= place.weather_data.for_time((weather_time - 1.hour)..(weather_time + 1.hour))
+      weather_data = (time_on_recording_start.beginning_of_hour..time_on_recording_end.end_of_hour).step(1.hour).map do |time|
+        place.weather_data.for_time(time)
+      end
+      return Place::WeatherDatum.none if weather_data.any?(&:blank?)
+
+      weather_data
     end
   end
 end
