65b6d0ebf8a71c11148ff6913f3a2d296fb4eea9
Aleksandr Kunin <skyksandr@gmail.com>
2024-12-17 11:11:42 +0800

    Fix weather data loading

--- app/models/concerns/track/weather_data.rb
+++ app/models/concerns/track/weather_data.rb
@@ -9,13 +9,13 @@ class Track < ApplicationRecord
       time_on_recording_end = points.trimmed.last&.gps_time
       return Place::WeatherDatum.none if time_on_recording_start.blank? || time_on_recording_end.blank?
 
-      time_range = time_on_recording_start.beginning_of_hour..time_on_recording_end.beginning_of_hour
+      time_range = time_on_recording_start.beginning_of_hour.to_i..time_on_recording_end.beginning_of_hour.to_i
       weather_data = time_range.step(1.hour).map do |time|
-        place.weather_data.for_time(time)
+        place.weather_data.for_time(Time.zone.at(time))
       end
-      return Place::WeatherDatum.none if weather_data.any?(&:blank?)
+      return Place::WeatherDatum.none if weather_data.any?(&:exists?)
 
-      weather_data
+      weather_data.inject(:or)
     end
   end
 end
