df79dc7e7811519445356e33076f1a619fcf44ad
Aleksandr Kunin <skyksandr@gmail.com>
2025-01-10 10:11:27 +0800

    Move spec to test and rename

---
+++ test/test_helper.rb
@@ -0,0 +1,35 @@
+ENV['RAILS_ENV'] ||= 'test'
+ENV['NODE_ENV'] ||= 'production'
+require_relative '../config/environment'
+require 'rails/test_help'
+require 'vcr'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+Dir[Rails.root.join('test/support/**/*.rb')].each { |f| require f }
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes'
+  config.hook_into :webmock
+  config.ignore_localhost = true
+end
+
+module ActiveSupport
+  class TestCase
+    include FactoryBot::Syntax::Methods
+    include ActionDispatch::TestProcess
+    include CreateTrackHelper
+
+    fixtures :all
+
+    def run
+      I18n.with_locale(:en) do
+        super
+      end
+    end
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+end
