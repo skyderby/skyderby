1b713b44196fd51dd3495e9f387b98b8ded3f99b
Aleksandr Kunin <skyksandr@gmail.com>
2025-01-14 10:51:01 +0800

    Show latest weather by default

--- app/controllers/places/weather_data_controller.rb
+++ app/controllers/places/weather_data_controller.rb
@@ -8,7 +8,7 @@ class Places::WeatherDataController < ApplicationController
       if params[:day].present? && params[:hour].present?
         Time.zone.parse("#{params[:day]}T#{params[:hour]}:00:00Z")
       else
-        @place.weather_data.pick(:actual_on)
+        @place.weather_data.order(actual_on: :desc).pick(:actual_on)
       end
 
     @weather_data = @place.weather_data.for_time(@time)
