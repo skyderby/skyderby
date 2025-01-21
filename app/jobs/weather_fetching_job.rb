a21a8de1ef3f320f4d32e9dd3d1f3a13dbd19d17
Aleksandr Kunin <skyksandr@gmail.com>
2025-01-21 15:12:16 +0800

    fix name

--- app/jobs/weather_fetching_job.rb
+++ app/jobs/weather_fetching_job.rb
@@ -12,7 +12,7 @@ class WeatherFetchingJob < ApplicationJob
       Rails.logger.info("Downloaded file #{path}. ")
 
       Place.skydive.each do |place|
-        next if place.weather_forecasts.exists?(actual_on: time)
+        next if place.weather_data.exists?(actual_on: time)
 
         place.import_weather_from_gfs_file(grib_file)
       end
