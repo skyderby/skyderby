befe6d5e539eeb3d9feabc2a946d42ca471fef61
Aleksandr Kunin <skyksandr@gmail.com>
2025-05-01 13:24:03 +0800

    Store selected tracks in Url

--- app/javascript/controllers/flight_profiles_controller.js
+++ app/javascript/controllers/flight_profiles_controller.js
@@ -1,7 +1,9 @@
 import { Controller } from 'stimulus'
 
 export default class FlightProfilesController extends Controller {
-  connect() {}
+  connect() {
+    this.selectedTracks = new Set(this.getSelectedTracksFromUrl())
+  }
 
   disconnect() {}
 
@@ -22,7 +24,40 @@ export default class FlightProfilesController extends Controller {
     event.preventDefault()
     const trackElement = event.target.closest('a')
     const trackId = trackElement.dataset.id
-    trackElement.classList.toggle('active')
-    console.log('Track clicked:', trackId)
+
+    if (this.selectedTracks.has(trackId)) {
+      this.selectedTracks.delete(trackId)
+      trackElement.classList.remove('active')
+    } else {
+      this.selectedTracks.add(trackId)
+      trackElement.classList.add('active')
+    }
+
+    this.updateUrlWithSelectedTracks()
+  }
+
+  setActiveTracks(event) {
+    for (let trackElement of event.target.children) {
+      const trackId = trackElement.dataset.id
+      if (this.selectedTracks.has(trackId)) {
+        trackElement.classList.add('active')
+      }
+    }
+  }
+
+  getSelectedTracksFromUrl() {
+    const url = new URL(window.location.href)
+    return url.searchParams.getAll('track[]')
+  }
+
+  updateUrlWithSelectedTracks() {
+    const url = new URL(window.location.href)
+    url.searchParams.delete('track[]')
+
+    this.selectedTracks.forEach(trackId => {
+      url.searchParams.append('track[]', trackId)
+    })
+
+    window.history.replaceState({}, '', url)
   }
 }
