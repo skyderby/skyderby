9af1aa4d41eab22d12e28aa7544456360a79c7fb
Aleksandr Kunin <skyksandr@gmail.com>
2025-05-03 12:44:12 +0800

    Saving/restoring filters in URL

--- app/controllers/flight_profiles_controller.rb
+++ app/controllers/flight_profiles_controller.rb
@@ -1,5 +1,18 @@
 class FlightProfilesController < ApplicationController
   def show
     @jump_profile = Place::JumpLine.find_by(id: params[:jump_profile_id])
+
+    @filters = [
+      Profile
+        .where(id: params[:profile_id])
+        .pluck(:id, :name).map { |id, name| [id, name, 'profile_id[]', 'Profile'] },
+      Suit
+        .where(id: params[:suit_id])
+        .pluck(:id, :name).map { |id, name| [id, name, 'suit_id[]', 'Suit'] },
+      Place
+        .where(id: params[:place_id])
+        .pluck(:id, :name).map { |id, name| [id, name, 'place_id[]', 'Place'] },
+      params[:year]&.map { |year| [year, year, "'year[]'", "'Year'"] }
+    ].flatten(1).compact
   end
 end
--- app/javascript/controllers/tracks_search_controller.js
+++ app/javascript/controllers/tracks_search_controller.js
@@ -1,6 +1,11 @@
 import { Controller } from 'stimulus'
 
 export default class TracksSearchController extends Controller {
+  connect() {
+    this.params = ['year[]', 'profile_id[]', 'suit_id[]', 'place_id[]']
+    this.form.requestSubmit()
+  }
+
   handleFilterAdd(event) {
     const { type, value } = event.detail
     const input = document.createElement('input')
@@ -9,14 +14,14 @@ export default class TracksSearchController extends Controller {
     input.value = value
     this.element.appendChild(input)
 
-    this.element.closest('form').requestSubmit()
+    this.submitWithUrlUpdate()
   }
 
   handleFilterRemove(event) {
     const { type, value } = event.detail
     this.element.querySelector(`input[name="${type}"][value="${value}"]`)?.remove()
 
-    this.element.closest('form').requestSubmit()
+    this.submitWithUrlUpdate()
   }
 
   handleFilterClear() {
@@ -24,6 +29,28 @@ export default class TracksSearchController extends Controller {
       .querySelectorAll('input:not([name="kind"])')
       .forEach(element => element.remove())
 
-    this.element.closest('form').requestSubmit()
+    this.submitWithUrlUpdate()
+  }
+
+  submitWithUrlUpdate() {
+    const url = new URL(window.location.href)
+    this.form.requestSubmit()
+
+    const formData = new FormData(this.form)
+
+    Array.from(url.searchParams.keys()).forEach(key => {
+      if (this.params.includes(key)) url.searchParams.delete(key)
+    })
+
+    for (const [key, value] of formData.entries()) {
+      if (key === 'kind') continue
+      url.searchParams.append(key, value)
+    }
+
+    window.history.replaceState({}, '', url)
+  }
+
+  get form() {
+    return this.element.closest('form')
   }
 }
