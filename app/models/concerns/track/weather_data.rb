0e126d403df9c86e60cc27701f279d4754e457a4
Aleksandr Kunin <skyksandr@gmail.com>
2025-07-27 22:36:52 +0800

    Fix potential error with no wind data due to next/prev hour

--- app/models/concerns/track/weather_data.rb
+++ app/models/concerns/track/weather_data.rb
@@ -5,7 +5,7 @@ class Track < ApplicationRecord
     def weather_data # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/PerceivedComplexity
       return Place::WeatherDatum.none if place.blank? || base?
 
-      trimmed_points = points.order(:gps_time_in_seconds).trimmed
+      trimmed_points = points.order(:gps_time_in_seconds).trimmed(seconds_before_start: 20, seconds_after_end: 125)
       time_on_recording_start = trimmed_points.first&.gps_time
       time_on_recording_end = trimmed_points.last&.gps_time
       return Place::WeatherDatum.none if time_on_recording_start.blank? || time_on_recording_end.blank?
