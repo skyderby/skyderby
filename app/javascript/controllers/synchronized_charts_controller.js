5552822bce9fcb9462a44af9969838f73b302a1d
Aleksandr Kunin <skyksandr@gmail.com>
2025-12-27 19:41:28 +0800

    Improve charts tooltip sync

--- app/javascript/controllers/synchronized_charts_controller.js
+++ app/javascript/controllers/synchronized_charts_controller.js
@@ -3,26 +3,26 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['chart']
 
-  refresh_tooltip(event) {
-    this.charts.forEach(chart => {
-      this.draw_tooltip(event, chart)
+  refreshTooltip(event) {
+    this.chartTargets.forEach(target => {
+      this.drawTooltip(event, target.chart)
     })
   }
 
-  draw_tooltip(event, chart) {
-    if (!chart) return
+  drawTooltip(event, chart) {
+    if (!chart || !chart.pointer) return
 
-    const normalized_event = chart.pointer.normalize(event)
-    const points = this.hovered_points(normalized_event, chart)
+    const normalizedEvent = chart.pointer.normalize(event)
+    const points = this.hoveredPoints(normalizedEvent, chart)
 
     if (points.length > 0) {
       points[0].onMouseOver()
       chart.tooltip.refresh(points)
-      chart.xAxis[0].drawCrosshair(normalized_event, points[0])
+      chart.xAxis[0].drawCrosshair(normalizedEvent, points[0])
     }
   }
 
-  hovered_points(event, chart) {
+  hoveredPoints(event, chart) {
     return chart.series.reduce((result, series) => {
       if (!series.visible || series.enableMouseTracking === false) return result
 
@@ -31,15 +31,5 @@ export default class extends Controller {
 
       return result
     }, [])
-  }
-
-  get charts() {
-    if (!this._charts) {
-      this._charts = this.chartTargets.map(el =>
-        Highcharts.charts.find(chart => chart.renderTo === el)
-      )
-    }
-
-    return this._charts
   }
 }
