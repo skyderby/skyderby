3a168cbd390f82a942b0bc639a5f2cb5e86fbfba
Aleksandr Kunin <skyksandr@gmail.com>
2026-01-07 19:03:38 +0800

    revert rails/all to specific railtie

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+require 'rails'
+
+require 'active_record/railtie'
+require 'active_job/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_model/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 require 'csv'
 require 'zip'
